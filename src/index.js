cf4eaa18093a10d720d55d6164dadc3a564db617
Jaufré Lallement <lallement.jaufre@orange.fr>
2018-07-19 01:29:32 +0200

    Update: changes in project tree

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 require('dotenv').config()
 const server = require('./server')
-const modbusServer = require('./modbusServer')
+const getConfig = require('./south/modbus/config/modbus.config')
+const modbusClient = require('./south/modbus/modbusClient')
+
+// Connection options for client
+const options = {
+  host: 'localhost',
+  port: 502,
+}
+
+getConfig() // Generates the optimized configuration
 
 const port = process.env.PORT || 3333
 server.listen(port, () => console.info(`API server started on ${port}`))
-modbusServer.listen(502, () => console.info('Modbus server is listening on port 502'))
+modbusClient.connect(options)
