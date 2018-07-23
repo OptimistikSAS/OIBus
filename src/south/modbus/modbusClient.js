616fa243858990b30e88aeaaad4c36b3a728431b
Jaufré Lallement <lallement.jaufre@orange.fr>
2018-07-23 10:17:03 +0200

    Update: modbus files, modbus.config.js is not writting an optimized config file anymore. modbusClient improved with dynamic call instead of switch/cases

--- src/south/modbus/modbusClient.js
+++ src/south/modbus/modbusClient.js
@@ -3,12 +3,13 @@ const net = require('net')
 const { CronJob } = require('cron')
 const fs = require('fs')
 const configService = require('../../services/config.service')
+const getConfig = require('./config/modbus.config')
 
 const socket = new net.Socket()
 const client = new modbus.client.TCP(socket)
 
 socket.on('connect', () => {
-  const args = configService.parseArgs() || {}
+  const args = configService.parseArgs() || {} // Arguments of the command
   const { configPath = './fTbus.config.json' } = args // Get the configuration file path
 
   // Check if the provided file is json
@@ -17,23 +18,25 @@ socket.on('connect', () => {
     return false
   }
 
-  const fTbusConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'))
-  const { scanModes } = fTbusConfig
+  const fTbusConfig = JSON.parse(fs.readFileSync(configPath, 'utf8')) // Get fTbus configuration file
+  const { scanModes } = fTbusConfig // Get the cron frequences file path
   const frequences = JSON.parse(fs.readFileSync(scanModes, 'utf8')) // Read the cron frequences file
-  const optimizedConfig = JSON.parse(fs.readFileSync('./tests/optimizedConfig.json', 'utf8')) // Read configuration file synchronously
+  const optimizedConfig = getConfig() // Read optimized configuration file
 
+  // Check if the optimized configuration is correctly set
   if (!optimizedConfig) {
     console.info('Optimized config file not found. Make sure you generated it correctly.')
     return false
   }
 
+  // Check if the frequences file has been correctly retreived
   if (!frequences) {
     console.info('Frequences file not found.')
     return false
   }
 
   optimizedConfig.forEach((equipment) => {
-    const { equipmentId, protocol, variableGroups } = equipment
+    const { variableGroups } = equipment
     Object.keys(variableGroups).forEach((frequenceName) => {
       const typesForFrequence = variableGroups[frequenceName]
       const { frequence: cronTime } = frequences.find(({ name }) => name === frequenceName)
@@ -41,60 +44,33 @@ socket.on('connect', () => {
         cronTime,
         onTick: () => {
           Object.keys(typesForFrequence).forEach((type) => {
-            const addressesForType = typesForFrequence[type]
+            const addressesForType = typesForFrequence[type] // Addresses of the group
 
-            const thenFunction = (resp) => {
-              console.log('Response: ', JSON.stringify(resp.response))
-            }
+            const funcName = `read${`${type.charAt(0).toUpperCase()}${type.slice(1)}`}s` // Build function name, IMPORTANT: type must be singular
 
-            const errorFunction = (error) => {
-              console.error(error)
-              socket.end()
-            }
-            let modbusFunction
-            switch (type) {
-              case 'holdingRegister':
-                modbusFunction = (startAddress, count) =>
-                  client
-                    .readHoldingRegisters(startAddress, count)
-                    .then(resp => thenFunction(resp))
-                    .catch(error => errorFunction(error))
-                break
-              case 'inputRegister':
-                modbusFunction = (startAddress, count) =>
-                  client
-                    .readInputRegisters(startAddress, count)
-                    .then(resp => thenFunction(resp))
-                    .catch(error => errorFunction(error))
-                break
-              case 'discreteInput':
-                modbusFunction = (startAddress, count) =>
-                  client
-                    .readDiscreteInputs(startAddress, count)
-                    .then(resp => thenFunction(resp))
-                    .catch(error => errorFunction(error))
-                break
-              default:
-                modbusFunction = (startAddress, count) =>
-                  client
-                    .readCoils(startAddress, count)
-                    .then(resp => thenFunction(resp))
-                    .catch(error => errorFunction(error))
-                break
-            }
+            // Dynamic call of the appropriate function based on type
+            const modbusFunction = (startAddress, count) =>
+              client[funcName](startAddress, count)
+                .then((resp) => {
+                  console.log('Response: ', JSON.stringify(resp.response))
+                })
+                .catch((error) => {
+                  console.error(error)
+                  socket.end()
+                })
+
             Object.keys(addressesForType).forEach((range) => {
-              const addresses = addressesForType[range]
               const rangeAddresses = range.split('-')
-              const startAddress = parseInt(rangeAddresses[0], 10)
-              const endAddress = parseInt(rangeAddresses[1], 10)
-              const rangeSize = endAddress - startAddress
+              const startAddress = parseInt(rangeAddresses[0], 10) // First address of the group
+              const endAddress = parseInt(rangeAddresses[1], 10) // Last address of the group
+              const rangeSize = endAddress - startAddress // Size of the addresses group
               modbusFunction(startAddress, rangeSize)
             })
           })
         },
         start: false,
       })
-      job.start()
+      job.start() // Run the job
     })
   })
   return true
