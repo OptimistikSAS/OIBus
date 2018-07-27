2b83a69f84a846f14e62ef269629f3fc01d4cb2c
Jaufré Lallement <lallement.jaufre@orange.fr>
2018-07-27 14:46:00 +0200

    Update: start to implement the Mapping of the responses

---
+++ src/ResponsesHandler.class.js
@@ -0,0 +1,25 @@
+/**
+ * Class ResponsesHandler : allows to manage requests responses in a Map
+ */
+class ResponsesHandler {
+  /**
+   * Constructor for the class ResponsesHandler
+   */
+  constructor(name) {
+    this.name = name
+    this.responses = new Map()
+  }
+
+  /**
+   * Updates the responses map
+   * @param {Object} entry : new entry (id and data of the entry)
+   * @param {Function} callback : callback function
+   * @return {void}
+   */
+  update({ id, data }, callback) {
+    this.responses.set(id, data)
+    callback(this.responses)
+  }
+}
+
+module.exports = ResponsesHandler
