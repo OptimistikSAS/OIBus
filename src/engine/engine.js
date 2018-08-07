c2582f6e8069d2fc81dc11155eabb5465d3b4b79
GDami <41579255+GDami@users.noreply.github.com>
2018-08-07 13:27:27 +0200

    multiple destinations
    - a modbus client is created for each equipment in the config file. the modbus client has destination address and port as new properties
    - made the optimizedConfig global so every client can use it.
    - modbus clients are put in 'protocols' which is now an object whose properties (modbus clients) are named after the equipment name, so it can be accessed using ' protocols['PLC-35'] ' for instance

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -3,15 +3,18 @@ const ModbusClient = require('../south/modbus/ModbusClient.class')
 const ResponsesHandler = require('./ResponsesHandler.class')
 
 const responses = new ResponsesHandler()
+const protocols = {}
 
 const start = (config, callback = () => {}) => {
-  const protocols = [new ModbusClient(config, responses)]
-
-  protocols.forEach(protocol => protocol.connect('localhost', 502))
+  Object.keys(config.equipments).forEach((key) => {
+    const equipment = config.equipments[key]
+    protocols[equipment.equipmentId] = new ModbusClient(responses, equipment.modbus.address, equipment.modbus.port)
+  })
+  Object.keys(protocols).forEach(key => protocols[key].connect())
   config.scanModes.forEach(({ scanMode, cronTime }) => {
     const job = new CronJob({
       cronTime,
-      onTick: () => protocols.forEach(protocol => protocol.poll(scanMode)),
+      onTick: () => Object.keys(protocols).forEach(key => protocols[key].poll(scanMode)),
       start: false,
     })
     job.start()
