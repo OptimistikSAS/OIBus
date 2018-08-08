3b6494d9af8514c0790287d8625d320867e78ddb
GDami <41579255+GDami@users.noreply.github.com>
2018-08-08 18:03:22 +0200

    more ModbusClient changes
    - add() is now called by the constructor rather than engine.js
    - 'address' was changed to 'host' in both ModbusClient and fTbus.json
    - removed the forEach structure in modbusFunction()

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -3,21 +3,21 @@ const ModbusClient = require('../south/modbus/ModbusClient.class')
 const ResponsesHandler = require('./ResponsesHandler.class')
 
 const responses = new ResponsesHandler()
-const protocols = {}
+const activeProtocols = {}
+const protocolList = { modbus: ModbusClient }
 
 const start = (config, callback = () => {}) => {
-  const protocolList = { modbus: ModbusClient }
+  // adds every protocol to be used in activeProtocols
   Object.values(config.equipments).forEach((equipment) => {
-    if (!protocols[equipment.protocol]) {
-      protocols[equipment.protocol] = new protocolList[equipment.protocol](config, responses)
+    if (!activeProtocols[equipment.protocol]) {
+      activeProtocols[equipment.protocol] = new protocolList[equipment.protocol](config, responses)
     }
-    protocols[equipment.protocol].add(equipment)
   })
-  Object.keys(protocols).forEach(key => protocols[key].connect())
+  Object.keys(activeProtocols).forEach(key => activeProtocols[key].connect())
   config.scanModes.forEach(({ scanMode, cronTime }) => {
     const job = new CronJob({
       cronTime,
-      onTick: () => Object.keys(protocols).forEach(key => protocols[key].poll(scanMode)),
+      onTick: () => Object.keys(activeProtocols).forEach(key => activeProtocols[key].poll(scanMode)),
       start: false,
     })
     job.start()
