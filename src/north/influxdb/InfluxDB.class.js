39806e380d50d065698c08e96988b4957d89a16f
GDami <41579255+GDami@users.noreply.github.com>
2018-08-29 18:16:27 +0200

    modbus and influxdb changes
    in modbus :
    - the modbusFunction will search the right data in the modbusResponse for each point and call Engine.addValue for each of those
    - InfluxDB uses the InfluxDB.insert string in the config file and regex to form the request

--- src/north/influxdb/InfluxDB.class.js
+++ src/north/influxdb/InfluxDB.class.js
@@ -1,9 +1,25 @@
 const fetch = require('node-fetch')
+const { sprintf } = require('sprintf-js')
 const Application = require('../Application.class')
+
+const pointIdToNodes = (pointId) => {
+  const attributes = {}
+  pointId
+    .slice(1)
+    .split('#')[0]
+    .split('/') // Split to treat every tag.tagvalue element out of the pointId
+    .forEach((node) => {
+      const nodeId = node.replace(/[\w ]+\.([\w]+)/g, '$1') // This regex extracts the part after the dot
+      const nodeValue = node.replace(/([\w ]+)\.[\w]+/g, '$1') // This regex extracts the part before the dot
+      attributes[nodeId] = nodeValue
+    })
+  return attributes
+}
 
 class InfluxDB extends Application {
   constructor(engine) {
     super(engine)
+    this.host = 'localhost:8086'
     this.currentObject = {}
   }
 
@@ -15,24 +31,22 @@ class InfluxDB extends Application {
   }
 
   makeRequest() {
-    const { /* data, */pointId, timestamp } = this.currentObject
+    const { data, pointId, timestamp } = this.currentObject
     const nodes = {}
-    // nodes.data = data
+    nodes.data = data
+    nodes.host = this.host
     nodes.timestamp = timestamp
-    pointId
-      .slice(1)
-      .split('#')[0]
-      .split('/')
-      .forEach((node) => {
-        const nodeId = node.replace(/[\w ]+\.([\w]+)/g, '$1')
-        const nodeValue = node.replace(/([\w ]+)\.[\w]+/g, '$1')
-        nodes[nodeId] = nodeValue
-      })
-    Object.entries(nodes).forEach(([nodeId, node]) => {
-      nodes[nodeId] = node.replace(/ /g, '\\ ')
+    Object.entries(pointIdToNodes(pointId)).forEach(([nodeId, node]) => {
+      nodes[nodeId] = node
     })
-    fetch(`http://localhost:8086/write?db=${nodes.base}`, {
-      body: `zzz,timestamp=${nodes.timestamp},tank=${nodes.tank} value=10`,
+    Object.entries(nodes).forEach(([nodeId, node]) => {
+      if (nodeId !== 'data') {
+        nodes[nodeId] = node.replace(/ /g, '\\ ')
+        if (!Number.isNaN(parseInt(node, 10))) nodes.measurement = nodeId
+      }
+    })
+    console.log(nodes)
+    const insert = global.fTbusConfig.applications[1].InfluxDB.insert.replace(/'/g,'')
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       method: 'POST',
     })
