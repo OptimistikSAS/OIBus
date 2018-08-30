4fd6bc9269d0deb364d3e9dd082e1ff65a70aeef
GDami <41579255+GDami@users.noreply.github.com>
2018-08-30 17:22:02 +0200

    comments 1
    partly commented code

--- src/engine/Queue.class.js
+++ src/engine/Queue.class.js
@@ -1,9 +1,9 @@
 /**
- * Class Responses : allows to manage requests responses in a Map
+ * Class Queue : manages a queue containing timestamped measurements
  */
 class Queue {
   /**
-   * Constructor for the class ResponsesHandler
+   * Constructor for the class Queue
    */
   constructor(engine) {
     this.buffer = []
@@ -11,25 +11,30 @@ class Queue {
   }
 
   /**
-   * Updates the queues map
+   * Updates the queue
    * @param {Object} entry : new entry (pointId, timestamp and data of the entry)
    * @return {void}
    */
   enqueue(value) {
     this.buffer.push(value)
-    // Ajouter une alerte pour tous les protocoles/applications
   }
 
+  /**
+   * Removes the first (oldest) entry in the queue
+   * @return {Object} entry : the removed entry is returned
+   */
   dequeue() {
     return this.buffer.shift()
   }
 
+  /**
+   * Provides an Object with informations about the queue.
+   * Currently only contains its length.
+   * @return {Object} informations about the queue
+   */
   info() {
     return { length: Object.keys(this.buffer).length }
   }
-
-  // Quand la map est mise à jour un évènement est créé pour toutes les applications
-  // Quand remove seule 1 app (celle qui remove) est alertée
 }
 
 module.exports = Queue
--- src/north/influxdb/InfluxDB.class.js
+++ src/north/influxdb/InfluxDB.class.js
@@ -2,15 +2,21 @@ const fetch = require('node-fetch')
 const { sprintf } = require('sprintf-js')
 const Application = require('../Application.class')
 
+
+/**
+ * Reads a string in pointId format and returns an object with corresponding indexes and values.
+ * @param {String} pointId : string with this form : value1.name1/value2.name2#value
+ * @return {Object} attributes : values indexed by name
+ */
 const pointIdToNodes = (pointId) => {
   const attributes = {}
   pointId
     .slice(1)
     .split('#')[0]
-    .split('/') // Split to treat every tag.tagvalue element out of the pointId
+    .split('/')
     .forEach((node) => {
-      const nodeId = node.replace(/[\w ]+\.([\w]+)/g, '$1') // This regex extracts the part after the dot
-      const nodeValue = node.replace(/([\w ]+)\.[\w]+/g, '$1') // This regex extracts the part before the dot
+      const nodeId = node.replace(/[\w ]+\.([\w]+)/g, '$1')
+      const nodeValue = node.replace(/([\w ]+)\.[\w]+/g, '$1')
       attributes[nodeId] = nodeValue
     })
   return attributes
@@ -45,8 +51,14 @@ class InfluxDB extends Application {
         if (!Number.isNaN(parseInt(node, 10))) nodes.measurement = nodeId
       }
     })
-    console.log(nodes)
-    const insert = global.fTbusConfig.applications[1].InfluxDB.insert.replace(/'/g,'')
+    const insert = global.fTbusConfig.applications[1].InfluxDB.insert.replace(/'/g, '')
+    const body = sprintf((`${insert.split(' ')[2]} ${insert.split(' ')[3]}`)
+      .replace(/xxx/g, 'timestamp')
+      .replace(/yyy(tag)?/g, 'tank')
+      .replace(/zzz/g, '%(measurement)s')
+      .replace(/( \w+=).*/g, '$1%(data)s'), nodes)
+    fetch(sprintf(`http://${insert.split(' ')[0]}`, nodes), {
+      body,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       method: 'POST',
     })
