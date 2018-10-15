fb83eaf633c6f71866bb769f53a1cf8eccebeae0
Baizheyuan <zheyuan.bai@utt.fr>
2018-10-15 09:23:28 +0200

    tests for CSV-large-file BZ

---
+++ src/south/CSV/test.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const readStream = fs.createReadStream('./tests/csv/input/fichier.txt')
+const content = readStream.read()
+console.log(content)
+readStream.on('error', (err) => {
+  console.error(err)
+})
+
+readStream.on('open', (fd) => {
+  console.log('FIle opened', fd)
+})
+
+readStream.on('ready', () => {
+  console.log('File ready..')
+})
+
+readStream.on('data', (chunk) => {
+  console.log('Reading datas:', chunk)
+})
