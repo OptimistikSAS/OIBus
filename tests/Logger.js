b244205c678bbe2884dcdf9e35ca234dc911f284
Baizheyuan <zheyuan.bai@utt.fr>
2018-10-17 16:19:30 +0200

    module Logger BZ

---
+++ tests/Logger.js
@@ -0,0 +1,17 @@
+const { createLogger, format, transports } = require('winston')
+
+const { combine, timestamp, printf } = format
+
+const fileFormat = printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+const consoleFormat = printf(info => `${info.level}: ${info.message}`)
+
+const logger = createLogger({
+  format: combine(timestamp(), fileFormat),
+  transports: [
+    new transports.Console({ format: consoleFormat }),
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' }),
+  ],
+})
+
+module.exports = logger
