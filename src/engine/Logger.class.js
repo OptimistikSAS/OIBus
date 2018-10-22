86bc2bb46a63a88e1a936bc49e271a6cdc067d27
Baizheyuan <zheyuan.bai@utt.fr>
2018-10-22 15:59:52 +0200

    Colorize and extra parameters  BZ
    Logs written to the file are already colorized, so we only need to change the format for console

--- src/engine/Logger.class.js
+++ src/engine/Logger.class.js
@@ -3,38 +3,36 @@
 // this is just a stuppid way to garantee the debugging work and need to be changed one day
 const { createLogger, format, transports } = require('winston')
 
-const { combine, timestamp, printf } = format
+const { combine, timestamp, printf, colorize } = format
 class Logger {
   constructor(logParameters) {
-    const { consoleLevel, fileLevel, filename } = logParameters
-    this.debugMode = logParameters.debugMode
-    const fileFormat = printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    const consoleFormat = printf(info => `${info.level}: ${info.message}`)
+    const { consoleLevel, fileLevel, filename, maxFiles, maxsize, tailable } = logParameters
+    const defaultFormat = combine(timestamp(), printf(info => `${info.timestamp} ${info.level}: ${info.message}`))
+    const consoleFormat = combine(colorize(), printf(info => `${info.level}: ${info.message}`))
     this.logger = createLogger({
       level: consoleLevel,
-      format: combine(timestamp(), fileFormat),
-      transports: [new transports.Console({ format: consoleFormat }), new transports.File({ filename, level: fileLevel })],
+      format: defaultFormat,
+      transports: [
+        new transports.Console({ format: consoleFormat }),
+        new transports.File({ filename, level: fileLevel, maxsize, maxFiles, tailable }),
+      ],
     })
   }
 
   info(message) {
     this.logger.info(message)
-    if (this.debugMode) console.info(message)
   }
 
   warn(message) {
     this.logger.warn(message)
-    if (this.debugMode) console.warn(message)
   }
 
   error(message) {
     this.logger.error(message)
-    if (this.debugMode) console.error(message)
   }
 
   debug(message) {
     this.logger.debug(message)
-    if (this.debugMode) console.debug(message)
   }
 }
 
