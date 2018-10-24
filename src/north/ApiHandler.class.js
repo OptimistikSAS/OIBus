e287f832cae24ef5726fc301d0d2b26a4ef09ab3
Baizheyuan <zheyuan.bai@utt.fr>
2018-10-24 11:43:23 +0200

    Seperation of Simular from Engine BZ

--- src/north/ApiHandler.class.js
+++ src/north/ApiHandler.class.js
@@ -9,6 +9,20 @@ class ApiHandler {
     this.queue = new Queue()
     this.application = api
     this.engine = engine
+    this.config = this.engine.config
+    // initialize the scanMode object with empty arrays
+    this.scanModes = {}
+    this.config.engine.scanModes.forEach(({ scanMode }) => {
+      this.scanModes[scanMode] = []
+    })
+    this.config.south.equipments.forEach((equipment) => {
+      if (equipment.defaultScanMode) {
+        const scanMode = equipment.defaultScanMode
+        if (this.scanModes[scanMode] && !this.scanModes[scanMode].includes(equipment.equipmentId)) {
+          this.scanModes[scanMode].push(equipment.equipmentId)
+        }
+      }
+    })
   }
 
   enqueue(value) {
