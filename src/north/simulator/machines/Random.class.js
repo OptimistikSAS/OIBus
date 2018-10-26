c0c784cb401d3e2b651b97a854c445e9d5536618
GDami <41579255+GDami@users.noreply.github.com>
2018-10-26 13:57:01 +0200

    Random simulator added

--- src/north/simulator/machines/Random.class.js
+++ src/north/simulator/machines/Random.class.js
@@ -1,5 +1,18 @@
 const Machine = require('./Machine.class')
 
 class Random extends Machine {
-    constructor()
+  run() {
+    const { integer, scale, qualityIndicator } = this.parameters
+    let quality
+    if (Math.random() < qualityIndicator) {
+      quality = true
+    } else {
+      quality = false
+    }
+    let number = Math.random() * scale
+    if (integer) number = Math.floor(number)
+    this.state = { number, quality }
+  }
 }
+
+module.exports = Random
