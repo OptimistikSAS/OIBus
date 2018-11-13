9bf6a8769dcd3ebfaf658c504a474894f567c184
Baizheyuan <zheyuan.bai@utt.fr>
2018-11-13 11:32:32 +0100

    crypto beta 1.0
    Only works with Chrome

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,11 +1,8 @@
-const crypto = require('crypto')
-
-
 /**
  * Module dependencies.
  */
-
-const auth = require('koa-basic-auth')
+const crypto = require('crypto')
+const auth = require('basic-auth')
 
 /**
  * Return basic auth middleware with
@@ -19,19 +16,15 @@ const auth = require('koa-basic-auth')
  * @return {GeneratorFunction}
  * @api public
  */
-
-const authCrpto = (opts = {}) => {
-  if (!opts.name && !opts.pass) throw new Error('Basic auth `name` and/or `pass` is required')
-
+module.exports = function (opts) {
   if (!opts.realm) opts.realm = 'Secure Area'
 
   return function basicAuth(ctx, next) {
     const user = auth(ctx)
     const hash = crypto.createHash('MD5', user.pass).digest('hex')
-    if (!user || (opts.name && opts.name !== user.name) || (opts.pass && opts.pass !== hash)) {
-      return ctx.throw(401, null, { headers: { 'WWW-Authenticate': `Basic realm="${opts.realm.replace(/"/g, '\\"')}"` } })
+    if (user && user.name === opts.name && hash === opts.pass) {
+      next()
     }
-    return next()
+    ctx.throw(401, null, { headers: { 'WWW-Authenticate': `Basic realm="${opts.realm.replace(/"/g, '\\"')}"` } })
   }
 }
-module.exports = authCrpto
