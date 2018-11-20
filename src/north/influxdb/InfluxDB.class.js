23a8e0cc5829ec7e85f8674b61f9a9ac6b9059e2
Baizheyuan <zheyuan.bai@utt.fr>
2018-11-20 16:31:43 +0100

    Rewrite influxDB

--- src/north/influxdb/InfluxDB.class.js
+++ src/north/influxdb/InfluxDB.class.js
@@ -1,22 +1,5 @@
 const fetch = require('node-fetch')
-const { sprintf } = require('sprintf-js')
 const ApiHandler = require('../ApiHandler.class')
-
-/**
- * Reads a string in pointId format and returns an object with corresponding indexes and values.
- * @param {String} pointId : string with this form : value1.name1/value2.name2#value
- * @return {Object} attributes : values indexed by name
- */
-const pointIdToNodes = (attributes, pointId) => {
-  pointId
-    .slice(1)
-    .split('/')
-    .forEach((node) => {
-      const nodeId = node.replace(/[\w ]+\.([\w]+)/g, '$1') // Extracts the word after the dot
-      const nodeValue = node.replace(/([\w ]+)\.[\w]+/g, '$1') // Extracts the one before
-      attributes[nodeId] = nodeValue
-    })
-}
 
 /**
  * Class InfluxDB : generates and sends InfluxDB requests
@@ -47,40 +30,20 @@ class InfluxDB extends ApiHandler {
    * @return {void}
    */
   makeRequest(entry) {
-    const { pointId } = entry
-    console.log(entry)
-    const nodes = { host: this.host }
-    Object.entries(entry).forEach(([id, prop]) => {
-      if (id !== 'pointId') {
-        nodes[id] = prop
-      }
+    const { host, user, password, db } = this.application.InfluxDB
+    const { pointId, data, timestamp } = entry
+    console.log('Auth succeeded!!')
+    const url = `http://${host}/write?u=${user}&p=${password}&db=${db}`
+    const measurement = 'temperature'
+    let fields
+    // The data received from MQTT is type of string, so we need to transform is to Json
+    const dataJson = JSON.parse(data)
+    Object.entries(dataJson).forEach(([fieldKey, fieldValue]) => {
+      if (fields) fields = `${fields},${fieldKey}=${fieldValue}`
+      else fields = `${fieldKey}=${fieldValue}`
     })
-    pointIdToNodes(nodes, pointId)
-    console.log(nodes.dataId)
-    Object.entries(nodes).forEach(([nodeId, node]) => {
-      if (nodeId !== nodes.dataId) {
-        console.log(node, nodeId)
-        nodes[nodeId] = node.replace(/ /g, '\\ ')
-        if (!Number.isNaN(parseInt(node, 10))) nodes.measurement = nodeId
-      }
-    })
-    const insert = this.applicationParameters.InfluxDB.insert.replace(/'/g, '')
-    let body = `${insert.split(' ')[2]} ${insert.split(' ')[3]}`
-      .replace(/zzz/g, '%(measurement)s') // // Use types from config ??
-      .replace(/ \w+=.*/g, ' %(dataId)s=') // Looks for the last field (value field) and inserts the data
-    body = `${body}%(${nodes.dataId})s`
-    Object.keys(nodes).forEach((nodeId) => {
-      const notTags = ['data', 'host', 'measurement', nodes.measurement, 'base']
-      if (!notTags.includes(nodeId)) {
-        // Replaces 3-same-letter fields by a tagName
-        // i.e : Transforms xxx=%(xxx) into tagName=%(tagName)
-        body = body.replace(/([a-z])\1\1(tag)?=%\(\1\1\1\)/, `${nodeId}=%(${nodeId})`)
-      }
-    })
-    // Removes any unused 'xxx=%(xxx)' field from body
-    body = body.replace(/,([a-z])\1\1(tag)?=%\(\1\1\1\)/g, '')
-    body = sprintf(body, nodes)
-    fetch(sprintf(`http://${insert.split(' ')[0]}`, nodes), {
+    const body = `${measurement} ${fields} ${timestamp.getTime()}`
+    fetch(url, {
       body,
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       method: 'POST',
