55e860c12be6d641e5ef15f9c78223a128443692
JFH <it@factorythings.com>
2018-12-09 23:23:06 +0100

    various simplifications

--- src/server/info.js
+++ src/server/info.js
@@ -1,27 +1,4 @@
 const getInfo = (ctx) => {
-  const authHeader = ctx.request.header.authorization || ''
-  // following sequence allow to determine the user/password
-  // used in the web request
-  // authHeader format: "Basic <hashcode for user:pass"
-
-  // 1/ Check format
-  if (authHeader.startsWith('Basic ')) {
-    // 2 extract encoded user:pass
-    const encodedUsernamePassword = authHeader.split(' ')[1]
-
-    // 3 decode back from Base64 to string:
-    const usernamePassword = Buffer.from(encodedUsernamePassword, 'base64')
-      .toString()
-      .split(':')
-    const username = usernamePassword[0]
-    const password = usernamePassword[1]
-    console.log(`request from ${username} with password:${password}`)
-  } else {
-    // Handle what happens if that isn't the case
-    ctx.throw(400, 'The authorization header is either empty or is not Basic.')
-    return
-  }
   ctx.ok({ config: ctx.app.engine.config })
 }
-
 module.exports = { getInfo }
