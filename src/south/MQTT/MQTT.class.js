b48a4916a71da0852e93bc38ce4f7ac4c9c993a0
kukukk <szabolcs.balogh@lynxsolutions.eu>
2019-01-18 09:12:34 +0200

    Every south driver sends the timestamp in milliseconds

--- src/south/MQTT/MQTT.class.js
+++ src/south/MQTT/MQTT.class.js
@@ -34,7 +34,7 @@ class MQTT extends ProtocolHandler {
       this.client.on('message', (topic1, message) => {
         if (topic1 === topic) {
           // message is Buffer
-          this.engine.addValue({ data: message.toString(), timestamp: new Date(), pointId })
+          this.engine.addValue({ data: message.toString(), timestamp: new Date().getTime(), pointId })
         }
       })
     })
