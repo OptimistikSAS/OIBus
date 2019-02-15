22437381527a58cc681618c1b1bbc04f59050fc1
kukukk <szabolcs.balogh@lynxsolutions.eu>
2019-02-15 10:06:33 +0200

    Skeleton for file cache

--- src/engine/Cache.class.js
+++ src/engine/Cache.class.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const path = require('path')
+
 const sqlite3 = require('sqlite3')
 
 /**
@@ -37,12 +39,20 @@ class Cache {
       const activeApi = {}
 
       activeApi.applicationId = application.application.applicationId
-      activeApi.database = this.createDatabase(`${activeApi.applicationId}.db`)
       activeApi.config = application.application.caching
       activeApi.timeout = setTimeout(
         this.sendCallback.bind(this, activeApi.applicationId),
         activeApi.config.sendInterval,
       )
+      activeApi.canHandleValues = application.canHandleValues
+      activeApi.canHandleFiles = application.canHandleFiles
+
+      if (application.canHandleValues) {
+        activeApi.database = this.createDatabase(`${activeApi.applicationId}.db`)
+      }
+      if (application.canHandleFiles) {
+        activeApi.storageFolder = this.createStorageFolder(activeApi.applicationId)
+      }
 
       this.activeApis[activeApi.applicationId] = activeApi
     })
@@ -78,6 +88,21 @@ class Cache {
   }
 
   /**
+   * Create storage directory.
+   * @param {string} directoryName - The directory name
+   * @return {string} - The storage folder
+   */
+  createStorageFolder(directoryName) {
+    const directoryPath = path.join(this.cacheFolder, directoryName)
+
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true })
+    }
+
+    return directoryPath
+  }
+
+  /**
    * Cache a new Value.
    * It will store the value in every database. If doNotCache is "true" it will immediately forward the value
    * to every North application.
@@ -90,33 +115,75 @@ class Cache {
    */
   cacheValues(value, doNotGroup) {
     Object.keys(this.activeApis).forEach((applicationId) => {
-      const { database, config } = this.activeApis[applicationId]
+      const { database, config, canHandleValues } = this.activeApis[applicationId]
 
-      database.serialize(() => {
-        const insertQuery = `INSERT INTO ${this.CACHE_TABLE_NAME} (timestamp, data, point_id) 
+      if (canHandleValues) {
+        database.serialize(() => {
+          const insertQuery = `INSERT INTO ${this.CACHE_TABLE_NAME} (timestamp, data, point_id) 
                              VALUES (?, ?, ?)`
-        const entries = [value.timestamp, encodeURI(value.data), value.pointId]
-        database.run(insertQuery, entries, (error) => {
-          if (error) {
-            this.logger.error(error)
-          }
-        })
-
-        if (doNotGroup) {
-          this.sendValues(applicationId, [value])
-        } else {
-          const countQuery = `SELECT COUNT(*) AS nr_entries
-                              FROM ${this.CACHE_TABLE_NAME}`
-          database.get(countQuery, (error, result) => {
-            if (!error) {
-              if (result.nr_entries >= config.groupCount) {
-                this.sendCallback(applicationId)
-              }
+          const entries = [value.timestamp, encodeURI(value.data), value.pointId]
+          database.run(insertQuery, entries, (error) => {
+            if (error) {
+              this.logger.error(error)
             }
           })
+
+          if (doNotGroup) {
+            this.sendValues(applicationId, [value])
+          } else {
+            const countQuery = `SELECT COUNT(*) AS nr_entries
+                                FROM ${this.CACHE_TABLE_NAME}`
+            database.get(countQuery, (error, result) => {
+              if (!error) {
+                if (result.nr_entries >= config.groupCount) {
+                  this.sendCallback(applicationId)
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  }
+
+  /**
+   * Copy file from source path to destination folder.
+   * @param {String} filePath - The source path
+   * @param {String} storageFolder - The destination folder
+   * @return {Promise} - The copy status
+   */
+  copyFile(filePath, storageFolder) {
+    return new Promise((resolve) => {
+      const cacheFilename = `${path.parse(filePath).name}-${new Date().getTime()}${path.parse(filePath).ext}`
+      const cachePath = path.join(storageFolder, cacheFilename)
+
+      fs.copyFile(filePath, cachePath, (error) => {
+        if (error) {
+          this.logger.error(error)
         }
+
+        resolve()
       })
     })
+  }
+
+  /**
+   * Cache the new raw file.
+   * @param {String} filePath - The path of the raw file
+   * @return {Promise} - The cache status
+   */
+  cacheFile(filePath) {
+    const actions = []
+
+    Object.keys(this.activeApis).forEach((applicationId) => {
+      const { storageFolder, canHandleFiles } = this.activeApis[applicationId]
+
+      if (canHandleFiles) {
+        actions.push(this.copyFile(filePath, storageFolder))
+      }
+    })
+
+    return Promise.all(actions)
   }
 
   /**
@@ -158,14 +225,82 @@ class Cache {
   }
 
   /**
+   * Get file to send to a given North application.
+   * @param {string} applicationId - The application ID
+   * @return {Promise} - The file read status
+   */
+  getFileToSend(applicationId) {
+    return new Promise((resolve, reject) => {
+      const { storageFolder } = this.activeApis[applicationId]
+
+      fs.readdir(storageFolder, (error, files) => {
+        if (error) {
+          reject(error)
+        }
+
+        const orderedFiles = files.map((fileName) => {
+          const file = {
+            name: fileName,
+            time: fs.statSync(path.join(storageFolder, fileName)).mtime.getTime(),
+          }
+
+          return file
+        })
+          .sort((a, b) => (a.time - b.time))
+          .map(file => file.name)
+
+        if (orderedFiles.length > 0) {
+          resolve(path.join(storageFolder, orderedFiles[0]))
+        } else {
+          resolve(null)
+        }
+      })
+    })
+  }
+
+  /**
+   * Delete sent file from the cache for a given North application.
+   * @param {string} applicationId - The application ID
+   * @param {String} filePath - The file to delete from the cache
+   * @return {void}
+   */
+  deleteSentFile(applicationId, filePath) {
+    // Delete original file
+    fs.unlink(filePath, (error) => {
+      if (error) {
+        this.logger.error(error)
+      } else {
+        this.logger.info(`File: ${filePath} deleted.`)
+      }
+    })
+  }
+
+  /**
    * Callback function used by the timer to send the values to the given North application.
    * @param {string} applicationId - The application ID
    * @return {void}
    */
   sendCallback(applicationId) {
-    let timeout = this.activeApis[applicationId].config.sendInterval
+    const application = this.activeApis[applicationId]
 
-    this.getValuesToSend(applicationId)
+    if (application.canHandleValues) {
+      this.handleValues(application)
+    }
+
+    if (application.canHandleFiles) {
+      this.handleFiles(application)
+    }
+  }
+
+  /**
+   * Handle value resending.
+   * @param {object} application - The application to send the values to
+   * @return {void}
+   */
+  handleValues(application) {
+    let timeout = application.config.sendInterval
+
+    this.getValuesToSend(application.applicationId)
       .then((values) => {
         if (values.length > 0) {
           // Decode values
@@ -175,27 +310,70 @@ class Cache {
           })
 
           // Send the values
-          this.engine.sendValues(applicationId, decodedValues)
+          this.engine.sendValues(application.applicationId, decodedValues)
             .then((success) => {
               if (success) {
-                this.removeSentValues(applicationId, values)
+                this.removeSentValues(application.applicationId, values)
               } else {
-                timeout = this.activeApis[applicationId].config.retryInterval
+                timeout = application.config.retryInterval
               }
             })
         }
 
         // Re-activate the send timer
-        this.activeApis[applicationId].timeout = setTimeout(
-          this.sendCallback.bind(this, applicationId),
+        application.timeout = setTimeout(
+          this.sendCallback.bind(this, application.applicationId),
           timeout,
         )
       })
       .catch((error) => {
         this.logger.error(error)
 
-        this.activeApis[applicationId].timeout = setTimeout(
-          this.sendCallback.bind(this, applicationId),
+        application.timeout = setTimeout(
+          this.sendCallback.bind(this, application.applicationId),
+          timeout,
+        )
+      })
+  }
+
+  /**
+   * Handle files resending.
+   * @param {object} application - The application to send the values to
+   * @return {void}
+   */
+  handleFiles(application) {
+    let timeout = application.config.sendInterval
+
+    this.getFileToSend(application.applicationId)
+      .then((filePath) => {
+        if (filePath) {
+          // Send the file
+          this.engine.sendFile(application.applicationId, filePath)
+            .then((success) => {
+              if (success) {
+                this.deleteSentFile(application.applicationId, filePath)
+              } else {
+                timeout = application.config.retryInterval
+              }
+
+              // Re-activate the send timer
+              application.timeout = setTimeout(
+                this.sendCallback.bind(this, application.applicationId),
+                timeout,
+              )
+            })
+        } else {
+          application.timeout = setTimeout(
+            this.sendCallback.bind(this, application.applicationId),
+            timeout,
+          )
+        }
+      })
+      .catch((error) => {
+        this.logger.error(error)
+
+        application.timeout = setTimeout(
+          this.sendCallback.bind(this, application.applicationId),
           timeout,
         )
       })
