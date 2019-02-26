311875d3f35b549fe332f1a86d886ab9a5b75507
jfh <it@factorythings.com>
2019-02-26 11:25:45 -0600

    setup react

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,16 @@
 const Router = require('koa-router')
+const koaSend = require('koa-send')
 
 const indexController = require('../controllers/indexController')
 const infoController = require('../controllers/infoController')
 
 const router = new Router()
 
+// route for client bundle (static files)
+router.get('/index.html', async ctx => koaSend(ctx, ctx.path, { root: `${__dirname}/../../../build` }))
+router.get('/bundle.js', async ctx => koaSend(ctx, ctx.path, { root: `${__dirname}/../../../build` }))
+router.get('/bundle.js.map', async ctx => koaSend(ctx, ctx.path, { root: `${__dirname}/../../../build` }))
+// route for API server
 router.get('/', indexController.getIndex)
 router.get('/infos', infoController.getInfo)
 
