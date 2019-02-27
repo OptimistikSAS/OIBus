66efe1d04cdaaa6cb3b1b892c80b70d2577d90ba
jfh <it@factorythings.com>
2019-02-27 12:06:35 -0600

    improve routing

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './style/main.less'
 
 import TopHeader from './TopHeader.jsx'
+import Welcome from './Welcome.jsx'
 import NotFound from './NotFound.jsx'
 import South from './South.jsx'
 import North from './North.jsx'
@@ -13,7 +14,7 @@ import Engine from './Engine.jsx'
 const Main = () => {
   const [configJson, setConfigJson] = React.useState()
   React.useEffect(() => {
-    fetch('/infos').then((response) => {
+    fetch('/config').then((response) => {
       const contentType = response.headers.get('content-type')
       if (contentType && contentType.indexOf('application/json') !== -1) {
         return response.json().then(({ config }) => {
@@ -28,6 +29,7 @@ const Main = () => {
       <div>
         <TopHeader />
         <Switch>
+          <Route exact path="/" render={() => <Welcome />} />
           <Route exact path="/engine" render={() => <Engine configJson={configJson} />} />
           <Route exact path="/south" render={() => <South configJson={configJson} />} />
           <Route exact path="/north" render={() => <North configJson={configJson} />} />
