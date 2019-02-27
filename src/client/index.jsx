79465d249e788d6e573b1b87078d9ed8c0f53ed2
jfh <it@factorythings.com>
2019-02-26 22:39:07 -0600

    add bootstrap and  less

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style/main.less'
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
+
+import 'bootstrap-less-port/less/bootstrap.less'
 
 const Welcome = () => {
   const [configJson, setConfigJson] = React.useState()
+  const [isOpen, setIsOpen] = React.useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   React.useEffect(() => {
     fetch('/infos').then((response) => {
       const contentType = response.headers.get('content-type')
@@ -17,6 +36,31 @@ const Welcome = () => {
   })
   return (
     <>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
       <h1 className="header">Hello</h1>
       <pre>{JSON.stringify(configJson, ' ', 2)}</pre>
     </>
