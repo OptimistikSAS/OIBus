6fb0bf1f21485fbe065788dc67940a9d7e8ac17b
jfh <it@factorythings.com>
2019-03-03 10:07:11 -0600

    complete view for Engine form

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Container } from 'reactstrap'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 import './style/main.less'
 
@@ -13,16 +15,18 @@ import Engine from '../engine/Engine.jsx'
 
 const Main = () => (
   <Router>
-    <div>
+    <>
       <TopHeader />
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/engine" component={Engine} />
-        <Route exact path="/south" component={South} />
-        <Route exact path="/north" component={North} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
+      <Container fluid>
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/engine" component={Engine} />
+          <Route exact path="/south" component={South} />
+          <Route exact path="/north" component={North} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+    </>
   </Router>
 )
 
