d340826ddde7b728fa7c38ece62c35d2fbafaae7
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-03-12 12:44:40 +0200

    Support reloading application using cluster

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,10 @@ const configController = require('../controllers/configController')
 const router = new Router()
 router.get('/config', configController.getConfig)
 
+router.get('/reload', (ctx) => {
+  ctx.ok('Reloading...')
+
+  process.exit(1)
+})
+
 module.exports = router
