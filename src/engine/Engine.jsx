f6ce67491c8191f9516ecdfeeaa220a6fa4f025b
jfh <it@factorythings.com>
2019-03-19 18:09:56 +0100

    improvements to validation

--- src/engine/Engine.jsx
+++ src/engine/Engine.jsx
@@ -19,6 +19,7 @@ const Engine = () => {
     <>
       <Form
         formData={configJson && configJson.engine}
+        liveValidate
         schema={Engine.schema}
         uiSchema={Engine.uiSchema}
         autocomplete="on"
@@ -38,15 +39,17 @@ Engine.schema = {
   type: 'object',
   required: ['port', 'user', 'password'],
   properties: {
-    port: { type: 'number', title: 'Port', default: 2223 },
+    port: { type: 'number', title: 'Port', default: 2223, minimum: 1, maximum: 65535 },
     user: { type: 'string', title: 'User', default: 'admin' },
     password: { type: 'string', title: 'Password', default: 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1' },
-    filter: { type: 'array', title: 'filter', items: { type: 'string' }, minItems: 1, uniqueItems: true, default: ['127.0.0.1'] },
+    filter: { type: 'array', title: 'filter', items: { type: 'string', "oneOf": [
+      {"format": "ipv4"},
+      {"format": "ipv6"} ]}, minItems: 1, uniqueItems: true, default: ['127.0.0.1'] },
     logParameters: {
       type: 'object',
       title: 'Log Parameters',
       properties: {
-        consoleLevel: { type: 'string', title: 'Console Level', default: 'debug' },
+        consoleLevel: { type: 'string', enum: ["debug", "info", "warning", "error"], title: 'Console Level', default: 'debug' },
         fileLevel: { type: 'string', title: 'File Level', default: 'debug' },
         filename: { type: 'string', title: 'Filename', default: './logs/journal.log' },
         maxsize: { type: 'number', title: 'Max Size', default: 1000000 },
@@ -85,7 +88,7 @@ Engine.schema = {
 }
 
 Engine.uiSchema = {
-  port: { 'ui:help': <div>the port for the web interface to fTbus</div> },
+  port: { 'ui:help': <div>the port to access the web interface to fTbus. Valid values range from 1 through 65535.</div> },
   password: { 'ui:widget': 'password' },
   filter: { 'ui:help': <div>the list of IP addresses allowed to access the Web interface</div> },
 }
