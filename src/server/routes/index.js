126c7f83d0674bb7e48174a4c1fe8bb238ee4210
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-03-20 13:44:59 +0200

    Fix serving the client

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,10 +2,13 @@ const Router = require('koa-router')
 
 const configController = require('../controllers/configController')
 const reloadController = require('../controllers/reloadController')
+const clientController = require('../controllers/clientController')
 
 const router = new Router()
 
 router.get('/config', configController.getConfig)
 router.get('/reload', reloadController.reload)
+
+router.get('/*', clientController.serveClient)
 
 module.exports = router
