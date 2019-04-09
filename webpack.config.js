760a1e5aea460cb17d130c772a2c9eb08bac85ce
Balazs Kendi <balazs.kendi@lynxsolutions.eu>
2019-04-09 17:39:14 +0300

    Add service layer to UI

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/client/index.jsx',
+  entry: ['babel-polyfill', './src/client/index.jsx'],
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
