cd0950b19bc7cdce2ffbaa3f8be8e305da1e2d0e
jfh <it@factorythings.com>
2019-04-14 21:37:34 +0200

    rearchitecture code

--- src/client/South.jsx
+++ src/client/South.jsx
@@ -1,99 +1,41 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import ReactJson from 'react-json-view'
 import ConfigService from './services/configService'
 import Table from './components/table/Table.jsx'
-import Select from './components/Select.jsx'
-import Input from './components/Input.jsx'
-
-const tableHeaders = ['Equipment ID', 'Enabled', 'Protocol']
+import NewEquipmentRow from './NewEquipmentRow.jsx'
 
 const South = ({ history }) => {
-  const [configJson, setConfigJson] = React.useState()
-  const [tableRows, setTableRows] = React.useState()
-  const [newRowData, setNewRowData] = React.useState({})
-  const [protocolList, setProtocolList] = React.useState()
-  const buttonRef = React.useRef()
+  const [equipments, setEquipments] = React.useState([])
+  const [protocolList, setProtocolList] = React.useState([])
 
   /**
    * Sets the content of the table
    * @param {object} config The configuration json
    * @returns {void} no return value
    */
-  const createTableRows = (config) => {
-    const array = []
-    config.equipments.forEach((equipment) => {
-      const { equipmentId, enabled, protocol } = equipment
-      array.push([equipmentId, enabled.toString(), protocol])
-    })
 
-    setTableRows(array)
-  }
-
+  // acquire the South configuration
   React.useEffect(() => {
     ConfigService.getConfig().then(({ config }) => {
-      setConfigJson(config.south)
-      createTableRows(config.south)
+      setEquipments(config.south.equipments)
     })
   }, [])
 
+  // acquire the list of Protocols
   React.useEffect(() => {
     ConfigService.getSouthProtocols().then((protocols) => {
       setProtocolList(protocols)
-      setNewRowData({ equipmentId: '', enabled: false, protocol: protocols[0] })
     })
   }, [])
 
   /**
-   * Gets the config json of a south equipment
+   * Gets the json of a south equipment
    * @param {string} equipmentId ID of an equipment
    * @returns {object} The selected equipment's config
    */
-  const getEquipmentData = (equipmentId) => {
-    let formData = {}
-    configJson.equipments.forEach((equipment) => {
-      if (equipment.equipmentId === equipmentId) {
-        formData = equipment
-      }
-    })
-
-    return formData
-  }
-
-  /**
-   * Update the equipment's state if any value changed
-   * @param {object} event The event object of the change
-   * @returns {void}
-   */
-  const handleChange = (event) => {
-    const {
-      target,
-      target: { type, name },
-    } = event
-    let value
-
-    //  set the value field based on the event's type
-    switch (type) {
-      case 'checkbox':
-        value = target.checked
-        break
-      default:
-        ({ value } = target)
-        break
-    }
-
-    //  update the new equipment's state
-    setNewRowData(prevState => ({ ...prevState, [name]: value }))
-  }
-
-  /**
-   * @returns {array} An array with the needed components
-   */
-  const createNewRow = () => [
-    <Input name="equipmentId" type="text" onChange={handleChange} />,
-    <Input name="enabled" type="checkbox" onChange={handleChange} />,
-    <Select name="protocol" options={protocolList} onChange={handleChange} />,
-  ]
+  const getEquipmentIndex = equipmentId => equipments.findIndex(equipment => equipment.equipmentId === equipmentId)
 
   /**
    * Handles the click of the table rows and redirects the
@@ -103,38 +45,29 @@ const South = ({ history }) => {
    */
   const handleRowClick = (equipment) => {
     const [equipmentId] = equipment
-    let formData
-    if (typeof equipmentId === 'string') {
-      formData = getEquipmentData(equipmentId)
-    } else {
-      formData = { ...newRowData }
-    }
-
-    // return if no id is provided
-    if (formData.equipmentId.length < 1) return
+    const equipmentIndex = getEquipmentIndex(equipmentId)
+    if (equipmentIndex === -1) return
+    const formData = equipments[equipmentIndex]
     const link = `/south/${formData.protocol}`
     history.push({ pathname: link, formData })
   }
 
-  /**
-   * A new row is added and the button will become disabled
-   * @returns {void}
-   */
-  const handleButtonClick = () => {
-    const newRows = [...tableRows]
-    const newRow = createNewRow()
-    newRows.push(newRow)
-    setTableRows(newRows)
-    buttonRef.current.setAttribute('disabled', 'disabled')
+  const addEquipment = ({ equipmentId, enabled, protocol }) => {
+    const equipmentIndex = getEquipmentIndex(equipmentId)
+    if (equipmentIndex === -1) {
+      setEquipments(prev => [...prev, { equipmentId, enabled, protocol }])
+    } else {
+      throw new Error('equipmentId already exists')
+    }
   }
 
+  const tableHeaders = ['Equipment ID', 'Enabled', 'Protocol']
+  const tableRows = equipments.map(({ equipmentId, enabled, protocol }) => [equipmentId, enabled ? 'enabled' : '', protocol])
   return (
     <>
       {tableRows && <Table headers={tableHeaders} rows={tableRows} onRowClick={handleRowClick} />}
-      <button ref={buttonRef} type="button" className="btn btn-primary" onClick={handleButtonClick}>
-        Add equipment
-      </button>
-      <pre>{configJson && JSON.stringify(configJson, ' ', 2)}</pre>
+      <NewEquipmentRow protocolList={protocolList} addEquipment={addEquipment} />
+      <ReactJson src={equipments} name={null} collapsed displayObjectSize={false} displayDataTypes={false} enableClipboard={false} />
     </>
   )
 }
