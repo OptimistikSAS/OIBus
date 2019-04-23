625c021521386912ad1c18bf51410bd38aaedb7c
Balazs Kendi <balazs.kendi@lynxsolutions.eu>
2019-04-23 10:38:23 +0300

    Add esdoc comments

--- src/client/NewApplicationRow.jsx
+++ src/client/NewApplicationRow.jsx
@@ -7,18 +7,28 @@ import apis from './services/apis'
 const NewApplicationRow = ({ apiList, addApplication }) => {
   const [application, setApplication] = React.useState({ applicationId: '', enable: false, api: 'Console' })
 
+  /**
+   * Updates the application's state
+   * @param {*} event The change event
+   * @returns {void}
+   */
   const handleChange = (event) => {
     const { target } = event
     const { value } = target
-    //  update the new equipment's state
+    //  update the new application's state
     setApplication(prevState => ({ ...prevState, [target.name]: value }))
   }
 
+  /**
+   * Submits a new application
+   * @returns {void}
+   */
   const handleAddApplication = async () => {
     if (application.applicationId === '') return
 
     try {
       await apis.addNorth(application)
+      // add submitted application to the table
       addApplication(application)
       // reset the line
       setApplication({ applicationId: '', enable: false, api: 'Console' })
