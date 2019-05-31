fe4c1e00e799f9db5123dade0372dd20188ca4e9
jfh <it@factorythings.com>
2019-05-31 12:16:58 +0200

    improve error logging

--- src/index.js
+++ src/index.js
@@ -6,22 +6,24 @@ const { parseArgs, checkOrCreateConfigFile } = require('./services/config.servic
 const Engine = require('./engine/Engine.class')
 
 if (cluster.isMaster) {
+  // Master role is nothing except launching a worker and relauching another
+  // one if exit is detected (typically to load a new configuration)
   console.info(`Starting OIBus version: ${VERSION}`)
-  // Fork a worker
   cluster.fork()
 
   cluster.on('exit', (worker, code, signal) => {
     if (signal) {
       console.info(`Worker ${worker.process.pid} was killed by signal: ${signal}`)
     } else {
-      console.info(`Worker ${worker.process.pid} exited with error code: ${code}`)
+      console.error(`Worker ${worker.process.pid} exited with error code: ${code}`)
     }
 
     cluster.fork()
   })
 } else {
+  // this condition is reached only for a worker (i.e. not master)
+  // so this is here where we execute the OIBus Engine
   const args = parseArgs() || {} // Arguments of the command
-
   const { config = './oibus.json' } = args // Get the configuration file path
 
   checkOrCreateConfigFile(config) // Create default config file if it doesn't exist
