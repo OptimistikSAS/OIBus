35b11a7b810b2e022cfd5150781c51b8f6825f79
jfh <it@factorythings.com>
2019-07-09 00:56:42 +0200

    changed in the logic

--- src/south/MQTT/MQTT.class.js
+++ src/south/MQTT/MQTT.class.js
@@ -9,6 +9,7 @@ class MQTT extends ProtocolHandler {
   connect() {
     super.connect()
     this.listen()
+    this.topics = {}
   }
 
   /**
@@ -17,16 +18,18 @@ class MQTT extends ProtocolHandler {
    */
   listen() {
     const { mqttProtocol, server, port, username, password, points } = this.dataSource
-    this.client = mqtt.connect(
-      `${mqttProtocol}://${server}`,
-      { port, username, password: Buffer.from(this.decryptPassword(password)) },
-    )
+    this.client = mqtt.connect(`${mqttProtocol}://${server}`, { port, username, password: Buffer.from(this.decryptPassword(password)) })
     this.client.on('error', (error) => {
       this.logger.error(error)
     })
-    points.forEach((point) => {
-      const { topic, pointId, doNotGroup = false } = point
-      this.client.on('connect', () => {
+    // JFH. review the logic?
+    // 1/ on connect forEachPoint subscribe to all the topics and fill pointIds[topic]
+    // 2/ on message update pointIds[topic] with message
+
+    this.client.on('connect', () => {
+      points.forEach((point) => {
+        const { topic, pointId, doNotGroup = false } = point
+        this.topics[topic] = { pointId, doNotGroup }
         this.client.subscribe(topic, (error) => {
           if (error) {
             this.logger.error(error)
@@ -34,18 +37,17 @@ class MQTT extends ProtocolHandler {
         })
       })
 
-      this.client.on('message', (topic1, message) => {
-        if (topic1 === topic) {
-          // message is Buffer
-          this.addValue(
-            {
-              data: message.toString(),
-              timestamp: new Date().getTime(),
-              pointId,
-            },
-            doNotGroup,
-          )
-        }
+      this.client.on('message', (topic, message) => {
+        console.log(topic.toString(), message.toString())
+
+        this.addValue(
+          {
+            data: message.toString(),
+            timestamp: new Date().getTime(),
+            pointId: this.topics[topic].pointId,
+          },
+          this.topics[topic].doNotGroup,
+        )
       })
     })
   }
