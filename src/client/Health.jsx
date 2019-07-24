3944bcba93807ddbdd6cd90d052905fd353b8e62
Attila Suto <attila.suto@lynxsolutions.eu>
2019-07-24 14:57:50 +0300

    Data from status request added to health page.

--- src/client/Health.jsx
+++ src/client/Health.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { Label } from 'reactstrap'
-
-import packageJson from '../../package.json'
+import Table from './components/table/Table.jsx'
+import apis from './services/apis'
 
 const Health = () => {
+  const [status, setStatus] = React.useState([])
+
+  /**
+   * Acquire the status
+   * @returns {void}
+   */
   React.useEffect(() => {
+    apis.getStatus().then((response) => {
+      setStatus(response)
+    })
   }, [])
+
+  const tableRows = Object.keys(status).map((key) => (
+    [
+      {
+        name: 'name',
+        value: key,
+      },
+      {
+        name: 'value',
+        value: status[key],
+      },
+    ]
+  ))
 
   return (
     <div>
-      <Label>{`Version ${packageJson.version}`}</Label>
+      <Label>Health status</Label>
+      {tableRows && <Table headers={[]} rows={tableRows} onRowClick={() => null} />}
     </div>
   )
 }
