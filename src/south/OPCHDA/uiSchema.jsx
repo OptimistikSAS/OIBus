241cb5bfe4414323c323544ea72d7d0cbefc13d9
jfh <it@factorythings.com>
2019-07-24 09:26:25 +0200

    Update uiSchema.jsx

--- src/south/OPCHDA/uiSchema.jsx
+++ src/south/OPCHDA/uiSchema.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const uiSchema = {
   dataSourceId: { 'ui:help': <div>Unique name for this OPCHDA Source</div> },
-  enabled: { 'ui:help': <div>If enabled, the data source will be enabled</div> },
+  enabled: { 'ui:help': <div>Enable this data source</div> },
   protocol: { 'ui:readonly': true },
   agentFilename: { 'ui:help': <div>path to the HDA Agent</div> },
-  tcpPort: { 'ui:help': <div>TCP Port of the HDA Agent</div> },
+  tcpPort: { 'ui:help': <div>TCP Port of the HDA Agent executable</div> },
   host: { 'ui:help': <div>IP address or hostname of the HDA server</div> },
   serverName: { 'ui:help': <div>Name of the HDA server</div> },
   points: {
@@ -17,8 +17,8 @@ const uiSchema = {
   scanGroups: {
     items: {
       scanMode: { 'ui:help': 'Name of a valid scan mode (see Engine tab)' },
-      aggregate: { 'ui:help': 'if the scan mode is using raw value or an aggregate' },
-      resampling: { 'ui:help': 'if an aggregate is chose, choose the aggregate period' },
+      aggregate: { 'ui:help': 'Choose if the query will use raw values or aggregates' },
+      resampling: { 'ui:help': 'if an aggregate is chosen, select the aggregate period' },
     },
   },
 }
