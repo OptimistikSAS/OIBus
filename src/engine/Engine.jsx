e0d047108e232e9ccddb5001cf219d4a2557ead0
jfh <it@factorythings.com>
2019-07-30 20:00:07 +0200

    in progress

--- src/engine/Engine.jsx
+++ src/engine/Engine.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Col, Row, Button, Form, FormGroup, Label, Input, FormText, FormFeedback } from 'reactstrap'
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import { EngineContext } from '../client/context/configContext.jsx'
 import { AlertContext } from '../client/context/AlertContext.js'
+import OIbInput from '../client/components/OIbForm/OIbInput.jsx'
 
 const Engine = () => {
   // const [configJson, setConfigJson] = React.useState()
-  const [validate, setValidate] = React.useState()
+  // const [validate, setValidate] = React.useState()
   const { state: configState /* , dispatch: configDispatch */ } = React.useContext(EngineContext)
 
   const { setAlert } = React.useContext(AlertContext)
@@ -31,13 +32,13 @@ const Engine = () => {
     // eslint-disable-next-line max-len
     const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (emailRex.test(e.target.value)) {
-      setValidate('has-success')
+      // setValidate('has-success')
     } else {
-      setValidate('has-danger')
+      // setValidate('has-danger')
     }
   }
 
-  const handleChange = (event) => {
+  const onChange = (event) => {
     validateEmail(event)
     const { target } = event
     const value = target.type === 'checkbox' ? target.checked : target.value
@@ -57,26 +58,14 @@ const Engine = () => {
     <>
       <Form>
         <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="exampleEmail"
-                placeholder="with a placeholder"
-                invalid={validate === 'has-danger'}
-                onChange={handleChange}
-              />
-              <FormFeedback>Oh noes! that name is not good</FormFeedback>
-              <FormText>Example help text.</FormText>
-            </FormGroup>
+          <Col md={4}>
+            <OIbInput config={Engine.port} onChange={onChange} />
           </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-            </FormGroup>
+          <Col md={4}>
+            <OIbInput config={Engine.port} onChange={onChange} />
+          </Col>
+          <Col md={4}>
+            <OIbInput config={Engine.port} onChange={onChange} />
           </Col>
         </Row>
         <FormGroup>
@@ -123,118 +112,118 @@ const Engine = () => {
 export default Engine
 
 Engine.schema = {
-  title: 'Engine',
-  type: 'object',
-  required: ['port', 'user', 'password'],
-  properties: {
-    port: { type: 'number', title: 'Port', default: 2223, minimum: 1, maximum: 65535 },
-    user: { type: 'string', title: 'User', default: 'admin' },
-    password: {
-      type: 'string',
-      title: 'Password',
-      default: 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1',
+  port: {
+    type: 'port', // => integer, min 1, max 65535
+    title: 'Port',
+    default: 2223,
+    help: <div>The port to access the Admin interface</div>,
+  },
+  user: { type: 'string', title: 'User', default: 'admin' },
+  password: {
+    type: 'string',
+    title: 'Password',
+    default: 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1',
+  },
+  filter: {
+    type: 'array',
+    title: 'Network Filter',
+    items: { type: 'string' },
+    minItems: 1,
+    uniqueItems: true,
+    default: ['127.0.0.1'],
+  },
+  logParameters: {
+    type: 'object',
+    title: 'Log Parameters',
+    properties: {
+      consoleLevel: {
+        type: 'string',
+        enum: ['silly', 'debug', 'info', 'warning', 'error'],
+        title: 'Console Level',
+        default: 'debug',
+      },
+      fileLevel: {
+        type: 'string',
+        enum: ['silly', 'debug', 'info', 'warning', 'error'],
+        title: 'File Level',
+        default: 'debug',
+      },
+      filename: { type: 'string', title: 'Filename', default: './logs/journal.log' },
+      maxsize: { type: 'number', title: 'Max Size (Byte)', default: 1000000 },
+      maxFiles: { type: 'number', title: 'Max Files', default: 5 },
+      tailable: { type: 'boolean', title: 'Tailable', default: true },
+      sqliteLevel: {
+        type: 'string',
+        enum: ['silly', 'debug', 'info', 'warning', 'error'],
+        title: 'SQLite logging Level',
+        default: 'debug',
+      },
+      sqliteFilename: { type: 'string', title: 'Filename', default: './logs/journal.db' },
+      sqliteMaxFileSize: {
+        type: 'number',
+        title: 'Max File Size When To Start Deleting Old Log Records (Byte)',
+        default: 5000000,
+      },
     },
-    filter: {
-      type: 'array',
-      title: 'Network Filter',
-      items: { type: 'string' },
-      minItems: 1,
-      uniqueItems: true,
-      default: ['127.0.0.1'],
+  },
+  caching: {
+    type: 'object',
+    title: 'Cache Parameters',
+    properties: {
+      cacheFolder: { type: 'string', title: 'Cache Folder', default: './cache' },
+      archiveFolder: { type: 'string', title: 'Archive Folder', default: './cache/archived/' },
+      archiveMode: { type: 'string', enum: ['archive', 'delete'], title: 'Archive Mode', default: 'archive' },
     },
-    logParameters: {
+  },
+  proxies: {
+    type: 'array',
+    title: 'Proxy Parameters',
+    items: {
       type: 'object',
-      title: 'Log Parameters',
       properties: {
-        consoleLevel: {
+        name: {
           type: 'string',
-          enum: ['silly', 'debug', 'info', 'warning', 'error'],
-          title: 'Console Level',
-          default: 'debug',
+          title: 'Name',
         },
-        fileLevel: {
+        protocol: {
           type: 'string',
-          enum: ['silly', 'debug', 'info', 'warning', 'error'],
-          title: 'File Level',
-          default: 'debug',
+          enum: ['http', 'https'],
+          title: 'Protocol',
+          default: 'http',
         },
-        filename: { type: 'string', title: 'Filename', default: './logs/journal.log' },
-        maxsize: { type: 'number', title: 'Max Size (Byte)', default: 1000000 },
-        maxFiles: { type: 'number', title: 'Max Files', default: 5 },
-        tailable: { type: 'boolean', title: 'Tailable', default: true },
-        sqliteLevel: {
+        host: {
           type: 'string',
-          enum: ['silly', 'debug', 'info', 'warning', 'error'],
-          title: 'SQLite logging Level',
-          default: 'debug',
+          title: 'Host',
         },
-        sqliteFilename: { type: 'string', title: 'Filename', default: './logs/journal.db' },
-        sqliteMaxFileSize: {
+        port: {
           type: 'number',
-          title: 'Max File Size When To Start Deleting Old Log Records (Byte)',
-          default: 5000000,
+          title: 'Port',
+        },
+        username: {
+          type: 'string',
+          title: 'Username',
+        },
+        password: {
+          type: 'string',
+          title: 'Password',
         },
       },
     },
-    caching: {
+  },
+  scanModes: {
+    type: 'array',
+    title: 'Scan Modes',
+    items: {
       type: 'object',
-      title: 'Cache Parameters',
+      required: ['scanMode', 'cronTime'],
       properties: {
-        cacheFolder: { type: 'string', title: 'Cache Folder', default: './cache' },
-        archiveFolder: { type: 'string', title: 'Archive Folder', default: './cache/archived/' },
-        archiveMode: { type: 'string', enum: ['archive', 'delete'], title: 'Archive Mode', default: 'archive' },
-      },
-    },
-    proxies: {
-      type: 'array',
-      title: 'Proxy Parameters',
-      items: {
-        type: 'object',
-        properties: {
-          name: {
-            type: 'string',
-            title: 'Name',
-          },
-          protocol: {
-            type: 'string',
-            enum: ['http', 'https'],
-            title: 'Protocol',
-            default: 'http',
-          },
-          host: {
-            type: 'string',
-            title: 'Host',
-          },
-          port: {
-            type: 'number',
-            title: 'Port',
-          },
-          username: {
-            type: 'string',
-            title: 'Username',
-          },
-          password: {
-            type: 'string',
-            title: 'Password',
-          },
+        scanMode: {
+          type: 'string',
+          title: 'Scan Mode',
         },
-      },
-    },
-    scanModes: {
-      type: 'array',
-      title: 'Scan Modes',
-      items: {
-        type: 'object',
-        required: ['scanMode', 'cronTime'],
-        properties: {
-          scanMode: {
-            type: 'string',
-            title: 'Scan Mode',
-          },
-          cronTime: {
-            type: 'string',
-            title: 'Cron Time',
-          },
+        cronTime: {
+          type: 'string',
+          title: 'Cron Time',
         },
       },
     },
@@ -242,7 +231,6 @@ Engine.schema = {
 }
 
 Engine.uiSchema = {
-  port: { 'ui:help': <div>The port to access the Admin interface. Valid values range from 1 through 65535.</div> },
   password: { 'ui:widget': 'password' },
   filter: { 'ui:help': <div>The list of IP addresses or hostnames allowed to access the Admin interface</div> },
   logParameters: {
