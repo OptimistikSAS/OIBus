c411f37ef706cfd4330bdfa9be6e65a3e82f979a
jfh <it@factorythings.com>
2019-08-05 00:14:18 +0200

    read ip filters array in table

--- src/client/Engine/Filters.jsx
+++ src/client/Engine/Filters.jsx
@@ -2,22 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Row, Col } from 'reactstrap'
 import Table from '../components/table/Table.jsx'
+import { OIbText } from '../components/OIbForm'
+import { EngineContext } from '../context/configContext.jsx'
 
-
-const Filters = ({ filters }) => filters && (
-  <>
-    <h2>IP Filters</h2>
-    <Row>
-      <Col md={6}>
-        <Table
-          headers={['adresse']}
-          rows={filters.map((filter) => [{ name: 'filter', value: filter }])}
-          onRowClick={() => null}
-        />
-      </Col>
-    </Row>
-  </>
-)
+const Filters = ({ filters }) => {
+  const { configDispatch } = React.useContext(EngineContext)
+  const onChange = (name, value, validity) => {
+    console.info('dispatch:', name, value, validity)
+    configDispatch({ type: 'updateFilters', name, value, validity })
+  }
+  return (
+    filters && (
+      <>
+        <h2>IP Filters</h2>
+        <Row>
+          <Col md={6}>
+            <Table
+              headers={['#', 'adresse']}
+              rows={filters.map((filter, i) => [
+                {
+                  name: `key[${i}]`,
+                  value: i,
+                },
+                {
+                  name: `filter[${i}]`,
+                  value: (
+                    <OIbText
+                      name={`filter[${i}]`}
+                      value={filter}
+                      regExp={/^.{2,}$/} // i.e. min size = 2
+                      onChange={onChange}
+                    />
+                  ),
+                },
+              ])}
+              onRowClick={() => null}
+            />
+          </Col>
+        </Row>
+      </>
+    )
+  )
+}
 
 Filters.propTypes = { filters: PropTypes.arrayOf(String).isRequired }
 
