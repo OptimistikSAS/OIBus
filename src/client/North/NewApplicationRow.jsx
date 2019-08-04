6abda9391d830302b115714f8f21549986485fe0
jfh <it@factorythings.com>
2019-08-05 00:12:26 +0200

    simplify alertContext

--- src/client/North/NewApplicationRow.jsx
+++ src/client/North/NewApplicationRow.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
 import Select from '../components/Select.jsx'
 import apis from '../services/apis'
-import { AlertContext } from '../context/AlertContext'
+import { AlertContext } from '../context/AlertContext.jsx'
 
 const NewApplicationRow = ({ apiList, addApplication }) => {
   const [application, setApplication] = React.useState({ applicationId: '', enable: false, api: 'Console' })
