4c2aa6fc82199807537c928ad85a4c4ac97574b5
jfh <it@factorythings.com>
2019-08-05 19:19:25 +0200

    reorg Engine page

--- src/client/Engine/Caching.jsx
+++ src/client/Engine/Caching.jsx
@@ -8,16 +8,16 @@ const Caching = ({ onChange, caching }) => (
     <OIbTitle title="Cache parameters">
       <>
         <p>
-          In case a communication errors prevents OIBus to send information to a North application, The values will be
-          stored in a local cache (one cache is needed for each North application) and they will be retried regularly.
+          In case a communication errors prevents OIBus to send information to a North application, The values will be stored in a local cache (one
+          cache is needed for each North application) and they will be retried regularly.
         </p>
         <p>
-          When the communication is restored, all values in the cache will be forwarded to the North application. The
-          location of these caches on the OIBus server is indicated in the cache folder parameters.
+          When the communication is restored, all values in the cache will be forwarded to the North application. The location of these caches on the
+          OIBus server is indicated in the cache folder parameters.
         </p>
         <p>
-          When OIBus manage files, it is possible to set the Archive mode to ask to the cache to delete files one they
-          have been sent to the North application or to archive them in the Archive folder).
+          When OIBus manage files, it is possible to set the Archive mode to ask to the cache to delete files one they have been sent to the North
+          application or to archive them in the Archive folder).
         </p>
       </>
     </OIbTitle>
@@ -31,6 +31,8 @@ const Caching = ({ onChange, caching }) => (
           onChange={onChange}
         />
       </Col>
+    </Row>
+    <Row>
       <Col md={4}>
         <OIbText
           name="caching.archiveFolder"
@@ -40,7 +42,7 @@ const Caching = ({ onChange, caching }) => (
           onChange={onChange}
         />
       </Col>
-      <Col md={4}>
+      <Col md={2}>
         <OIbSelect
           label="Archive Mode"
           name="cachine.archiveMode"
