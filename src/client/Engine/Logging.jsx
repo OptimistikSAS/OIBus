5df95b4d277a871356d78c8580568bc912709aef
jfh <it@factorythings.com>
2019-08-05 19:19:25 +0200

    reorg Engine page

--- src/client/Engine/Logging.jsx
+++ src/client/Engine/Logging.jsx
@@ -14,20 +14,19 @@ const Logging = ({ logParameters, onChange }) => (
           <ul>
             <li>filename: The filename of the logfile to write output to.</li>
             <li>
-              maxsize: Max size in bytes of the logfile, if the size is exceeded then a new file is created, a counter
-              will become a suffix of the log file.
+              maxsize: Max size in bytes of the logfile, if the size is exceeded then a new file is created, a counter will become a suffix of the log
+              file.
             </li>
             <li>maxFiles: Limit the number of files created when the size of the logfile is exceeded.</li>
             <li>
-              tailable: If true, log files will be rolled based on maxsize and maxfiles, but in ascending order. The
-              filename will always have the most recent log lines. The larger the appended number, the older the log
-              file. This option requires maxFiles to be set, or it will be ignored.
+              tailable: If true, log files will be rolled based on maxsize and maxfiles, but in ascending order. The filename will always have the
+              most recent log lines. The larger the appended number, the older the log file. This option requires maxFiles to be set, or it will be
+              ignored.
             </li>
           </ul>
           <li>
-            The sqlite logs will be used to store logs on the chosen database of the server. This allows to see logs
-            remotely using the Logs menu. The maximum size can be defined so older message will be deleted
-            automatically.
+            The sqlite logs will be used to store logs on the chosen database of the server. This allows to see logs remotely using the Logs menu. The
+            maximum size can be defined so older message will be deleted automatically.
           </li>
         </ul>
         <p>the logging level are defined as below:</p>
@@ -39,8 +38,8 @@ const Logging = ({ logParameters, onChange }) => (
           <li>silly: informations useful to debug an issue but they will significantly impact performances</li>
         </ul>
         <p>
-          We recommend to use the info or warning levels in normal Mode and to put the debug or the silly mode only to
-          identify the origin of an issue.
+          We recommend to use the info or warning levels in normal Mode and to put the debug or the silly mode only to identify the origin of an
+          issue.
         </p>
       </>
     </OIbTitle>
@@ -86,7 +85,31 @@ const Logging = ({ logParameters, onChange }) => (
           onChange={onChange}
         />
       </Col>
-      <Col md={1} />
+      <Col md={2}>
+        <OIbInteger
+          name="maxsize"
+          label="File Max size"
+          value={logParameters.maxsize}
+          help={<div>Maximum size of file logs (Bytes)</div>}
+          onChange={onChange}
+        />
+      </Col>
+      <Col md={2}>
+        <OIbInteger
+          name="logParameters.maxFiles"
+          label="number of files"
+          value={logParameters.maxFiles}
+          min={1}
+          max={10}
+          help={<div>The number of journal files (rotating)</div>}
+          onChange={onChange}
+        />
+      </Col>
+      <Col md={3}>
+        <OIbCheckBox label="Tailable" name="logParameters.tailable" value={logParameters.tailable} onChange={onChange} />
+      </Col>
+    </Row>
+    <Row>
       <Col md={4}>
         <OIbText
           name="logParameters.sqliteFilename"
@@ -96,42 +119,12 @@ const Logging = ({ logParameters, onChange }) => (
           onChange={onChange}
         />
       </Col>
-    </Row>
-    <Row>
-      <Col md={3}>
-        <OIbInteger
-          name="maxsize"
-          label="Max size of the journal file"
-          value={logParameters.maxsize}
-          help={<div>Maximum size of file logs (Bytes)</div>}
-          onChange={onChange}
-        />
-      </Col>
-      <Col md={3}>
+      <Col md={2}>
         <OIbInteger
           name="logParameters.sqliteMaxFileSize"
-          label="Max size of sqlite db"
+          label="Db Max size"
           value={logParameters.sqliteMaxFileSize}
           help={<div>Max File Size of the sqlite database (Byte)</div>}
-          onChange={onChange}
-        />
-      </Col>
-      <Col md={3}>
-        <OIbInteger
-          name="logParameters.maxFiles"
-          label="number of journal files"
-          value={logParameters.maxFiles}
-          min={1}
-          max={10}
-          help={<div>The number of journal files (rotating)</div>}
-          onChange={onChange}
-        />
-      </Col>
-      <Col md={3}>
-        <OIbCheckBox
-          label="Tailable"
-          name="logParameters.tailable"
-          value={logParameters.tailable}
           onChange={onChange}
         />
       </Col>
