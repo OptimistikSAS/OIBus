16bd14c46be790aeb1eaa2eac6a4f928c5fa7297
jfh <it@factorythings.com>
2019-08-07 00:44:45 +0200

    improve Table to support actions (add and delete buttons)

--- src/client/components/table/TableRows.jsx
+++ src/client/components/table/TableRows.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { FaTrashAlt } from 'react-icons/fa'
+import { Button } from 'reactstrap'
 
-const TableRows = ({ rows, onRowClick }) => (
+const TableRows = ({ rows, onRowClick, actions }) => (
   <tbody>
     {rows.map((row) => (
       <tr key={row[0].name} onClick={() => onRowClick(row)}>
-        {row.map((field) => <td key={field.name}>{field.value}</td>)}
+        {row.map((field) => (
+          <td key={field.name}>{field.value}</td>
+        ))}
+        {actions && (
+          <td>
+            <Button close>
+              <FaTrashAlt className="oi-icon" />
+            </Button>
+          </td>
+        )}
       </tr>
     ))}
   </tbody>
@@ -14,6 +25,9 @@ const TableRows = ({ rows, onRowClick }) => (
 TableRows.propTypes = {
   rows: PropTypes.arrayOf(PropTypes.array).isRequired,
   onRowClick: PropTypes.func.isRequired,
+  actions: PropTypes.bool,
 }
+
+TableRows.defaultProps = { actions: false }
 
 export default TableRows
