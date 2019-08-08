54156d15b2e7882307a08840f48f5938cad01c30
jfh <it@factorythings.com>
2019-08-08 16:55:50 +0200

    implement a valid function

--- src/client/components/OIbForm/OIbInteger.jsx
+++ src/client/components/OIbForm/OIbInteger.jsx
@@ -2,30 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormGroup, FormFeedback, FormText, Label, Input } from 'reactstrap'
 
-const OIbInteger = ({ label, help, min, max, value, name, onChange }) => {
-  const isValid = (val) => (max ? val <= max : true) && (min ? val >= min : true)
+const OIbInteger = ({ label, help, valid, value, name, onChange }) => {
   const handleChange = (event) => {
     const { target } = event
     const { value: newVal } = target
-    onChange(name, isValid(newVal) ? parseInt(newVal, 10) : newVal, isValid(newVal) ? '' : `incorrect entry for ${name}`)
+    onChange(name, valid(newVal) ? parseInt(newVal || 0, 10) : newVal, valid(newVal))
   }
   // if no label, we are in a table so we need to minimize the row height
   const style = label ? null : { style: { marginBottom: 0 } }
+  const validCheck = valid ? valid(value) : null
   return (
     <FormGroup {...style}>
       {label && <Label for={name}>{label}</Label>}
-      <Input
-        className="oi-form-input"
-        type="integer"
-        min={min}
-        max={max}
-        id={name}
-        name={name}
-        invalid={!isValid(value)}
-        value={value}
-        onChange={handleChange}
-      />
-      <FormFeedback>Invalid Entry</FormFeedback>
+      <Input className="oi-form-input" type="integer" id={name} name={name} invalid={validCheck !== null} value={value} onChange={handleChange} />
+      <FormFeedback>{validCheck}</FormFeedback>
       {help && <FormText>{help}</FormText>}
     </FormGroup>
   )
@@ -36,12 +26,10 @@ OIbInteger.propTypes = {
   help: PropTypes.element,
   onChange: PropTypes.func.isRequired,
   value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-  min: PropTypes.number,
-  max: PropTypes.number,
+  valid: PropTypes.func,
 }
 OIbInteger.defaultProps = {
-  min: null,
-  max: null,
+  valid: null,
   label: null,
   help: null,
 }
