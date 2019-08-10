abced8019dd8122586e968f0d647746e05cf4d7f
jfh <it@factorythings.com>
2019-08-10 15:30:42 +0200

    create unique id not based on title

--- src/client/components/OIbForm/OIbTitle.jsx
+++ src/client/components/OIbForm/OIbTitle.jsx
@@ -2,26 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { UncontrolledCollapse, Button, Row } from 'reactstrap'
 
-const Help = ({ title, children }) => (
-  <>
-    <Row>
-      <h3>
-        {title}
-        {children && (
-          // remove spaces so it can used as an Id
-          <Button size="sm" color="link" id={title.replace(/ /g, '')}>
-            Help
-          </Button>
-        )}
-      </h3>
-    </Row>
-    {children && (
-      <UncontrolledCollapse toggler={title.replace(/ /g, '')}>
-        <Row className="oi-help">{children}</Row>
-      </UncontrolledCollapse>
-    )}
-  </>
-)
+const Help = ({ title, children }) => {
+  const id = `id${Math.random()
+    .toString(36)
+    .substr(2, 9)}` // generate a unique id
+  return (
+    <>
+      <Row>
+        <h3>
+          {title}
+          {children && (
+            // remove spaces so it can used as an Id
+            <Button size="sm" color="link" id={id}>
+              Help
+            </Button>
+          )}
+        </h3>
+      </Row>
+      {children && (
+        <UncontrolledCollapse toggler={id}>
+          <Row className="oi-help">{children}</Row>
+        </UncontrolledCollapse>
+      )}
+    </>
+  )
+}
 Help.propTypes = { children: PropTypes.element, title: PropTypes.string.isRequired }
 Help.defaultProps = { children: null }
 
