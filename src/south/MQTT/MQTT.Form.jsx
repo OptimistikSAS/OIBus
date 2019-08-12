3e0741408e3c2aca5b59c9486b870618950ee571
jfh <it@factorythings.com>
2019-08-12 17:53:31 +0200

    MQTT migration

--- src/south/MQTT/MQTT.Form.jsx
+++ src/south/MQTT/MQTT.Form.jsx
@@ -1,20 +1,4 @@
-import React from 'react'
-
-const uiSchema = {
-  dataSourceId: {
-    'ui:help': '',
-    'ui:readonly': true,
-  },
-  enabled: { 'ui:help': <div>If enabled, equipment will be enabled</div> },
-  protocol: { 'ui:help': '' },
-  server: { 'ui:help': '' },
-  mqttProtocol: { 'ui:help': '' },
-  port: { 'ui:help': '' },
-  username: { 'ui:help': '' },
-  password: {
-    'ui:help': '',
-    'ui:widget': 'password',
-  },
+/*
   points: {
     items: {
       pointId: { 'ui:help': '' },
@@ -22,6 +6,72 @@ const uiSchema = {
       topic: { 'ui:help': '' },
     },
   },
-}
+*/
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col } from 'reactstrap'
+import { OIbText, OIbInteger, OIbSelect, OIbPassword } from '../../client/components/OIbForm'
 
-export default uiSchema
+const MQTTForm = ({ dataSource, onChange }) => (
+  <>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="Server"
+          onChange={onChange}
+          value={dataSource.MQTT.server}
+          name="MQTT.server"
+          defaultValue=""
+          help={<div>IP address of the MQTT server</div>}
+        />
+      </Col>
+      <Col md="4">
+        <OIbInteger
+          label="Port"
+          onChange={onChange}
+          value={dataSource.MQTT.port}
+          name="MQTT.port"
+          defaultValue="8883"
+          help={<div>Port number of the MQTT server</div>}
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbSelect
+          label="MQTT protocol"
+          onChange={onChange}
+          options={['mqtt', 'mqtts']}
+          option={dataSource.MQTT.mqttProtocol}
+          defaultValue="mqtts"
+          name="MQTT.mqttProtocol"
+          help={<div>Protocol MQTT</div>}
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="User"
+          onChange={onChange}
+          value={dataSource.MQTT.username}
+          name="MQTT.username"
+          defaultValue=""
+        />
+      </Col>
+      <Col md="4">
+        <OIbPassword
+          label="Password"
+          onChange={onChange}
+          value={dataSource.MQTT.password}
+          name="MQTT.password"
+          defaultValue=""
+        />
+      </Col>
+    </Row>
+  </>
+)
+
+MQTTForm.propTypes = { dataSource: PropTypes.object.isRequired, onChange: PropTypes.func.isRequired }
+
+export default MQTTForm
