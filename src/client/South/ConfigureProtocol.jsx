792cdb791c52fc480bdda984e47809a5c35b6a56
jfh <it@factorythings.com>
2019-08-12 12:33:21 +0200

    move South to the new UI (in progress)

--- src/client/South/ConfigureProtocol.jsx
+++ src/client/South/ConfigureProtocol.jsx
@@ -1,117 +1,67 @@
 import React from 'react'
-import Form from 'react-jsonschema-form-bs4'
-import { withRouter } from 'react-router-dom'
-import { Button, Col } from 'reactstrap'
 import PropTypes from 'prop-types'
-import Modal from '../components/Modal.jsx'
-import apis from '../services/apis'
-import { AlertContext } from '../context/AlertContext.jsx'
+import { Button, Form, Col, Row } from 'reactstrap'
+import { OIbText, OIbSelect } from '../components/OIbForm/index.js'
 
-const ConfigureProtocol = ({ match, location }) => {
-  const [configJson, setConfigJson] = React.useState()
-  const [configSchema, setConfigSchema] = React.useState()
-  const { setAlert } = React.useContext(AlertContext)
-
+const NewDataSourceRow = ({ protocolList, addDataSource }) => {
+  const [dataSourceId, setDataSourceId] = React.useState('')
+  const [protocol, setProtocol] = React.useState(protocolList[0])
   /**
-   * Sets the configuration JSON
-   * @param {Object} formData Data of the form
+   * Updates the dataSource's state
+   * @param {*} event The change event
    * @returns {void}
    */
-  const updateForm = (formData) => {
-    setConfigJson(formData)
+  const handleAddDataSource = () => {
+    //  update the new application's state
+    if (dataSourceId === '') return
+    addDataSource({ dataSourceId, protocol })
   }
 
-  /**
-   * Acquire the schema and set the configuration JSON
-   * @returns {void}
-   */
-  React.useEffect(() => {
-    const { protocol } = match.params
-    const { formData } = location
-
-    apis.getSouthProtocolSchema(protocol).then((schema) => {
-      delete schema.properties.points
-      setConfigSchema(schema)
-      updateForm(formData)
-    }).catch((error) => {
-      console.error(error)
-      setAlert({ text: error.message, type: 'danger' })
-    })
-  }, [])
-
-  /**
-   * Handles the form's submittion
-   * @param {*} param0 Object containing formData field
-   * @returns {void}
-   */
-  const handleSubmit = ({ formData }) => {
-    const { dataSourceId } = formData
-    apis.updateSouth(dataSourceId, formData).catch((error) => {
-      console.error(error)
-      setAlert({ text: error.message, type: 'danger' })
-    })
-  }
-
-  /**
-   * Handles the form's change
-   * @param {Object} form The data of the form
-   * @returns {void}
-   */
-  const handleChange = (form) => {
-    const { formData } = form
-
-    updateForm(formData)
-    handleSubmit(form)
-  }
-
-  /**
-   * Shows the modal to delete application
-   * @returns {void}
-   */
-  const handleDelete = async () => {
-    const { dataSourceId } = configJson
-    try {
-      await apis.deleteSouth(dataSourceId)
-      // TODO: Show loader and redirect to main screen
-    } catch (error) {
-      console.error(error)
-      setAlert({ text: error.message, type: 'danger' })
+  const handleChange = (name, value) => {
+    switch (name) {
+      case 'dataSourceId':
+        setDataSourceId(value)
+        break
+      case 'protocol':
+      default:
+        setProtocol(value)
+        break
     }
   }
-
-  const log = (type) => console.info.bind(console, type)
   return (
-    <Col xs="12" md="6">
-      {configJson && configSchema && (
-        <>
-          <Form
-            formData={configJson}
-            liveValidate
-            showErrorList={false}
-            schema={configSchema}
-            // uiSchema={uiSchema(configJson.protocol)}
-            autocomplete="on"
+    <Form>
+      <Row>
+        <Col md="5">
+          <OIbText
+            label="New Data Source ID"
+            value={dataSourceId}
+            name="dataSourceId"
             onChange={handleChange}
-            onError={log('errors')}
-          >
-            <></>
-          </Form>
-          <Modal show={false} title="Delete data source" body="Are you sure you want to delete this data source?">
-            {(confirm) => (
-              <Button color="danger" onClick={confirm(handleDelete)}>
-                Delete
-              </Button>
-            )}
-          </Modal>
-        </>
-      )}
-    </Col>
+            defaultValue=""
+          />
+        </Col>
+        <Col md="3">
+          <OIbSelect
+            label="protocol"
+            option={protocol}
+            name="protocol"
+            options={protocolList}
+            defaultOption={protocolList[0]}
+            onChange={handleChange}
+          />
+        </Col>
+        <Col md="3">
+          <Button size="sm" className="oi-add-button" color="primary" onClick={() => handleAddDataSource()}>
+            Add
+          </Button>
+        </Col>
+      </Row>
+    </Form>
   )
 }
 
-ConfigureProtocol.propTypes = {
-  match: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired,
+NewDataSourceRow.propTypes = {
+  protocolList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  addDataSource: PropTypes.func.isRequired,
 }
-
-export default withRouter(ConfigureProtocol)
+export default NewDataSourceRow
