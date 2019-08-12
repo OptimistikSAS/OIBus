2eb85aa69928576577eaa4f3b8d5fe45b8b3798c
jfh <it@factorythings.com>
2019-08-12 11:12:12 +0200

    apply defaultValue to CheckBox

--- src/client/components/OIbForm/OIbCheckBox.jsx
+++ src/client/components/OIbForm/OIbCheckBox.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FormGroup, Label, Input } from 'reactstrap'
 
-const OIbCheckBox = ({ label, value, name, onChange }) => {
+const OIbCheckBox = ({ label, value, name, onChange, defaultValue }) => {
+  // if no value was found, load the context with the default value
+  // (this will cause a rerender with the correct value)
+  React.useLayoutEffect(() => {
+    if (!value) onChange(name, defaultValue)
+  }, [value])
   const handleChange = (event) => {
     const { target } = event
     const { checked } = target
     onChange(name, checked, null)
   }
-
+  if (value === null) return null
   return (
     <FormGroup>
       <Label>{label}</Label>
@@ -28,7 +33,10 @@ OIbCheckBox.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  value: PropTypes.bool.isRequired,
+  value: PropTypes.bool,
+  defaultValue: PropTypes.bool.isRequired,
 }
+
+OIbCheckBox.defaultProps = { value: null }
 
 export default OIbCheckBox
