487cdd593320ce6d98aea77e9d1c093eff481efb
jfh <it@factorythings.com>
2019-08-12 17:52:54 +0200

    Modbus migration

--- src/south/Modbus/Modbus.Form.jsx
+++ src/south/Modbus/Modbus.Form.jsx
@@ -1,22 +1,63 @@
-import React from 'react'
-
-const uiSchema = {
-  dataSourceId: {
-    'ui:help': '',
-    'ui:readonly': true,
-  },
-  enabled: { 'ui:help': <div>If enabled, equipment will be enabled</div> },
-  protocol: { 'ui:help': '' },
-  host: { 'ui:help': '' },
-  port: { 'ui:help': '' },
-  points: {
-    items: {
-      pointId: { 'ui:help': '' },
-      scanMode: { 'ui:help': <div>List of the scan modes defined by the user</div> },
-      address: { 'ui:help': '' },
-      type: { 'ui:help': '' },
+/*
+    points: {
+      type: 'array',
+      title: 'Points',
+      items: {
+        type: 'object',
+        properties: {
+          pointId: {
+            title: 'Point ID',
+            type: 'string',
+          },
+          scanMode: {
+            title: 'Scan Mode',
+            type: 'string',
+          },
+          address: {
+            type: 'string',
+            title: 'Address',
+          },
+          type: {
+            type: 'string',
+            title: 'Type',
+          },
+        },
+      },
     },
-  },
-}
+*/
 
-export default uiSchema
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col } from 'reactstrap'
+import { OIbText, OIbInteger } from '../../client/components/OIbForm'
+
+const ModbusForm = ({ dataSource, onChange }) => (
+  <>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="Host"
+          onChange={onChange}
+          value={dataSource.Modbus.host}
+          name="Modbus.host"
+          defaultValue=""
+          help={<div>IP address of the Modbus source</div>}
+        />
+      </Col>
+      <Col md="4">
+        <OIbInteger
+          label="Port"
+          onChange={onChange}
+          value={dataSource.Modbus.port}
+          name="Modbus.port"
+          defaultValue="502"
+          help={<div>Port number of the Modbus source</div>}
+        />
+      </Col>
+    </Row>
+  </>
+)
+
+ModbusForm.propTypes = { dataSource: PropTypes.object.isRequired, onChange: PropTypes.func.isRequired }
+
+export default ModbusForm
