6f60b885fc988c8b8be9f29feb779748a457c18e
jfh <it@factorythings.com>
2019-08-12 19:34:41 +0200

    SQLFile migration

--- src/south/SQLFile/SQLFile.Form.jsx
+++ src/south/SQLFile/SQLFile.Form.jsx
@@ -1,30 +1,92 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col } from 'reactstrap'
+import { OIbText, OIbInteger, OIbSelect, OIbPassword } from '../../client/components/OIbForm'
 
-const uiSchema = {
-  dataSourceId: {
-    'ui:help': '',
-    'ui:readonly': true,
-  },
-  enabled: { 'ui:help': <div>If enabled, equipment will be enabled</div> },
-  protocol: { 'ui:help': '' },
-  driver: { 'ui:help': '' },
-  host: { 'ui:help': '' },
-  port: { 'ui:help': '' },
-  username: { 'ui:help': '' },
-  password: {
-    'ui:help': '',
-    'ui:widget': 'password',
-  },
-  database: { 'ui:help': '' },
-  query: { 'ui:help': '' },
-  delimiter: { 'ui:help': '' },
-  tmpFolder: { 'ui:help': '' },
-  points: {
-    items: {
-      pointId: { 'ui:help': '' },
-      scanMode: { 'ui:help': <div>List of the scan modes defined by the user</div> },
-    },
-  },
-}
+const SQLFileForm = ({ dataSource, onChange }) => (
+  <>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="Host"
+          onChange={onChange}
+          value={dataSource.SQLFile.host}
+          name="SQLFile.host"
+          defaultValue="localhost"
+          help={<div>IP address of the OPC-UA server</div>}
+        />
+      </Col>
+      <Col md="4">
+        <OIbInteger
+          label="Port"
+          onChange={onChange}
+          value={dataSource.SQLFile.port}
+          name="SQLFile.port"
+          defaultValue="1433"
+          help={<div>Port number of the SQLFile server</div>}
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbSelect
+          label="Driver"
+          onChange={onChange}
+          options={['mssql', 'mysql', 'postgresql', 'oracle']}
+          option={dataSource.SQLFile.driver}
+          defaultValue="mssql"
+          name="SQLFile.driver"
+          help={<div>Driver SQL</div>}
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="User"
+          onChange={onChange}
+          value={dataSource.SQLFile.username}
+          name="SQLFile.username"
+          defaultValue=""
+        />
+      </Col>
+      <Col md="4">
+        <OIbPassword
+          label="Password"
+          onChange={onChange}
+          value={dataSource.SQLFile.password}
+          name="SQLFile.password"
+          defaultValue=""
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="Database"
+          onChange={onChange}
+          value={dataSource.SQLFile.database}
+          name="SQLFile.database"
+          defaultValue="db"
+          help={<div>Name of the SQL database</div>}
+        />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="4">
+        <OIbText
+          label="Query"
+          onChange={onChange}
+          value={dataSource.SQLFile.query}
+          name="SQLFile.query"
+          defaultValue=""
+          help={<div>SQL query</div>}
+        />
+      </Col>
+    </Row>
+  </>
+)
 
-export default uiSchema
+SQLFileForm.propTypes = { dataSource: PropTypes.object.isRequired, onChange: PropTypes.func.isRequired }
+
+export default SQLFileForm
