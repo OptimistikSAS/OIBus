b0ac544b76d3a25fbb5a5d52303da89874573374
jfh <it@factorythings.com>
2019-08-26 23:25:38 +0200

    Update PointsButton.jsx

--- src/client/South/PointsButton.jsx
+++ src/client/South/PointsButton.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from 'reactstrap'
 import { withRouter } from 'react-router-dom'
+import ProtocolForms from './Protocols.jsx'
 
 const PointsButton = ({ history, dataSource }) => {
   const handleEditPoints = () => {
     const link = `/south/${dataSource.dataSourceId}/points`
     history.push({ pathname: link })
   }
-  const { points, dataSourceId } = dataSource
-  return (
+  const { points, dataSourceId, protocol } = dataSource
+  return ProtocolForms[protocol].renderPoints !== null ? (
     <Button
       className="inline-button autosize oi-points-button"
       color={points && points.length ? 'success' : 'primary'}
@@ -17,7 +18,7 @@ const PointsButton = ({ history, dataSource }) => {
     >
       {`Points ${points ? `(${points.length})` : '(0)'}`}
     </Button>
-  )
+  ) : null
 }
 
 PointsButton.propTypes = { dataSource: PropTypes.object.isRequired, history: PropTypes.object.isRequired }
