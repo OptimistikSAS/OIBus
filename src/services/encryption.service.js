26ba337b8aeb4405aaa2695215f170cfcf34fe4f
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-08-26 11:59:40 +0300

    Issue #185: do not decrypt passwords and encrypt if {{notEncrypted}}

--- src/services/encryption.service.js
+++ src/services/encryption.service.js
@@ -92,7 +92,7 @@ const encryptSecrets = (configEntry, keyFolder) => {
     Object.entries(configEntry).forEach(([key, value]) => {
       if (typeof value === 'object') {
         encryptSecrets(value, keyFolder)
-      } else if (['password', 'secretKey'].includes(key)) {
+      } else if (['password', 'secretKey'].includes(key) && value.startsWith('{{notEncrypted}}')) {
         configEntry[key] = encryptText(value, keyFolder)
       }
     })
