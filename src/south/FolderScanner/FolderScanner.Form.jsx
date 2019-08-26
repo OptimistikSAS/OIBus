53688b676da89082cfffceca56a2dadfe7f92452
jfh <it@factorythings.com>
2019-08-26 23:27:23 +0200

    Update FolderScanner.Form.jsx

--- src/south/FolderScanner/FolderScanner.Form.jsx
+++ src/south/FolderScanner/FolderScanner.Form.jsx
@@ -54,42 +54,7 @@ const RawFileForm = ({ dataSource, onChange }) => (
 
 RawFileForm.propTypes = { dataSource: PropTypes.object.isRequired, onChange: PropTypes.func.isRequired }
 
-/**
- * The following keys will be used by the **ConfigurePoints** form to display the headers
- * and the rows that are specific for each protocol.
- * Note: alternatively, we can send a "fake" point array to get the headers
- * using: ProtocolForm.renderPoints([{}],()=>null)[0].map(el => el.value.props.title)
- * @returns {array} Headers for each column
- */
-RawFileForm.renderHeaders = () => ['Point Id', 'ScanMode']
-RawFileForm.renderPoints = (points, onChange) => {
-  const rows = points.map((point, index) => [
-    {
-      name: `points.${index}.pointId`,
-      value: (
-        <OIbText
-          title="Point Id"
-          name={`points.${index}.pointId`}
-          value={point.pointId}
-          onChange={onChange}
-          defaultValue=""
-        />
-      ),
-    },
-    {
-      name: `points.${index}.scanMode`,
-      value: (
-        <OIbText
-          title="ScanMode"
-          name={`points.${index}.scanMode`}
-          value={point.scanMode}
-          onChange={onChange}
-          defaultValue="everySecond"
-        />
-      ),
-    },
-  ])
-  return rows
-}
+/** this South Protocol is in "File" mode so we set renderPoints to null */
+RawFileForm.renderPoints = null
 
 export default RawFileForm
