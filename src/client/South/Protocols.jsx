0397b25dcb4022f438d4ac75fd448c223803751d
jfh <it@factorythings.com>
2019-08-26 10:40:43 +0200

    update package and fix bug after name changes

--- src/client/South/Protocols.jsx
+++ src/client/South/Protocols.jsx
@@ -3,9 +3,9 @@ import Modbus from '../../south/Modbus/Modbus.Form.jsx'
 import MQTT from '../../south/MQTT/MQTT.Form.jsx'
 import OPCHDA from '../../south/OPCHDA/OPCHDA.Form.jsx'
 import OPCUA from '../../south/OPCUA/OPCUA.Form.jsx'
-import RawFile from '../../south/FolderScanner/FolderScanner.Form.jsx'
-import SQLFile from '../../south/SQLDbToFile/SQLDbToFile.Form.jsx'
+import FolderScanner from '../../south/FolderScanner/FolderScanner.Form.jsx'
+import SQLDbToFile from '../../south/SQLDbToFile/SQLDbToFile.Form.jsx'
 
-const ProtocolForms = { CSV, Modbus, MQTT, OPCHDA, OPCUA, RawFile, SQLFile }
+const ProtocolForms = { CSV, Modbus, MQTT, OPCHDA, OPCUA, FolderScanner, SQLDbToFile }
 
 export default ProtocolForms
