82342a7da22a2d4b4d9dec31459542e2ba8d9db8
Attila Suto <attila.suto@lynxsolutions.eu>
2019-08-28 13:33:51 +0300

    Replaced OIbText with new OIbScanMode where needed.

--- src/south/Modbus/Modbus.Form.jsx
+++ src/south/Modbus/Modbus.Form.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Row, Col } from 'reactstrap'
-import { OIbText, OIbInteger } from '../../client/components/OIbForm'
+import { OIbText, OIbInteger, OIbScanMode } from '../../client/components/OIbForm'
 
 const ModbusForm = ({ dataSource, onChange }) => (
   <>
@@ -57,12 +57,10 @@ ModbusForm.renderPoints = (points, onChange) => {
     {
       name: `points.${index}.scanMode`,
       value: (
-        <OIbText
-          title="ScanMode"
+        <OIbScanMode
           name={`points.${index}.scanMode`}
-          value={point.scanMode}
+          option={point.scanMode}
           onChange={onChange}
-          defaultValue=""
         />
       ),
     },
