52605e3cb0f2d7c309753ff34c65bc1d8e113d12
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-09-16 18:26:42 +0300

    Issue #344: Use functions as migration rules

--- src/migration/migrationRules.js
+++ src/migration/migrationRules.js
@@ -1,10 +1,14 @@
 module.exports = {
-  '0.3.12': [
-    { action: 'renameProtocol', arg: { source: 'Link', target: 'OIConnect' }, comment: 'Rename Link into OILink' },
-  ],
-  '0.3.13': [
-    { action: 'renameField', arg: { sourceField: 'a.b.c', targetField: 'a.b.d' }, comment: 'Rename c to d' },
-  ],
-  '0.5.0': [],
-  '0.5.1': [],
+  '0.3.12': (config, fromVersion) => {
+    console.info(`Migrating from version ${fromVersion} to version 0.3.12`)
+  },
+  '0.3.13': (config, fromVersion) => {
+    console.info(`Migrating from version ${fromVersion} to version 0.3.13`)
+  },
+  '0.5.0': (config, fromVersion) => {
+    console.info(`Migrating from version ${fromVersion} to version 0.5.0`)
+  },
+  '0.5.1': (config, fromVersion) => {
+    console.info(`Migrating from version ${fromVersion} to version 0.5.1`)
+  },
 }
