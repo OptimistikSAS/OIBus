beb1708f27e17b522e091f370339a4ef1f5fac23
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-09-19 13:51:06 +0300

    Issue #344: leave an example rule and set required version to 1

--- src/migration/migrationRules.js
+++ src/migration/migrationRules.js
@@ -1,35 +1,21 @@
-module.exports = {
-  2: (config) => {
-    // Rename filename => logFilename
-    console.info('config.engine.logParameters.filename => config.engine.logParameters.logFilename')
-    config.engine.logParameters.logFilename = config.engine.logParameters.filename
-    delete config.engine.logParameters.filename
-  },
-  3: (config) => {
-    // Rename logFilename => filename
-    console.info('config.engine.logParameters.logFilename => config.engine.logParameters.filename')
-    config.engine.logParameters.filename = config.engine.logParameters.logFilename
-    delete config.engine.logParameters.logFilename
-
-    // Rename FolderScanner => RawFile
-    console.info('FolderScanner => RawFile')
-    config.south.dataSources.forEach((dataSource) => {
-      if (dataSource.protocol === 'FolderScanner') {
-        dataSource.protocol = 'RawFile'
-        dataSource.RawFile = dataSource.FolderScanner
-        delete dataSource.FolderScanner
-      }
-    })
-  },
-  4: (config) => {
-    // Rename RawFile => FolderScanner
-    console.info('RawFile => FolderScanner')
-    config.south.dataSources.forEach((dataSource) => {
-      if (dataSource.protocol === 'RawFile') {
-        dataSource.protocol = 'FolderScanner'
-        dataSource.FolderScanner = dataSource.RawFile
-        delete dataSource.RawFile
-      }
-    })
-  },
-}
+/*
+ * An example entry how to migrate to version 2
+ *
+ * 2: (config) => {
+ *   // Rename logFilename => filename
+ *   console.info('config.engine.logParameters.logFilename => config.engine.logParameters.filename')
+ *   config.engine.logParameters.filename = config.engine.logParameters.logFilename
+ *   delete config.engine.logParameters.logFilename
+ *
+ *   // Rename FolderScanner => RawFile
+ *   console.info('FolderScanner => RawFile')
+ *   config.south.dataSources.forEach((dataSource) => {
+ *     if (dataSource.protocol === 'FolderScanner') {
+ *       dataSource.protocol = 'RawFile'
+ *       dataSource.RawFile = dataSource.FolderScanner
+ *       delete dataSource.FolderScanner
+ *     }
+ *   })
+ * }
+ */
+module.exports = { }
