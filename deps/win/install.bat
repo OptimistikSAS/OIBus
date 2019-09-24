117d09084a7ca388a703935ce127fc638a406674
kukukk <szabolcs.balogh@lynxsolutions.eu>
2019-09-24 11:10:57 +0300

    Issue #367: show an input if no path is specified and check if it ends in .json

--- deps/win/install.bat
+++ deps/win/install.bat
@@ -2,23 +2,35 @@
 
 title Install OIBus as Windows service
 
-if NOT "%~1" == "" (
-    echo Administrator permissions required. Detecting permission...
+echo Administrator permissions required. Detecting permission...
 
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Stopping OIBus service...
-        START nssm.exe stop OIBus
-
-        echo Installing OIBus as Windows service...
-        START nssm.exe install OIBus "%cd%\oibus.exe" "--config ""%~1"""
-        START nssm.exe set OIBus AppDirectory "%cd%"
-
-        echo Starting OIBus service...
-        START nssm.exe start OIBus
-    ) else (
-        echo No Administrator permission. Please run Command Prompt as Administrator
-    )
-) else (
-    echo Config file location missing. Usage: install.bat C:\OIBus\oibus.json
+net session >nul 2>&1
+if NOT %errorLevel% == 0 (
+    echo No Administrator permission. Please run Command Prompt as Administrator
+    goto EOF
 )
+
+set "CONFIG_PATH=%~1"
+
+goto CHECK
+
+:INPUT
+SET /P CONFIG_PATH=Enter the path for the config file:
+
+:CHECK
+if "%CONFIG_PATH:~-5%" neq ".json" (
+    echo %CONFIG_PATH% is not a valid config file path
+    goto INPUT
+)
+
+echo Stopping OIBus service...
+START nssm.exe stop OIBus
+
+echo Installing OIBus as Windows service...
+START nssm.exe install OIBus "%cd%\oibus.exe" "--config ""%~1"""
+START nssm.exe set OIBus AppDirectory "%cd%"
+
+echo Starting OIBus service...
+START nssm.exe start OIBus
+
+:EOF
