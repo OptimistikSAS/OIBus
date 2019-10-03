d6c3b90eda458d7f16c0100870aa03ca1224c47e
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-10-03 13:27:48 +0300

    Issue #387: migratation for schema version 2

--- src/migration/migrationRules.js
+++ src/migration/migrationRules.js
@@ -18,4 +18,36 @@
  *   })
  * }
  */
-module.exports = { }
+module.exports = {
+  2: (config) => {
+    console.info('Move protocol dependent parameters under a sub object of the protocol')
+    const engineRelatedDataSourceFields = ['dataSourceId', 'enabled', 'protocol', 'scanMode', 'points', 'scanGroups']
+    config.south.dataSources.forEach((dataSource) => {
+      if (!Object.prototype.hasOwnProperty.call(dataSource, dataSource.protocol)) {
+        const dataSourceRelatedFields = {}
+        Object.entries(dataSource).forEach(([key, value]) => {
+          if (!engineRelatedDataSourceFields.includes(key)) {
+            dataSourceRelatedFields[key] = value
+            delete dataSource[key]
+          }
+        })
+        dataSource[dataSource.protocol] = dataSourceRelatedFields
+      }
+    })
+
+    console.info('Move api dependent parameters under a sub object of the api')
+    const engineRelatedApplicationFields = ['applicationId', 'enabled', 'api', 'caching', 'subscribedTo']
+    config.north.applications.forEach((application) => {
+      if (!Object.prototype.hasOwnProperty.call(application, application.api)) {
+        const applicationRelatedFields = {}
+        Object.entries(application).forEach(([key, value]) => {
+          if (!engineRelatedApplicationFields.includes(key)) {
+            applicationRelatedFields[key] = value
+            delete application[key]
+          }
+        })
+        application[application.api] = applicationRelatedFields
+      }
+    })
+  },
+}
