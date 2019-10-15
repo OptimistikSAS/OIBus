5516eaa48d36f33f6cfbcf26583c6b6fe4bcc79c
Attila Suto <attila.suto@lynxsolutions.eu>
2019-10-15 11:46:46 +0300

    Added more validation rules to south protocols.

--- src/south/OPCHDA/OPCHDA.validation.js
+++ src/south/OPCHDA/OPCHDA.validation.js
@@ -1,13 +1,11 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   OPCHDA: {
-    agentFilename: type.string,
-    tcpPort: (val) => (val >= 1 && val <= 65535 ? null : 'value should be between 1 and 65535'),
+    agentFilename: (val) => ((val && val.length > 0) ? null : 'Agent Filename should not be empty'),
+    tcpPort: (val) => (val >= 1 && val <= 65535 ? null : 'Value should be between 1 and 65535'),
     host: (val) => ((val && val.length > 2) ? null : 'Length should be greater than 2'),
-    serverName: type.string,
-    retryInterval: type.number,
-    points: { pointId: type.string },
+    serverName: (val) => ((val && val.length > 0) ? null : 'Server Name should not be empty'),
+    retryInterval: (val) => (val > 0 ? null : 'Retry interval should be greater than 0'),
+    points: { pointId: (val) => ((val && val.length > 0) ? null : 'Point Id should not be empty') },
   },
 }
 
