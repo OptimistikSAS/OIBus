3995483fd838211c52adddf092a73ef648f57f5c
Attila Suto <attila.suto@lynxsolutions.eu>
2019-10-15 11:46:46 +0300

    Added more validation rules to south protocols.

--- src/south/MQTT/MQTT.validation.js
+++ src/south/MQTT/MQTT.validation.js
@@ -1,14 +1,12 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   MQTT: {
-    server: type.string,
+    server: (val) => ((val && val.length > 0) ? null : 'Server should not be empty'),
     port: (val) => (val >= 1 && val <= 65535 ? null : 'Value should be between 1 and 65535'),
     username: (val) => ((val && val.length > 0) ? null : 'Username should not be empty'),
     password: (val) => ((val && val.length > 0) ? null : 'Password should not be empty'),
     points: {
-      pointId: type.string,
-      topic: type.string,
+      pointId: (val) => ((val && val.length > 0) ? null : 'Point Id should not be empty'),
+      topic: (val) => ((val && val.length > 0) ? null : 'Topic should not be empty'),
     },
   },
 }
