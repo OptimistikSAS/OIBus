d202129a2d5e67223d25f22e9301115ad38229f3
Attila Suto <attila.suto@lynxsolutions.eu>
2019-10-15 11:46:46 +0300

    Added more validation rules to south protocols.

--- src/south/OPCUA/OPCUA.validation.js
+++ src/south/OPCUA/OPCUA.validation.js
@@ -1,15 +1,13 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   OPCUA: {
     host: (val) => ((val && val.length > 2) ? null : 'Length should be greater than 2'),
     opcuaPort: (val) => (val >= 1 && val <= 65535 ? null : 'value should be between 1 and 65535'),
     httpsPort: (val) => (val >= 1 && val <= 65535 ? null : 'value should be between 1 and 65535'),
-    endPoint: type.string,
+    endPoint: (val) => ((val && val.length > 0) ? null : 'End Point should not be empty'),
     points: {
-      pointId: type.string,
-      ns: type.stringOrNumber,
-      s: type.string,
+      pointId: (val) => ((val && val.length > 0) ? null : 'Point Id should not be empty'),
+      ns: (val) => (((val && val.length > 0) || val > 0) ? null : 'Value should not be empty'),
+      s: (val) => ((val && val.length > 0) ? null : 'Value should not be empty'),
     },
   },
 }
