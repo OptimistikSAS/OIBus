88458cacea4385ebe6042ea11e368dfcc108c298
Attila Suto <attila.suto@lynxsolutions.eu>
2019-10-16 16:57:55 +0300

    Additional validation rules to North APIs.

--- src/client/North/Form/North.validation.js
+++ src/client/North/Form/North.validation.js
@@ -1,11 +1,9 @@
-import type from '../../helpers/validation'
-
 const validation = {
   caching: {
-    sendInterval: type.number,
-    retryInterval: type.number,
-    groupCount: type.number,
-    maxSendCount: type.number,
+    sendInterval: (val) => (val >= 1000 ? null : 'Send interval should be greater or equal to 1000'),
+    retryInterval: (val) => (val >= 1000 ? null : 'Retry interval should be greater or equal to 1000'),
+    groupCount: (val) => (val > 0 ? null : 'Group count should be greater than 0'),
+    maxSendCount: (val) => (val > 0 ? null : 'Max group count should be greater than 0'),
   },
 }
 
--- src/north/alivesignal/AliveSignal.validation.js
+++ src/north/alivesignal/AliveSignal.validation.js
@@ -1,5 +1,3 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   AliveSignal: {
     host: (val) => ((val && val.length > 2) ? null : 'Length should be greater than 2'),
@@ -7,8 +5,8 @@ const validation = {
       username: (val) => ((val && val.length > 0) ? null : 'Username should not be empty'),
       password: (val) => ((val && val.length > 0) ? null : 'Password should not be empty'),
     },
-    id: type.string,
-    frequency: type.number,
+    id: (val) => ((val && val.length > 0) ? null : 'ID should not be empty'),
+    frequency: (val) => (val >= 1000 ? null : 'Frequency should be greater or equal to 1000'),
   },
 }
 
