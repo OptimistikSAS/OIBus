90b04c60f3e60192c2e6fa87efa38db3aac872a4
Attila Suto <attila.suto@lynxsolutions.eu>
2019-10-16 16:57:55 +0300

    Additional validation rules to North APIs.

--- src/north/oianalyticsfile/OIAnalyticsFile.validation.js
+++ src/north/oianalyticsfile/OIAnalyticsFile.validation.js
@@ -1,9 +1,7 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   OIAnalyticsFile: {
     host: (val) => ((val && val.length > 2) ? null : 'Length should be greater than 2'),
-    endpoint: type.string,
+    endpoint: (val) => ((val && val.length > 0) ? null : 'End point should not be empty'),
     authentication: {
       username: (val) => ((val && val.length > 0) ? null : 'Username should not be empty'),
       password: (val) => ((val && val.length > 0) ? null : 'Password should not be empty'),
--- src/north/oiconnect/OIConnect.validation.js
+++ src/north/oiconnect/OIConnect.validation.js
@@ -1,14 +1,12 @@
-import type from '../../client/helpers/validation'
-
 const validation = {
   OIConnect: {
     host: (val) => ((val && val.length > 2) ? null : 'Length should be greater than 2'),
-    endpoint: type.string,
+    endpoint: (val) => ((val && val.length > 0) ? null : 'End point should not be empty'),
     authentication: {
       username: (val) => ((val && val.length > 0) ? null : 'Username should not be empty'),
       password: (val) => ((val && val.length > 0) ? null : 'Password should not be empty'),
     },
-    timeout: type.number,
+    timeout: (val) => (val >= 1000 ? null : 'Timeout should be greater or equal to 1000'),
   },
 }
 
