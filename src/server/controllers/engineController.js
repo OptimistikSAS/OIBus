418915f900cb42ae9a509f3e9cba89e835073c44
Szabolcs Balogh <szabolcs.balogh@lynxsolutions.eu>
2019-10-23 16:13:32 +0300

    Issue #401: clean endpoints

--- src/server/controllers/engineController.js
+++ src/server/controllers/engineController.js
@@ -1,3 +1,65 @@
+const os = require('os')
+const moment = require('moment-timezone')
+
+/**
+ * Get status info for the dashboard
+ * @param {Object} ctx - The KOA context
+ * @return {void}
+ */
+const getStatus = async (ctx) => {
+  const apisCacheStats = await ctx.app.engine.cache.getCacheStatsForApis()
+  const protocolsCacheStats = await ctx.app.engine.cache.getCacheStatsForProtocols()
+
+  const status = {
+    Version: ctx.app.engine.getVersion(),
+    'Configuration File': ctx.app.engine.configFile,
+    Architecture: process.arch,
+    CurrentDirectory: process.cwd(),
+    'Node Version': process.version,
+    Executable: process.execPath,
+    ConfigFile: ctx.app.engine.configService.getConfigurationFileLocation(),
+    'Free/Total Memory/%': `${os.freemem()}/${os.totalmem()}/${Number((os.freemem() / os.totalmem()) * 100).toFixed(2)}%`,
+    'Process Id': process.pid,
+    'Up time': moment.duration(process.uptime(), 'seconds').humanize(),
+    Hostname: os.hostname(),
+    'OS release': os.release(),
+    'OS type': os.type(),
+    Copyright: '(c) Copyright 2019 Optimistik, all rights reserved.',
+    ...apisCacheStats,
+    ...protocolsCacheStats,
+  }
+  ctx.ok(status)
+}
+
+/**
+ * Get North list.
+ * @param {Object} ctx - The KOA context
+ * @return {void}
+ */
+const getNorthList = (ctx) => {
+  ctx.ok(ctx.app.engine.getNorthList())
+}
+
+/**
+ * Get South list.
+ * @param {Object} ctx - The KOA context
+ * @return {void}
+ */
+const getSouthList = (ctx) => {
+  ctx.ok(ctx.app.engine.getSouthList())
+}
+
+/**
+ * Reload OIBus.
+ * @param {Object} ctx  - The KOA context
+ * @returns {void}
+ */
+const reload = async (ctx) => {
+  await ctx.app.engine.reload(10000)
+
+  ctx.ok('Reloading...')
+}
+
 /**
  * Add Values to the Engine
  * @param {Object} ctx - The KOA context
@@ -15,4 +77,10 @@ const addValues = async (ctx) => {
   }
 }
 
-module.exports = { addValues }
+module.exports = {
+  getStatus,
+  getNorthList,
+  getSouthList,
+  reload,
+  addValues,
+}
