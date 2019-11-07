f2b8de00dadf4a4fe84438cb1de7128b7d98023f
JFH <jfh@JFH-MacBook-Air.local>
2019-11-07 21:50:43 +0100

    folderScanner schema

---
+++ src/south/FolderScanner/FolderScanner.schema.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const schema = { name: 'FolderScanner' }
+schema.form = {
+  inputFolder: {
+    type: 'OIbText',
+    newRow: true,
+    md: 4,
+    label: 'Input Folder',
+    valid: (val) => (val && val.length > 0 ? null : 'Input Folder should not be empty'),
+    defaultValue: './input',
+    help: <div>Path to the input folder</div>,
+  },
+  scanMode: { md: 4, newRow: false, type: 'OIbScanMode', label: 'Scan Mode' },
+  preserve: {
+    type: 'OIbCheckbox',
+    newRow: true,
+    md: 4,
+    label: 'Preserve File?',
+    valid: (val) => ((val === true || val === false) ? null : 'Preserve must be a boolean'),
+    defaultValue: true,
+    help: <div>Path to the archive folder</div>,
+  },
+  minAge: {
+    type: 'OIbInteger',
+    newRow: true,
+    md: 4,
+    label: 'Minimum Age',
+    valid: (val) => (val > 0 ? null : 'Minimum Age should be greater than 0'),
+    defaultValue: 1000,
+    help: <div>Path to the error folder</div>,
+  },
+  regex: {
+    type: 'OIbText',
+    newRow: true,
+    md: 4,
+    label: 'RegExp',
+    valid: (val) => ((val && val.length > 0) ? null : 'RegExp should not be empty'),
+    defaultValue: '.txt',
+    help: <div>RegExp to filter the folder</div>,
+  },
+}
+
+schema.points = null
+
+export default schema
