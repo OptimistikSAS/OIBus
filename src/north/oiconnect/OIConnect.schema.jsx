f722bad3e93a759fb00858d672a8cd959ac4d9fa
Szabolcs Balogh <bszabolcs@gmail.com>
2019-12-04 00:50:26 +0200

    Issue #465: add help text and use dateFormat in SQLDbToFile UI (#466)
    * Issue #465: add help text and use dateFormat in SQLDbToFile UI
    * improve help and remove some unused files

--- src/north/oiconnect/OIConnect.schema.jsx
+++ src/north/oiconnect/OIConnect.schema.jsx
@@ -3,10 +3,21 @@ import { minValue, notEmpty } from '../../services/validation.service'
 
 const schema = { name: 'OIConnect' }
 schema.form = {
-  endPointSection: {
+  OIConnectSettings: {
     type: 'OIbTitle',
-    label: 'End Point',
-    children: <p>endpoint for OIBus</p>,
+    label: 'OIConnect settings',
+    md: 12,
+    children: (
+      <>
+        <p>
+          OIConnect sends values to an another OIBus or OIAnalytics instance. The target OIBus instance will handle the
+          received data the same way as the data coming from its own South. It can be useful in situation when multiple
+          OIBus installations are present on the same network but only one of them has access to the internet.
+        </p>
+        <p>Endpoint for OIBus is /engine/addValues</p>
+        <p>Endpoint for OIAnalytics is /api/optimistik/oibus/data/time_values</p>
+      </>
+    ),
   },
   host: {
     type: 'OIbText',
@@ -18,7 +29,7 @@ schema.form = {
     label: 'End Point',
     newRow: false,
     valid: notEmpty(),
-    defaultValue: '',
+    defaultValue: '/engine/addValues',
   },
   authentication: { type: 'OIbAuthentication' },
   networkSection: {
@@ -31,9 +42,9 @@ schema.form = {
           <li>Proxy: proxy name to use (proxy parameters are defined in the Engine page)</li>
           <li>
             Stack: OIBus can use several IP stacks to communicate with the host. In certain network configuration
-            (firewall settings for example), it might be useful to try a different stack. We generally advise to
-            leave &apos;fetch&apos; as it is the native nodej stack but we also use axios as it reports good
-            messages to diagnostic network errors.
+            (firewall settings for example), it might be useful to try a different stack. We generally advise to leave
+            &apos;fetch&apos; as it is the native nodej stack but we also use axios as it reports good messages to
+            diagnostic network errors.
           </li>
         </ul>
       </>
