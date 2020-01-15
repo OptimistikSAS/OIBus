669f3796485006d896f24cde2fe2263291b7f7e0
JFH <jfh@JFH-MacBook-Air.local>
2020-01-15 11:58:58 +0100

    update package and stop using enzyme for tests

--- src/client/components/OIbForm/OIbInteger.spec.jsx
+++ src/client/components/OIbForm/OIbInteger.spec.jsx
@@ -1,36 +1,45 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
 
 import OIbInteger from './OIbInteger.jsx'
 import { minValue } from '../../../services/validation.service'
 
-Enzyme.configure({ adapter: new Adapter() })
+let container
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
 
 describe('OIbInteger', () => {
   test('check OIbInteger with value="a text"', () => {
-    const wrapper = shallow(
-      <OIbInteger
+    act(() => {
+      ReactDOM.render(<OIbInteger
         label="label"
         value="12345678"
         name="name"
         onChange={() => (1)}
         defaultValue="0"
-      />,
-    )
-    expect(wrapper.html()).toMatchSnapshot()
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
   })
   test('check Texte with too small value', () => {
-    const wrapper = shallow(
-      <OIbInteger
+    act(() => {
+      ReactDOM.render(<OIbInteger
         label="label"
         name="name"
         onChange={() => (1)}
         value="12"
         valid={minValue(100)}
         defaultValue="0"
-      />,
-    )
-    expect(wrapper.html()).toMatchSnapshot()
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
   })
 })
