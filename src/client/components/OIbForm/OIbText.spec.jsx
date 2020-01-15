7f751932e10d613afb493dedff22590bcbd2d462
JFH <jfh@JFH-MacBook-Air.local>
2020-01-15 11:58:58 +0100

    update package and stop using enzyme for tests

--- src/client/components/OIbForm/OIbText.spec.jsx
+++ src/client/components/OIbForm/OIbText.spec.jsx
@@ -1,49 +1,58 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
 
 import OIbText from './OIbText.jsx'
 import { minLength } from '../../../services/validation.service'
 
-Enzyme.configure({ adapter: new Adapter() })
+let container
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
 
 describe('OIbText', () => {
   test('check Texte with value="a text"', () => {
-    const wrapper = shallow(
-      <OIbText
+    act(() => {
+      ReactDOM.render(<OIbText
         label="label"
         value="a text"
         name="name"
         onChange={() => (1)}
         defaultValue="default text"
-      />,
-    )
-    expect(wrapper.html()).toMatchSnapshot()
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
   })
   test('check Texte with too short value', () => {
-    const wrapper = shallow(
-      <OIbText
+    act(() => {
+      ReactDOM.render(<OIbText
         label="label"
         name="name"
         onChange={() => (1)}
         value="a text"
         valid={minLength(10)}
         defaultValue="default text"
-      />,
-    )
-    expect(wrapper.html()).toMatchSnapshot()
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
   })
   test('check Texte with inline', () => {
-    const wrapper = shallow(
-      <OIbText
+    act(() => {
+      ReactDOM.render(<OIbText
         label="label"
         name="name"
         onChange={() => (1)}
         value="a text"
         defaultValue="default text"
         inline
-      />,
-    )
-    expect(wrapper.html()).toMatchSnapshot()
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
   })
 })
