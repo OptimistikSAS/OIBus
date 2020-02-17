bcd4e64476401a64db2bcf7eaaca779751e02c4f
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-02-17 10:17:05 +0200

    Issue #528: Clean OIAnalyticsFile

--- src/north/oianalyticsfile/OIAnalyticsFile.class.js
+++ src/north/oianalyticsfile/OIAnalyticsFile.class.js
@@ -16,14 +16,11 @@ class OIAnalyticsFile extends ApiHandler {
   constructor(applicationParameters, engine) {
     super(applicationParameters, engine)
 
-    const { host, endpoint, authentication, proxy = null, stack = 'fetch', timeout = 60000 } = applicationParameters.OIAnalyticsFile
+    const { host, endpoint, authentication, proxy = null } = applicationParameters.OIAnalyticsFile
 
     this.url = `${host}${endpoint}`
     this.authentication = authentication
     this.proxy = this.getProxy(proxy)
-    this.stack = stack
-
-    this.timeout = timeout
 
     this.canHandleFiles = true
   }
@@ -34,10 +31,8 @@ class OIAnalyticsFile extends ApiHandler {
    * @return {Promise} - The send status
    */
   async handleFile(filePath) {
-    this.logger.silly(`OIAnalyticsFile handleFile() call with ${filePath}`)
     const stats = fs.statSync(filePath)
-    const fileSizeInBytes = stats.size
-    this.logger.debug(`Sending file ${filePath} (${fileSizeInBytes} bytes) using ${this.stack} stack`)
+    this.logger.silly(`OIAnalyticsFile handleFile() call with ${filePath} (${stats.size} bytes)`)
 
     return this.engine.sendRequest(this.url, 'POST', this.authentication, this.proxy, filePath)
   }
