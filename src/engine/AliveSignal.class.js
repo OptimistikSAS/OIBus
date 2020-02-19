3e6c1a9c740b53871ae9de3313aee35a30e778a4
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-02-19 08:23:45 +0200

    Issue #528: Use debug log instead of info

--- src/engine/AliveSignal.class.js
+++ src/engine/AliveSignal.class.js
@@ -59,7 +59,7 @@ class AliveSignal {
 
     try {
       await this.engine.sendRequest(this.host, 'POST', this.authentication, this.proxy, status)
-      this.logger.info('Alive signal successful')
+      this.logger.debug('Alive signal successful')
     } catch (error) {
       this.logger.error(error)
     }
