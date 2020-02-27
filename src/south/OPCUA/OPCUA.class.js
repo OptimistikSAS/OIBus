c60d30a9e08ef0dbd65304bb2cccddda6f12de98
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-02-27 17:43:31 +0200

    Issue #459: OPCUA-HDA starting...

--- src/south/OPCUA/OPCUA.class.js
+++ src/south/OPCUA/OPCUA.class.js
@@ -1,5 +1,6 @@
 const Opcua = require('node-opcua')
 const ProtocolHandler = require('../ProtocolHandler.class')
+const databaseService = require('../../services/database.service')
 
 /**
  *
@@ -17,9 +18,27 @@ class OPCUA extends ProtocolHandler {
    */
   constructor(dataSource, engine) {
     super(dataSource, engine)
-    const { url, maxAge } = dataSource.OPCUA
+    const { url, maxAge, maxReturnValues, maxReadInterval } = dataSource.OPCUA
     this.url = url
     this.maxAge = maxAge || 10
+    this.maxReturnValues = maxReturnValues
+    this.maxReadInterval = maxReadInterval
+
+    this.lastCompletedAt = {}
+    this.ongoingReads = {}
+
+    this.scanGroups = this.dataSource.OPCUA.scanGroups.map((scanGroup) => {
+      const points = this.dataSource.points
+        .filter((point) => point.scanMode === scanGroup.scanMode)
+        .map((point) => point.nodeId)
+      this.lastCompletedAt[scanGroup.scanMode] = new Date().getTime()
+      this.ongoingReads[scanGroup.scanMode] = false
+      return {
+        name: scanGroup.scanMode,
+        ...scanGroup,
+        points,
+      }
+    })
   }
 
   /**
@@ -28,6 +47,27 @@ class OPCUA extends ProtocolHandler {
    */
   async connect() {
     super.connect()
+
+    // Initialize lastCompletedAt for every scanGroup
+    const { dataSourceId, startTime } = this.dataSource
+    const { engineConfig } = this.engine.configService.getConfig()
+    const databasePath = `${engineConfig.caching.cacheFolder}/${dataSourceId}.db`
+    this.configDatabase = await databaseService.createConfigDatabase(databasePath)
+
+    const defaultLastCompletedAt = startTime ? new Date(startTime).getTime() : new Date().getTime()
+    // Disable ESLint check because we need for..in loop to support async calls
+    // eslint-disable-next-line no-restricted-syntax
+    for (const scanGroup in Object.keys(this.lastCompletedAt)) {
+      if ({}.hasOwnProperty.call(this.lastCompletedAt, scanGroup)) {
+        // Disable ESLint check because we want to get the values one by one to avoid parallel access to the SQLite database
+        // eslint-disable-next-line no-await-in-loop
+        let lastCompletedAt = await databaseService.getConfig(this.configDatabase, `lastCompletedAt-${scanGroup}`)
+        lastCompletedAt = lastCompletedAt ? parseInt(lastCompletedAt, 10) : defaultLastCompletedAt
+        this.logger.info(`Initializing lastCompletedAt for ${scanGroup} with ${lastCompletedAt}`)
+        this.lastCompletedAt[scanGroup] = lastCompletedAt
+      }
+    }
+
     try {
       // define OPCUA connection parameters
       const connectionStrategy = {
@@ -59,8 +99,25 @@ class OPCUA extends ProtocolHandler {
    * @todo on the very first Scan dataSource.session might not be created yet, find out why
    */
   async onScan(scanMode) {
-    /* @todo: ScanGroup should be calculated on startup and not on each scan */
-    const scanGroup = this.dataSource.points.filter((point) => point.scanMode === scanMode)
+    const scanGroup = this.scanGroups.find((item) => item.scanMode === scanMode)
+    console.info(scanGroup)
+
+    if (!this.connected || this.ongoingReads[scanMode] || !scanGroup.points.length) return
+
+    try {
+      const nodesToRead = scanGroup.points.map((point) => ({ nodeId: point }))
+      console.info(nodesToRead)
+      const dataValues = await this.session.readHistoryValue(nodesToRead, this.lastCompletedAt[scanMode], new Date())
+      if (dataValues.length !== nodesToRead.length) {
+        this.logger.error(`received ${dataValues.length}, requested ${nodesToRead.length}`)
+      }
+      console.info(dataValues)
+    } catch (error) {
+      console.error(error)
+      this.logger.error(error)
+    }
+
+    /*
     if (!this.connected || !scanGroup.length) return
     const nodesToRead = scanGroup.map((point) => ({ nodeId: point.nodeId, attributeId: Opcua.AttributeIds.Value }))
     try {
@@ -80,6 +137,7 @@ class OPCUA extends ProtocolHandler {
     } catch (error) {
       this.logger.error(error)
     }
+     */
   }
 
   /**
--- src/south/OPCUA/OPCUA.schema.jsx
+++ src/south/OPCUA/OPCUA.schema.jsx
@@ -24,6 +24,50 @@ schema.form = {
     options: ['server', 'source'],
     defaultValue: 'server',
   },
+  maxReturnValues: {
+    type: 'OIbInteger',
+    newRow: false,
+    md: 2,
+    valid: minValue(0),
+    defaultValue: 0,
+    help: <div>Max return values</div>,
+  },
+  maxReadInterval: {
+    type: 'OIbInteger',
+    newRow: false,
+    md: 2,
+    valid: minValue(0),
+    defaultValue: 3600,
+    help: <div>Max read interval (s)</div>,
+  },
+  scanGroupsSection: {
+    type: 'OIbTitle',
+    label: 'ScanGroups',
+    md: 12,
+    children: (
+      <p>
+        OPCHDA application will request all points in the same scanMode. OPCHDA can query raw values but can also
+        aggregate points on a given period. if an aggregate is chosen, the resampling period must also be selected.
+        Important: a point with a scanMode without the corresponding scangroup will not be requested
+      </p>
+    ),
+  },
+  scanGroups: {
+    type: 'OIbTable',
+    rows: {
+      scanMode: { type: 'OIbScanMode', label: 'Scan Mode' },
+      Aggregate: {
+        type: 'OIbSelect',
+        options: ['Raw', 'Average', 'Minimum', 'Maximum', 'Start', 'End'],
+        defaultValue: 'Raw',
+      },
+      resampling: {
+        type: 'OIbSelect',
+        options: ['None', 'Minute', 'Hour', 'Day'],
+        defaultValue: 'None',
+      },
+    },
+  },
 }
 
 schema.points = {
