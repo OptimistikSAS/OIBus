ab6576a1c087ba4f6754c80de3fa571a09a0e1c0
attilasuto <47381370+attilasuto@users.noreply.github.com>
2020-03-13 11:11:57 +0200

    Clicks changes tests (#625)
    * More tests for Engine.jsx and Logging.jsx
    
    * engine testing completed.

--- src/client/Engine/AliveSignal.spec.jsx
+++ src/client/Engine/AliveSignal.spec.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 
 import testConfig from '../../../tests/testConfig'
 import AliveSignal from './AliveSignal.jsx'
+
+const onChange = jest.fn()
 
 const mockMath = Object.create(global.Math)
 mockMath.random = () => 1
@@ -28,6 +30,17 @@ describe('AliveSignal', () => {
         onChange={() => (1)}
       />, container)
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check change aliveSignal to false', () => {
+    act(() => {
+      ReactDOM.render(<AliveSignal
+        aliveSignal={testConfig.engine.aliveSignal}
+        onChange={onChange}
+      />, container)
+    })
+    Simulate.change(document.getElementById('engine.aliveSignal.enabled'), { target: { checked: false } })
+    expect(onChange).toBeCalledWith('engine.aliveSignal.enabled', false, null)
     expect(container).toMatchSnapshot()
   })
 })
--- src/client/Engine/ScanModes.spec.jsx
+++ src/client/Engine/ScanModes.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 
 import testConfig from '../../../tests/testConfig'
 import ScanModes from './ScanModes.jsx'
@@ -8,6 +8,9 @@ import ScanModes from './ScanModes.jsx'
 const mockMath = Object.create(global.Math)
 mockMath.random = () => 1
 global.Math = mockMath
+
+const dispatchNewConfig = jest.fn()
+React.useContext = jest.fn().mockReturnValue({ dispatchNewConfig })
 
 let container
 beforeEach(() => {
@@ -27,6 +30,46 @@ describe('ScanModes', () => {
         scanModes={testConfig.engine.scanModes}
       />, container)
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check change first scan mode', () => {
+    act(() => {
+      ReactDOM.render(<ScanModes
+        scanModes={testConfig.engine.scanModes}
+      />, container)
+    })
+    Simulate.change(document.getElementById('engine.scanModes.0.scanMode'), { target: { value: 'every30Second' } })
+    expect(dispatchNewConfig).toBeCalledWith({ type: 'update', name: 'engine.scanModes.0.scanMode', value: 'every30Second', validity: null })
+    expect(container).toMatchSnapshot()
+  })
+  test('check change first cron', () => {
+    act(() => {
+      ReactDOM.render(<ScanModes
+        scanModes={testConfig.engine.scanModes}
+      />, container)
+    })
+    Simulate.change(document.getElementById('engine.scanModes.0.cronTime'), { target: { value: '* * * * * /30' } })
+    expect(dispatchNewConfig).toBeCalledWith({ type: 'update', name: 'engine.scanModes.0.cronTime', value: '* * * * * /30', validity: null })
+    expect(container).toMatchSnapshot()
+  })
+  test('check delete first scan mode', () => {
+    act(() => {
+      ReactDOM.render(<ScanModes
+        scanModes={testConfig.engine.scanModes}
+      />, container)
+    })
+    Simulate.click(document.querySelector('td path'))
+    expect(dispatchNewConfig).toBeCalledWith({ type: 'deleteRow', name: 'engine.scanModes.0' })
+    expect(container).toMatchSnapshot()
+  })
+  test('check add line to scan modes', () => {
+    act(() => {
+      ReactDOM.render(<ScanModes
+        scanModes={testConfig.engine.scanModes}
+      />, container)
+    })
+    Simulate.click(document.querySelector('th path'))
+    expect(dispatchNewConfig).toBeCalledWith({ type: 'addRow', name: 'engine.scanModes', value: { scanMode: '', cronTime: '' } })
     expect(container).toMatchSnapshot()
   })
 })
