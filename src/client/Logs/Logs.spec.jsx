d3f2a3378b912d64b99b4c1bb8407815242463f1
attilasuto <47381370+attilasuto@users.noreply.github.com>
2020-03-16 13:34:52 +0200

    Clicks changes tests (#629)
    * More tests for Engine.jsx and Logging.jsx
    
    * engine testing completed.
    
    * Tests on Engine and Logs.

--- src/client/Logs/Logs.spec.jsx
+++ src/client/Logs/Logs.spec.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 import { BrowserRouter } from 'react-router-dom'
 
+import apis from '../services/apis'
 import Logs from './Logs.jsx'
 
 // fixing date to match snapshot
@@ -66,11 +67,39 @@ const testLogs = [
     source: 'Source',
     message: 'Testing silly logs',
   },
+  {
+    id: 6,
+    timestamp: '2020-01-01T00:00:00.000Z',
+    level: 'silly',
+    source: 'Source',
+    message: 'Testing silly logs on page 2',
+  },
 ]
 
 // setting initial test logs to be displayed
+// setting max log to 5 to have more pages
 const setState = jest.fn()
-React.useState = jest.fn().mockImplementation((init) => [init === undefined ? testLogs : init, setState])
+const defaultMaxLog = 300
+const tempDefaultMaxLog = 5
+React.useState = jest.fn().mockImplementation((init) => {
+  if (init === undefined) {
+    return [testLogs, setState]
+  }
+  if (init === defaultMaxLog) {
+    return [tempDefaultMaxLog, setState]
+  }
+  return [init, setState]
+})
+
+// mock get Logs
+let resolve
+let reject
+const setAlert = jest.fn()
+apis.getLogs = () => new Promise((_resolve, _reject) => {
+  resolve = _resolve
+  reject = _reject
+})
+React.useContext = jest.fn().mockReturnValue({ setAlert })
 
 let container
 beforeEach(() => {
@@ -93,6 +122,104 @@ describe('Logs', () => {
         container,
       )
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check change fromDate', () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    Simulate.change(document.getElementById('fromDate'), { target: { value: '2020-02-01T00:00:00.000Z' } })
+    expect(setState).toBeCalledWith('2020-02-01T00:00:00.000Z')
+    expect(container).toMatchSnapshot()
+  })
+  test('check change toDate', () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    Simulate.change(document.getElementById('toDate'), { target: { value: '2020-02-01T00:00:00.000Z' } })
+    expect(setState).toBeCalledWith('2020-02-01T00:00:00.000Z')
+    expect(container).toMatchSnapshot()
+  })
+  test('check change filterText', () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    Simulate.change(document.getElementById('filterText'), { target: { value: 'debug' } })
+    expect(setState).toBeCalledWith('debug')
+    expect(container).toMatchSnapshot()
+  })
+  test('check change verbosity', () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    Simulate.change(document.getElementById('verbosity'), { target: { checked: false } })
+    expect(setState).toBeCalledWith(['info', 'warning', 'error', 'silly'])
+    expect(container).toMatchSnapshot()
+  })
+  test('check press Show Logs, success in logs call', async () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    expect(container).toMatchSnapshot()
+    // resolve the call requested by showLog
+    Simulate.click(document.getElementById('showLog'))
+    await act(async () => {
+      resolve(testLogs)
+    })
+    expect(container).toMatchSnapshot()
+  })
+  test('check press Show Logs, error in logs call', async () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    // resolve the call requested by showLog
+    Simulate.click(document.getElementById('showLog'))
+    await act(async () => {
+      reject('Logs: testing error on getLogs request')
+    })
+    expect(setAlert).toHaveBeenCalled()
+  })
+  test('check press showMore', () => {
+    act(() => {
+      ReactDOM.render(
+        <BrowserRouter>
+          <Logs />
+        </BrowserRouter>,
+        container,
+      )
+    })
+    Simulate.click(document.getElementById('showMore'))
+    expect(setState).toBeCalledWith(defaultMaxLog)
     expect(container).toMatchSnapshot()
     global.Date = RealDate
     global.Date.prototype.toLocaleString = realToLocaleString
