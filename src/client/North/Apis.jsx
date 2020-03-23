004f94356968523de5b63235c722a89a00513d4d
Mollier-Pierret <h37219@dsp0919252.atlas.edf.fr>
2020-03-23 23:31:44 +0100

    v0.5.7.8.0 / Integration JFH remarks part2 (modify directory and files name of North MQTT connector)

--- src/client/North/Apis.jsx
+++ src/client/North/Apis.jsx
@@ -5,7 +5,7 @@ import InfluxDB from '../../north/influxdb/InfluxDB.schema.jsx'
 import OIAnalyticsFile from '../../north/oianalyticsfile/OIAnalyticsFile.schema.jsx'
 import TimescaleDB from '../../north/timescaledb/TimescaleDB.schema.jsx'
 import MongoDB from '../../north/mongodb/MongoDB.schema.jsx'
-import MQTTNorth from '../../north/mqtt/MQTT.schema.jsx'
+import MQTTNorth from '../../north/mqttnorth/MQTTNorth.schema.jsx'
 
 const ApiSchemas = { OIConnect, AmazonS3, Console, InfluxDB, OIAnalyticsFile, TimescaleDB, MongoDB, MQTTNorth }
 
