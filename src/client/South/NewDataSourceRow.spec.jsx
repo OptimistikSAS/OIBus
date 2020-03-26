39ff7e2ab068ac9b4c1273ccbaa86f6ff06b5f2f
attilasuto <47381370+attilasuto@users.noreply.github.com>
2020-03-26 17:59:06 +0200

    Clicks changes tests (#638)
    * More tests for Engine.jsx and Logging.jsx
    
    * engine testing completed.
    
    * Tests on Engine and Logs.
    
    * Fixed Warning: Cannot update a component from inside the function body of a different component
    Warning occured when pressed edit on a north/south id(name), because it would trigger an update on the parent component as the validation would change the state on the parent component.
    Fixed by: Moving the validation function outside of the parent component.
    
    * More tests for north.
    
    * New snapshots after test updates.
    
    * North testing 100%
    
    * SubscribedTo tests 100%
    
    * More test to ConfigurePoints. (still needs some tests)
    
    * NewDataSourceRow tests 100%
    
    * PointsButton test 100%
    
    * South tests 100%

--- src/client/South/NewDataSourceRow.spec.jsx
+++ src/client/South/NewDataSourceRow.spec.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 
 import testConfig from '../../../tests/testConfig'
 import NewDataSourceRow from './NewDataSourceRow.jsx'
+
+// mocking state updates
+let dataSourceId = ''
+const setDataSourceId = jest.fn().mockImplementation((newValue) => {
+  dataSourceId = newValue
+})
+let protocol = testConfig.protocolList[0]
+const setProtocol = jest.fn().mockImplementation((newValue) => {
+  protocol = newValue
+})
+const setState = jest.fn()
+React.useState = jest.fn().mockImplementation((init) => {
+  if (init === '') {
+    return [dataSourceId, setDataSourceId]
+  }
+  if (init === testConfig.protocolList[0]) {
+    return [protocol, setProtocol]
+  }
+  return [init, setState]
+})
 
 let container
 beforeEach(() => {
@@ -26,6 +46,60 @@ describe('NewDataSourceRow', () => {
         />, container,
       )
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check add pressed with empty dataSource name', () => {
+    const addDataSource = jest.fn()
+    act(() => {
+      ReactDOM.render(
+        <NewDataSourceRow
+          protocolList={testConfig.protocolList}
+          addDataSource={addDataSource}
+        />, container,
+      )
+    })
+    Simulate.change(document.getElementById('dataSourceId'), { target: { value: '' } })
+    Simulate.click(document.querySelector('div button'))
+    expect(container).toMatchSnapshot()
+  })
+  test('check change dataSourceId', () => {
+    act(() => {
+      ReactDOM.render(
+        <NewDataSourceRow
+          protocolList={testConfig.protocolList}
+          addDataSource={() => (1)}
+        />, container,
+      )
+    })
+    Simulate.change(document.getElementById('dataSourceId'), { target: { value: 'new_datasource' } })
+    expect(setDataSourceId).toBeCalledWith('new_datasource')
+    expect(container).toMatchSnapshot()
+  })
+  test('check change protocol', () => {
+    act(() => {
+      ReactDOM.render(
+        <NewDataSourceRow
+          protocolList={testConfig.protocolList}
+          addDataSource={() => (1)}
+        />, container,
+      )
+    })
+    Simulate.change(document.getElementById('protocol'), { target: { value: 'Modbus' } })
+    expect(setProtocol).toBeCalledWith('Modbus')
+    expect(container).toMatchSnapshot()
+  })
+  test('check add press', () => {
+    const addDataSource = jest.fn()
+    act(() => {
+      ReactDOM.render(
+        <NewDataSourceRow
+          protocolList={testConfig.protocolList}
+          addDataSource={addDataSource}
+        />, container,
+      )
+    })
+    Simulate.click(document.querySelector('div button'))
+    expect(addDataSource).toBeCalledWith({ dataSourceId, protocol })
     expect(container).toMatchSnapshot()
   })
 })
