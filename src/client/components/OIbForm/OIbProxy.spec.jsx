9d71e0dd8474380e190a987de89e1578c9d7f43d
Attila Suto <attila.suto@lynxsolutions.eu>
2020-03-31 14:36:43 +0300

    OIbProxy.jsx tests 100%

--- src/client/components/OIbForm/OIbProxy.spec.jsx
+++ src/client/components/OIbForm/OIbProxy.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 import newConfig from '../../../../tests/testConfig'
 import OIbProxy from './OIbProxy.jsx'
 
@@ -49,6 +49,33 @@ describe('OIbProxy', () => {
         onChange={() => (1)}
       />, container)
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check Proxy with no value', () => {
+    act(() => {
+      ReactDOM.render(<OIbProxy
+        label="label"
+        value={null}
+        help={<div>some help text</div>}
+        name="name"
+        onChange={() => (1)}
+      />, container)
+    })
+    expect(container).toMatchSnapshot()
+  })
+  test('check Proxy change', () => {
+    const onChange = jest.fn()
+    act(() => {
+      ReactDOM.render(<OIbProxy
+        label="label"
+        value="proxy"
+        help={<div>some help text</div>}
+        name="name"
+        onChange={onChange}
+      />, container)
+    })
+    Simulate.change(document.getElementById('name'), { target: { value: 'new_proxy' } })
+    expect(onChange).toBeCalledWith('name', 'new_proxy', null)
     expect(container).toMatchSnapshot()
   })
 })
