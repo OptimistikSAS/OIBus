72399763b6d2946e1151329d6001fb4971639a5b
Attila Suto <attila.suto@lynxsolutions.eu>
2020-03-31 11:43:19 +0300

    Modal.jsx tests 100%

--- src/client/components/Modal.spec.jsx
+++ src/client/components/Modal.spec.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
+import { act, Simulate } from 'react-dom/test-utils'
 
 import Modal from './Modal.jsx'
+
+const setOpen = jest.fn()
+const func = jest.fn()
+const callback = { func }
+const setCallback = jest.fn()
+const setState = jest.fn()
+React.useState = jest.fn().mockImplementation((init) => {
+  if (init === null) {
+    return [callback, setCallback]
+  }
+  if (init === true) {
+    return [init, setOpen]
+  }
+  return [init, setState]
+})
 
 let container
 beforeEach(() => {
@@ -30,6 +45,45 @@ describe('Modal', () => {
         </Modal>, container,
       )
     })
+    expect(container).toMatchSnapshot()
+  })
+  test('check confirm press', () => {
+    act(() => {
+      ReactDOM.render(
+        <Modal
+          show
+          title="title"
+          body="body content"
+          acceptLabel="accept"
+          denyLabel="deny"
+        >
+          {() => (1)}
+        </Modal>, container,
+      )
+    })
+    Simulate.click(document.getElementsByClassName('btn btn-primary')[0])
+    expect(callback.func).toBeCalledWith()
+    expect(setOpen).toBeCalledWith(false)
+    expect(setCallback).toBeCalledWith(null)
+    expect(container).toMatchSnapshot()
+  })
+  test('check cancel press', () => {
+    act(() => {
+      ReactDOM.render(
+        <Modal
+          show
+          title="title"
+          body="body content"
+          acceptLabel="accept"
+          denyLabel="deny"
+        >
+          {() => (1)}
+        </Modal>, container,
+      )
+    })
+    Simulate.click(document.getElementsByClassName('btn btn-secondary')[0])
+    expect(setOpen).toBeCalledWith(false)
+    expect(setCallback).toBeCalledWith(null)
     expect(container).toMatchSnapshot()
   })
 })
