e7e8f6500dde1ce8c5b732c600988ac12c7cfacc
Attila Suto <attila.suto@lynxsolutions.eu>
2020-04-04 12:55:17 +0300

    AlertContext test.

---
+++ src/client/context/AlertContext.spec.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
+
+import { AlertProvider } from './AlertContext.jsx'
+
+let container
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  document.body.removeChild(container)
+  container = null
+})
+
+describe('Logs', () => {
+  test('check AlertProvider', () => {
+    act(() => {
+      ReactDOM.render(
+        <AlertProvider />,
+        container,
+      )
+    })
+    expect(container).toMatchSnapshot()
+  })
+})
