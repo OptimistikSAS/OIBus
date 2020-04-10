86ca3420e0745b6f08f17687cd3ec054e300ac27
attilasuto <47381370+attilasuto@users.noreply.github.com>
2020-04-10 11:15:59 +0300

    Clicks changes tests (#655)
    * More tests for Engine.jsx and Logging.jsx
    
    * engine testing completed.
    
    * Tests on Engine and Logs.
    
    * Fixed Warning: Cannot update a component from inside the function body of a different component
    Warning occured when pressed edit on a north/south id(name), because it would trigger an update on the parent component as the validation would change the state on the parent component.
    Fixed by: Moving the validation function outside of the parent component.
    
    * More tests for north.
    
    * New snapshots after test updates.
    
    * North testing 100%
    
    * SubscribedTo tests 100%
    
    * More test to ConfigurePoints. (still needs some tests)
    
    * NewDataSourceRow tests 100%
    
    * PointsButton test 100%
    
    * South tests 100%
    
    * Modal.jsx tests 100%
    
    * OIbPassword.jsx tests 100%
    
    * OIbProxy.jsx tests 100%
    
    * OIbTable tests 100%
    
    * OIbTextArea test 100%
    
    * TablePagination.jsx tests
    
    * update pkg
    
    * AlertContext test.
    
    * AlertContext tests
    
    * AlertContext test
    
    * configContext tests started
    
    * Fixed initial test on configContext.
    
    * Health.jsx tests 100%
    
    * Logs.jsx tests 100%
    
    * ConfigureApi.jsx tests 100%
    
    * Added more tests to ConfigurePoints.jsx
    
    * North.validation tests 100%
    
    * ConfigureProtocol tests 100%
    
    * South.validation test 100%
    
    * EditableField tests 100%
    
    * OIbInteger tests 100%
    
    * OIbTimezone tests 100%
    
    Co-authored-by: JFH <jfh@JFH-MacBook-Air.local>

---
+++ src/client/North/Form/North.validation.spec.js
@@ -0,0 +1,52 @@
+import validation from './North.validation'
+
+describe('North.validation', () => {
+  it('check caching.sendInterval no error', () => {
+    const error = validation.caching.sendInterval(1000)
+    expect(error).toEqual(null)
+  })
+  it('check caching.sendInterval error', () => {
+    const error = validation.caching.sendInterval(999)
+    expect(error).toBeTruthy()
+  })
+  it('check caching.retryInterval no error', () => {
+    const error = validation.caching.retryInterval(1000)
+    expect(error).toEqual(null)
+  })
+  it('check caching.retryInterval error', () => {
+    const error = validation.caching.retryInterval(999)
+    expect(error).toBeTruthy()
+  })
+  it('check caching.groupCount no error', () => {
+    const error = validation.caching.groupCount(1)
+    expect(error).toEqual(null)
+  })
+  it('check caching.groupCount error', () => {
+    const error = validation.caching.groupCount(0)
+    expect(error).toBeTruthy()
+  })
+  it('check caching.maxSendCount no error', () => {
+    const error = validation.caching.maxSendCount(1)
+    expect(error).toEqual(null)
+  })
+  it('check caching.maxSendCount error', () => {
+    const error = validation.caching.maxSendCount(0)
+    expect(error).toBeTruthy()
+  })
+  it('check application.isValidName no error', () => {
+    const error = validation.application.isValidName('new_name', ['name1', 'name2'])
+    expect(error).toEqual(null)
+  })
+  it('check application.isValidName name already exists', () => {
+    const error = validation.application.isValidName('name1', ['name1', 'name2'])
+    expect(error).toEqual('Id already exists')
+  })
+  it('check application.isValidName name empty', () => {
+    const error = validation.application.isValidName('', ['name1', 'name2'])
+    expect(error).toBeTruthy()
+  })
+  it('check application.isValidName name int', () => {
+    const error = validation.application.isValidName(1, ['name1', 'name2'])
+    expect(error).toBeTruthy()
+  })
+})
