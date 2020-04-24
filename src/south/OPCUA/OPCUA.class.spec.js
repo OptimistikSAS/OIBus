9d8bf109eba0acdd54fe8f6e7d92d9e8d87a868f
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-04-24 12:44:45 +0300

    Issue #459: extend tests

--- src/south/OPCUA/OPCUA.class.spec.js
+++ src/south/OPCUA/OPCUA.class.spec.js
@@ -1,6 +1,15 @@
+const Opcua = require('node-opcua')
+
 const OPCUA = require('./OPCUA.class')
 const config = require('../../config/defaultConfig.json')
 const databaseService = require('../../services/database.service')
+
+// Mock node-opcua
+jest.mock('node-opcua', () => ({
+  OPCUAClient: { create: jest.fn() },
+  MessageSecurityMode: { None: 1 },
+  SecurityPolicy: { None: 'http://opcfoundation.org/UA/SecurityPolicy#None' },
+}))
 
 // Mock database service
 jest.mock('../../services/database.service', () => ({
@@ -24,6 +33,7 @@ engine.configService = { getConfig: () => ({ engineConfig: config.engine }) }
 
 beforeEach(() => {
   jest.resetAllMocks()
+  jest.useFakeTimers()
 })
 
 describe('OPCUA south', () => {
@@ -59,28 +69,103 @@ describe('OPCUA south', () => {
   }]
 
   it('should be properly initialized', () => {
-    const opcua = new OPCUA(opcuaConfig, engine)
+    const opcuaSouth = new OPCUA(opcuaConfig, engine)
 
-    expect(opcua.url).toEqual(opcuaConfig.OPCUA.url)
-    expect(opcua.retryInterval).toEqual(opcuaConfig.OPCUA.retryInterval)
-    expect(opcua.maxReturnValues).toEqual(opcuaConfig.OPCUA.maxReturnValues)
-    expect(opcua.maxReadInterval).toEqual(opcuaConfig.OPCUA.maxReadInterval)
-    expect(opcua.scanGroups).toEqual(opcuaScanGroups)
-    expect(Object.keys(opcua.lastCompletedAt)).toEqual([opcuaConfig.OPCUA.scanGroups[0].scanMode])
-    expect(Object.keys(opcua.ongoingReads)).toEqual([opcuaConfig.OPCUA.scanGroups[0].scanMode])
-    expect(opcua.reconnectTimeout).toBeNull()
+    expect(opcuaSouth.url).toEqual(opcuaConfig.OPCUA.url)
+    expect(opcuaSouth.retryInterval).toEqual(opcuaConfig.OPCUA.retryInterval)
+    expect(opcuaSouth.maxReturnValues).toEqual(opcuaConfig.OPCUA.maxReturnValues)
+    expect(opcuaSouth.maxReadInterval).toEqual(opcuaConfig.OPCUA.maxReadInterval)
+    expect(opcuaSouth.scanGroups).toEqual(opcuaScanGroups)
+    expect(Object.keys(opcuaSouth.lastCompletedAt)).toEqual([opcuaConfig.OPCUA.scanGroups[0].scanMode])
+    expect(Object.keys(opcuaSouth.ongoingReads)).toEqual([opcuaConfig.OPCUA.scanGroups[0].scanMode])
+    expect(opcuaSouth.reconnectTimeout).toBeNull()
   })
 
   it('should properly connect', async () => {
     databaseService.getConfig.mockReturnValue('1587640141001.0')
 
-    const opcua = new OPCUA(opcuaConfig, engine)
-    opcua.connectToOpcuaServer = jest.fn()
-    await opcua.connect()
+    const opcuaSouth = new OPCUA(opcuaConfig, engine)
+    opcuaSouth.connectToOpcuaServer = jest.fn()
+    await opcuaSouth.connect()
 
     expect(databaseService.createConfigDatabase).toBeCalledWith(`${config.engine.caching.cacheFolder}/${opcuaConfig.dataSourceId}.db`)
     expect(databaseService.getConfig).toHaveBeenCalledTimes(1)
-    expect(opcua.lastCompletedAt.every10Second).toEqual(1587640141001)
-    expect(opcua.connectToOpcuaServer).toHaveBeenCalledTimes(1)
+    expect(opcuaSouth.lastCompletedAt.every10Second).toEqual(1587640141001)
+    expect(opcuaSouth.connectToOpcuaServer).toHaveBeenCalledTimes(1)
+  })
+
+  it('should properly connect to OPC UA server', async () => {
+    const expectedOptions = {
+      applicationName: 'OIBus',
+      connectionStrategy: {
+        initialDelay: 1000,
+        maxRetry: 1,
+      },
+      securityMode: Opcua.MessageSecurityMode.None,
+      securityPolicy: Opcua.SecurityPolicy.None,
+      endpoint_must_exist: false,
+    }
+    Opcua.OPCUAClient.create.mockReturnValue({
+      connect: jest.fn(),
+      createSession: jest.fn(),
+    })
+
+    const opcuaSouth = new OPCUA(opcuaConfig, engine)
+    await opcuaSouth.connect()
+
+    expect(Opcua.OPCUAClient.create).toBeCalledWith(expectedOptions)
+    expect(opcuaSouth.client.connect).toBeCalledWith(opcuaConfig.OPCUA.url)
+    expect(opcuaSouth.client.createSession).toBeCalledTimes(1)
+    expect(opcuaSouth.connected).toBeTruthy()
+    expect(setTimeout).not.toBeCalled()
+  })
+
+  it('should properly retry connection to OPC UA server', async () => {
+    const expectedOptions = {
+      applicationName: 'OIBus',
+      connectionStrategy: {
+        initialDelay: 1000,
+        maxRetry: 1,
+      },
+      securityMode: Opcua.MessageSecurityMode.None,
+      securityPolicy: Opcua.SecurityPolicy.None,
+      endpoint_must_exist: false,
+    }
+    Opcua.OPCUAClient.create.mockReturnValue({
+      connect: jest.fn(() => Promise.reject()),
+      createSession: jest.fn(),
+    })
+
+    const opcuaSouth = new OPCUA(opcuaConfig, engine)
+    await opcuaSouth.connect()
+
+    expect(Opcua.OPCUAClient.create).toBeCalledWith(expectedOptions)
+    expect(opcuaSouth.client.connect).toBeCalledWith(opcuaConfig.OPCUA.url)
+    expect(opcuaSouth.client.createSession).not.toBeCalled()
+    expect(opcuaSouth.connected).toBeFalsy()
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), opcuaConfig.OPCUA.retryInterval)
+  })
+
+  it('should properly handle onScan', async () => {
+
+  })
+
+  it('should properly disconnect', async () => {
+    Opcua.OPCUAClient.create.mockReturnValue({
+      connect: jest.fn(),
+      createSession: jest.fn(),
+      disconnect: jest.fn(),
+    })
+
+    const opcuaSouth = new OPCUA(opcuaConfig, engine)
+    await opcuaSouth.connect()
+    opcuaSouth.reconnectTimeout = true
+    opcuaSouth.connected = true
+    opcuaSouth.session = { close: jest.fn() }
+    await opcuaSouth.disconnect()
+
+    expect(clearTimeout).toBeCalled()
+    expect(opcuaSouth.session.close).toBeCalled()
+    expect(opcuaSouth.client.disconnect).toBeCalled()
   })
 })
