faa97afd9a482f42fbb7b42b433ba071a101b025
JFH <jfh@JFH-MacBook-Air.local>
2020-05-31 00:06:05 +0200

    new refactor

--- src/south/FolderScanner/FolderScanner.class.spec.js
+++ src/south/FolderScanner/FolderScanner.class.spec.js
@@ -1,16 +1,4 @@
-const fs = require('fs')
 const FolderScanner = require('./FolderScanner.class')
-
-// Mock database service
-jest.mock('../../services/database.service', () => ({ getFolderScannerModifyTime: jest.fn() }))
-
-// Mock nodejs fs api
-jest.mock('fs')
-
-// Mock logger
-jest.mock('../../engine/Logger.class', () => (function logger() {
-  return { silly: () => jest.fn() }
-}))
 
 const engine = {}
 
@@ -20,9 +8,12 @@ beforeEach(() => {
 
 describe('folder-scanner', () => {
   it('should check file properly', () => {
-    fs.statSync.mockReturnValue({ mtimeMs: 100 })
-
     const dataSource = {
+      dataSourceId: 'fileid',
+      protocol: 'FolderScanner',
+      enabled: true,
+      points: [],
+      scanMode: 'every10Seconds',
       FolderScanner: {
         inputFolder: 'inputFolder',
         preserve: false,
@@ -31,8 +22,7 @@ describe('folder-scanner', () => {
       },
     }
     const folderScanner = new FolderScanner(dataSource, engine)
-    const filenames = ['my-test-file.csv', 'other.csv']
-    const checkResult = folderScanner.keepMatchingFiles(filenames)
-    expect(checkResult).toBe(true)
+    // check created Object (is it a useful test?)
+    expect(folderScanner).toMatchSnapshot()
   })
 })
