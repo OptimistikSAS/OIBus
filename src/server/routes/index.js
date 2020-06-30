12af91445dea44e66ac035d69f590a4b391807d2
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-06-30 10:41:36 +0300

    Issue #753: support files for OIConnect

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,10 +1,18 @@
 const Router = require('@koa/router')
+const multer = require('@koa/multer')
 
 const configController = require('../controllers/configController')
 const logController = require('../controllers/logController')
 const engineController = require('../controllers/engineController')
 
 const router = new Router()
+
+const storage = multer.diskStorage({
+  filename(req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+const upload = multer({ storage })
 
 router.get('/config', configController.getActiveConfiguration)
 router.put('/config', configController.updateConfig)
@@ -13,6 +21,7 @@ router.put('/config/activate', configController.activateConfiguration)
 router.get('/config/schemas/north', engineController.getNorthList)
 router.get('/config/schemas/south', engineController.getSouthList)
 router.post('/engine/addValues', engineController.addValues)
+router.post('/engine/addFile', upload.single('file'), engineController.addFile)
 router.post('/engine/aliveSignal', engineController.aliveSignal)
 router.get('/status', engineController.getStatus)
 router.get('/reload', engineController.reload)
