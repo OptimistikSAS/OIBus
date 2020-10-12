8a5431e8ad2712de338ae32654e4f0686b99967a
jfh <dev@optimistik.fr>
2020-10-13 00:28:15 +0200

    Create queue.class.spec.js

---
+++ src/services/queue.class.spec.js
@@ -0,0 +1,33 @@
+import Queue from './queue.class'
+// Mock logger
+jest.mock('../engine/Logger.class', () => (function logger() {
+  return {
+    info: jest.fn(),
+    error: jest.fn(),
+    silly: jest.fn(),
+  }
+}))
+
+describe('Queue services', () => {
+  jest.useFakeTimers()
+  const functionWithDelay = async (cb) => new Promise((resolve) => setTimeout(() => {
+    cb()
+    resolve()
+  }, 1000))
+
+  const queue = new Queue()
+  it('should run appropriately when using queue', async () => {
+    const callback = jest.fn()
+    queue.add(functionWithDelay, callback)
+    queue.add(functionWithDelay, callback)
+    /** @todo should below be 1? */
+    expect(callback).toHaveBeenCalledTimes(0)
+    // Fast-forward until all timers have been executed
+    jest.runAllTimers()
+    expect(callback).toHaveBeenCalledTimes(1)
+    queue.clear()
+    queue.add(functionWithDelay, callback)
+    jest.runAllTimers()
+    expect(callback).toHaveBeenCalledTimes(1)
+  })
+})
