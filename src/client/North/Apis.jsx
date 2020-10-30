fd2d19df8883cf2735c4bcb7130f369ed48fa4fe
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-10-30 14:00:08 +0200

    Issue #894: OIAnalytics

--- src/client/North/Apis.jsx
+++ src/client/North/Apis.jsx
@@ -2,11 +2,11 @@ import OIConnect from '../../north/oiconnect/OIConnect.schema.jsx'
 import AmazonS3 from '../../north/amazon/AmazonS3.schema.jsx'
 import Console from '../../north/console/Console.schema.jsx'
 import InfluxDB from '../../north/influxdb/InfluxDB.schema.jsx'
-import OIAnalyticsFile from '../../north/oianalyticsfile/OIAnalyticsFile.schema.jsx'
+import OIAnalytics from '../../north/oianalytics/OIAnalytics.schema.jsx'
 import TimescaleDB from '../../north/timescaledb/TimescaleDB.schema.jsx'
 import MongoDB from '../../north/mongodb/MongoDB.schema.jsx'
 import MQTTNorth from '../../north/mqttnorth/MQTTNorth.schema.jsx'
 
-const ApiSchemas = { OIConnect, AmazonS3, Console, InfluxDB, OIAnalyticsFile, TimescaleDB, MongoDB, MQTTNorth }
+const ApiSchemas = { OIConnect, AmazonS3, Console, InfluxDB, OIAnalytics, TimescaleDB, MongoDB, MQTTNorth }
 
 export default ApiSchemas
