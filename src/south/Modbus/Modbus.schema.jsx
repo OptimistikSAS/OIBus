cb20dde804cf41dc7b1c71402605ab527c5feb7b
Nicolas Burger <burgerni10@outlook.fr>
2020-11-05 18:58:33 +0100

    fix(ModBus): communication with modbus slave ok

--- src/south/Modbus/Modbus.schema.jsx
+++ src/south/Modbus/Modbus.schema.jsx
@@ -6,12 +6,24 @@ schema.form = {
   ModbusSettings: {
     type: 'OIbTitle',
     children: (
-      <p>This protocol is in restricted release. Please contact Optimistik</p>
+      <div>
+        <p>The Modbus address scheme follows the Modicon Convention Notation.</p>
+        <ul>
+          <li>Coil = [0x00001 - 0x09999 (=39321)]</li>
+          <li>Discrete Input = [0x10001 (=65537) - 0x19999 (=104857)]</li>
+          <li>Input Register = [0x30001 (=196609) - 0x39999 (=235929)]</li>
+          <li>Holding Register = [0x40001 (=262145) - 0x49999 (=301465)]</li>
+        </ul>
+        <p>
+          When adding a new point, please be sure that your point address (which you can enter in decimal or hexadecimal) includes the Modicon
+          Convention Notation.
+        </p>
+      </div>
     ),
   },
   host: {
     type: 'OIbIpAddress',
-    defaultValue: '',
+    defaultValue: '127.0.0.1',
     help: <div>IP address of the Modbus source</div>,
   },
   port: {
@@ -37,8 +49,8 @@ schema.points = {
   },
   type: {
     type: 'OIbSelect',
-    options: ['boolean', 'number'],
-    defaultValue: 'boolean',
+    options: ['number', 'boolean'],
+    defaultValue: 'number',
   },
 }
 
