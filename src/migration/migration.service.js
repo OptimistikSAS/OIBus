2e4f81dd1182c2ae792d3a22d4e5db8390cedaee
kukukk <szabolcs.balogh@lynxsolutions.eu>
2020-12-18 15:55:08 +0200

    Migration to remove listen scan mode

--- src/migration/migration.service.js
+++ src/migration/migration.service.js
@@ -6,7 +6,7 @@ const Logger = require('../engine/Logger.class')
 
 const logger = new Logger('migration')
 
-const REQUIRED_SCHEMA_VERSION = 17
+const REQUIRED_SCHEMA_VERSION = 18
 const DEFAULT_VERSION = 1
 
 /**
