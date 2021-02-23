32acdecf8fb6d7c73208491e2adc061109965be5
kukukk <szabolcs.balogh@lynxsolutions.eu>
2021-02-23 09:56:47 +0200

    feat(south-opcua-da): Add UI

--- src/client/South/Protocols.jsx
+++ src/client/South/Protocols.jsx
@@ -1,10 +1,11 @@
 import Modbus from '../../south/Modbus/Modbus.schema.jsx'
 import MQTT from '../../south/MQTT/MQTT.schema.jsx'
 import OPCHDA from '../../south/OPCHDA/OPCHDA.schema.jsx'
-import OPCUA from '../../south/OPCUA_HA/OPCUA_HA.schema.jsx'
+import OPCUA_HA from '../../south/OPCUA_HA/OPCUA_HA.schema.jsx'
+import OPCUA_DA from '../../south/OPCUA_DA/OPCUA_DA.schema.jsx'
 import FolderScanner from '../../south/FolderScanner/FolderScanner.schema.jsx'
 import SQLDbToFile from '../../south/SQLDbToFile/SQLDbToFile.schema.jsx'
 
-const ProtocolSchemas = { OPCUA, OPCHDA, MQTT, Modbus, FolderScanner, SQLDbToFile }
+const ProtocolSchemas = { OPCUA: OPCUA_HA, OPCUA_DA, OPCHDA, MQTT, Modbus, FolderScanner, SQLDbToFile }
 
 export default ProtocolSchemas
---
+++ src/south/OPCUA_DA/OPCUA_DA.schema.spec.js
@@ -0,0 +1,4 @@
+const schema = require('./OPCUA_DA.schema.jsx').default
+const testSchema = require('../../services/testSchema.js').default
+
+testSchema(schema)
