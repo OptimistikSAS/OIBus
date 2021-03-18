7c666f60af0d1d31df259f8e3b8f4a3954cd20cd
Attila Suto <attila.suto@lynxsolutions.eu>
2021-03-18 18:42:56 +0200

    feat(south-live): Changed live screen icon to “Status” button on protocol settings screen.

--- src/client/South/StatusButton.jsx
+++ src/client/South/StatusButton.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Button } from 'reactstrap'
 import { FaInfoCircle } from 'react-icons/fa'
 
-const StatusButton = ({ handler }) => (
-  <FaInfoCircle
-    className="oi-icon"
-    onClick={(e) => {
-      e.preventDefault()
-      handler()
-    }}
-  />
+const StatusButton = ({ handler, isIcon }) => (
+  <>
+    {isIcon ? (
+      <FaInfoCircle
+        className="oi-icon"
+        onClick={(e) => {
+          e.preventDefault()
+          handler()
+        }}
+      />
+    ) : (
+      <Button
+        className="inline-button autosize oi-status-button"
+        color="success"
+        onClick={handler}
+        size="sm"
+        outline
+      >
+        Status
+      </Button>
+    )}
+  </>
 )
 
-StatusButton.propTypes = { handler: PropTypes.func.isRequired }
+StatusButton.propTypes = { handler: PropTypes.func.isRequired, isIcon: PropTypes.bool }
+StatusButton.defaultProps = { isIcon: false }
 
 export default StatusButton
