9fb4d70d88e0780c20dd9ff8d561eb1b8fe08681
franck fotso <eden2007uiop@gmail.com>
2021-07-06 14:55:13 +0200

    fix(ui): increase space between icons

--- src/client/components/OIbForm/OIbScanMode.jsx
+++ src/client/components/OIbForm/OIbScanMode.jsx
@@ -16,7 +16,7 @@ const OIbScanMode = ({ label, help, valid, value, name, onChange }) => {
     ? scanGroups.map((e) => e.scanMode)
     : scanModes.map((e) => e.scanMode)
   if (options === null || options.length === 0) {
-    options.push(['']) // empty string if no scan mode on engine
+    options.push('') // empty string if no scan mode on engine
   }
   const defaultValue = options[0]
   let validCheck = valid(value, name, newConfig)
