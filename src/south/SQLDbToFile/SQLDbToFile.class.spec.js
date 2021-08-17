9c05217dc8877d2a9d4d7814723ee0bcdf0d7c2e
kukukk <szabolcs.balogh@lynxsolutions.eu>
2021-08-17 08:38:53 +0300

    fix(bulk): update tests

--- src/south/SQLDbToFile/SQLDbToFile.class.spec.js
+++ src/south/SQLDbToFile/SQLDbToFile.class.spec.js
@@ -67,7 +67,7 @@ describe('sql-db-to-file', () => {
     expect(sqlSouth.lastCompletedAt[sqlConfig.scanMode]).toEqual(new Date('2020-04-23T11:09:01.001Z'))
   })
 
-  it('should properly connect and set lastCompletedAt from startDate', async () => {
+  it('should properly connect and set lastCompletedAt from startTime', async () => {
     databaseService.getConfig.mockReturnValue(null)
 
     const tempConfig = { ...sqlConfig }
@@ -79,7 +79,7 @@ describe('sql-db-to-file', () => {
     expect(tempSqlSouth.lastCompletedAt[sqlConfig.scanMode]).toEqual(new Date('2020-02-02 02:02:02'))
   })
 
-  it('should trigger an error on connection if timezone is invalid and create folder', async () => {
+  it('should trigger an error on connection if timezone is invalid', async () => {
     jest.spyOn(fs, 'existsSync').mockImplementation(() => false)
     jest.spyOn(fs, 'mkdirSync').mockImplementation(() => true)
 
@@ -93,7 +93,7 @@ describe('sql-db-to-file', () => {
     expect(fs.mkdirSync).toHaveBeenCalledTimes(1)
   })
 
-  it('should properly connect and set lastCompletedAt now', async () => {
+  it('should properly connect and set lastCompletedAt to now', async () => {
     const RealDate = Date
     global.Date = jest.fn(() => new RealDate(nowDateString))
 
@@ -127,15 +127,15 @@ describe('sql-db-to-file', () => {
     const entryList4 = [{ name: 'name1' }, { name: 'name2' }, { name: 'name3' }] // no timestamp
 
     sqlSouth.setLastCompletedAt(entryList1, new Date('2020-02-02T02:02:02.000Z')) // with string format
-    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date('2021-03-30 12:30:00.150').toISOString()}`)
+    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date('2021-03-30 12:30:00.150')}`)
     sqlSouth.logger.debug.mockClear()
 
     sqlSouth.setLastCompletedAt(entryList2, new Date('2020-02-02T02:02:02.000Z')) // with number format - no ms
-    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date(1617107400000).toISOString()}`)
+    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date(1617107400000)}`)
     sqlSouth.logger.debug.mockClear()
 
     sqlSouth.setLastCompletedAt(entryList3, new Date('2020-02-02T02:02:02.000Z')) // with date format
-    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date(1617193800000).toISOString()}`)
+    expect(sqlSouth.logger.debug).toHaveBeenCalledWith(`Updating lastCompletedAt to ${new Date(1617193800000)}`)
     sqlSouth.logger.debug.mockClear()
 
     sqlSouth.setLastCompletedAt(entryList4, new Date('2020-02-02T02:02:02.000Z')) // without timestamp
@@ -146,7 +146,7 @@ describe('sql-db-to-file', () => {
     const { timezone } = sqlSouth
     sqlSouth.timezone = undefined
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toHaveBeenCalledWith('Invalid timezone')
     sqlSouth.timezone = timezone
@@ -161,7 +161,7 @@ describe('sql-db-to-file', () => {
     jest.spyOn(mssql, 'ConnectionPool').mockImplementation(() => ({ connect }))
     jest.spyOn(mssql, 'close')
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     const expectedConfig = {
       server: sqlConfig.SQLDbToFile.host,
@@ -179,53 +179,8 @@ describe('sql-db-to-file', () => {
     expect(mssql.close).toHaveBeenCalledTimes(1)
   })
 
-  it('should interact with MS SQL server and not set lastCompletedAt', async () => {
-    sqlSouth.driver = 'mssql'
-    sqlSouth.domain = 'TestDomain'
-    const query = jest.fn(() => ({ recordsets: [[{ timestamp: 'not a timestamp' }]] }))
-    const input = jest.fn(() => ({ query }))
-    const request = jest.fn(() => ({ input }))
-    const connect = jest.fn(() => ({ request }))
-    jest.spyOn(mssql, 'ConnectionPool').mockImplementation(() => ({ connect }))
-    jest.spyOn(mssql, 'close')
-
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
-
-    const expectedConfig = {
-      server: sqlConfig.SQLDbToFile.host,
-      port: sqlConfig.SQLDbToFile.port,
-      user: sqlConfig.SQLDbToFile.username,
-      password: sqlConfig.SQLDbToFile.password,
-      database: sqlConfig.SQLDbToFile.database,
-      connectionTimeout: sqlConfig.SQLDbToFile.connectionTimeout,
-      requestTimeout: sqlConfig.SQLDbToFile.requestTimeout,
-      options: { encrypt: sqlConfig.SQLDbToFile.encryption, trustServerCertificate: true },
-      domain: 'TestDomain',
-    }
-    expect(mssql.ConnectionPool).toHaveBeenCalledWith(expectedConfig)
-    expect(sqlSouth.logger.debug).toHaveBeenCalledWith('lastCompletedAt not used')
-  })
-
-  it('should interact with MS SQL server with no LastCompletedDate and catch error', async () => {
-    sqlSouth.driver = 'mssql'
-    sqlSouth.containsLastCompletedDate = false
-    const query = jest.fn(() => {
-      throw new Error('request error')
-    })
-    const request = jest.fn(() => ({ query }))
-    const connect = jest.fn(() => ({ request }))
-    jest.spyOn(mssql, 'ConnectionPool').mockImplementation(() => ({ connect }))
-    jest.spyOn(mssql, 'close')
-
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
-
-    expect(sqlSouth.logger.error).toHaveBeenCalledWith(new Error('request error'))
-    sqlSouth.containsLastCompletedDate = true
-  })
-
   it('should interact with MySQL server if driver is mysql', async () => {
     sqlSouth.driver = 'mysql'
-    sqlSouth.lastCompletedAt[sqlConfig.scanMode] = new Date('2020-02-02T02:02:02.222Z')
     const connection = {
       execute: jest.fn(() => ([{
         value: 75.2,
@@ -235,7 +190,7 @@ describe('sql-db-to-file', () => {
     }
     jest.spyOn(mysql, 'createConnection').mockImplementation(() => connection)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     const expectedConfig = {
       host: sqlConfig.SQLDbToFile.host,
@@ -247,21 +202,26 @@ describe('sql-db-to-file', () => {
       timezone: 'Z',
     }
     const expectedExecute = {
-      sql: 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > ?',
+      sql: 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > ? AND created_at <= ? LIMIT ?',
       timeout: sqlConfig.SQLDbToFile.requestTimeout,
     }
-    const expectedExecuteParams = [new Date(nowDateString)]
+    const expectedExecuteParams = [
+      new Date('2019-10-03T13:36:38.590Z'),
+      new Date('2019-10-03T15:36:38.590Z'),
+      sqlConfig.SQLDbToFile.maxReturnValues,
+    ]
     expect(mysql.createConnection).toHaveBeenCalledWith(expectedConfig)
     expect(connection.execute).toBeCalledWith(expectedExecute, expectedExecuteParams)
     expect(connection.end).toBeCalledTimes(1)
     // eslint-disable-next-line max-len
-    expect(sqlSouth.logger.debug).toBeCalledWith('Executing "SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > ?" with LastCompletedDate')
+    expect(sqlSouth.logger.debug).toBeCalledWith('Executing "SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > ? AND created_at <= ? LIMIT ?" with StartTime/EndTime/MaxReturnValues')
 
-    sqlSouth.containsLastCompletedDate = false
     mysql.createConnection.mockClear()
     connection.execute.mockClear()
     connection.end.mockClear()
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
+
     expect(mysql.createConnection).toHaveBeenCalledWith(expectedConfig)
     // expect(connection.execute).toBeCalledWith(expectedExecute, expectedExecuteParams)
     expect(connection.execute).toBeCalledTimes(1)
@@ -280,7 +240,7 @@ describe('sql-db-to-file', () => {
     }
     jest.spyOn(mysql, 'createConnection').mockImplementationOnce(() => connection)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toBeCalledWith(new Error('execute error'))
 
@@ -306,7 +266,7 @@ describe('sql-db-to-file', () => {
     }
     Client.mockReturnValue(client)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     const expectedConfig = {
       host: sqlConfig.SQLDbToFile.host,
@@ -316,8 +276,12 @@ describe('sql-db-to-file', () => {
       database: sqlConfig.SQLDbToFile.database,
       query_timeout: sqlConfig.SQLDbToFile.requestTimeout,
     }
-    const expectedQuery = 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > $1'
-    const expectedExecuteParams = [new Date(nowDateString)]
+    const expectedQuery = 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > $1 AND created_at <= $2 LIMIT $3'
+    const expectedExecuteParams = [
+      new Date('2019-10-03T13:36:38.590Z'),
+      new Date('2019-10-03T15:36:38.590Z'),
+      sqlConfig.SQLDbToFile.maxReturnValues,
+    ]
     expect(types.setTypeParser).toBeCalledWith(1114, expect.any(Function))
     expect(Client).toBeCalledWith(expectedConfig)
     expect(client.connect).toBeCalledTimes(1)
@@ -348,7 +312,7 @@ describe('sql-db-to-file', () => {
     }
     Client.mockReturnValueOnce(client)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toBeCalledWith(new Error('query error'))
   })
@@ -363,7 +327,7 @@ describe('sql-db-to-file', () => {
       types: jest.fn(),
     }))
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toBeCalledTimes(2)
   })
@@ -384,15 +348,19 @@ describe('sql-db-to-file', () => {
     }
     jest.spyOn(oracledb, 'getConnection').mockImplementation(() => connection)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     const expectedConfig = {
       user: sqlConfig.SQLDbToFile.username,
       password: sqlConfig.SQLDbToFile.password,
       connectString: `${sqlConfig.SQLDbToFile.host}:${sqlConfig.SQLDbToFile.port}/${sqlConfig.SQLDbToFile.database}`,
     }
-    const expectedQuery = 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > :date1'
-    const expectedExecuteParams = [new Date(nowDateString)]
+    const expectedQuery = 'SELECT created_at AS timestamp, value1 AS temperature FROM oibus_test WHERE created_at > :date1 AND created_at <= :date2 LIMIT :values'
+    const expectedExecuteParams = [
+      new Date('2019-10-03T13:36:38.590Z'),
+      new Date('2019-10-03T15:36:38.590Z'),
+      sqlConfig.SQLDbToFile.maxReturnValues,
+    ]
     expect(oracledb.getConnection).toHaveBeenCalledWith(expectedConfig)
     expect(connection.execute).toBeCalledWith(expectedQuery, expectedExecuteParams)
     expect(connection.close).toBeCalledTimes(1)
@@ -421,7 +389,7 @@ describe('sql-db-to-file', () => {
     }
     jest.spyOn(oracledb, 'getConnection').mockImplementationOnce(() => connection)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toBeCalledWith(new Error('execute error'))
 
@@ -444,7 +412,8 @@ describe('sql-db-to-file', () => {
       close,
     }
     jest.spyOn(sqlite, 'open').mockResolvedValue(database)
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(database.prepare).toBeCalledTimes(1)
     expect(database.close).toBeCalledTimes(1)
@@ -465,7 +434,7 @@ describe('sql-db-to-file', () => {
       throw new Error('test')
     })
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toBeCalledWith(new Error('test'))
   })
@@ -482,7 +451,7 @@ describe('sql-db-to-file', () => {
       throw new Error('test')
     })
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toHaveBeenCalledWith(new Error('test'))
   })
@@ -500,7 +469,7 @@ describe('sql-db-to-file', () => {
 
     sqlSouth.getDataFromMySQL = () => []
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(engine.addFile).not.toBeCalled()
   })
@@ -520,7 +489,7 @@ describe('sql-db-to-file', () => {
     csv.unparse.mockReturnValue(csvContent)
     jest.spyOn(fs, 'writeFileSync').mockImplementation(() => true)
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     const { engineConfig: { caching: { cacheFolder } } } = sqlSouth.engine.configService.getConfig()
     const tmpFolder = path.resolve(cacheFolder, sqlSouth.dataSource.id)
@@ -554,7 +523,7 @@ describe('sql-db-to-file', () => {
     const decompressedCsv = path.join(tmpFolder, 'decompressed.csv')
     sqlSouth.compression = true
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(csv.unparse).toBeCalledTimes(1)
     expect(fs.writeFileSync).toBeCalledWith(targetCsv, csvContent)
@@ -595,7 +564,7 @@ describe('sql-db-to-file', () => {
     fs.mkdirSync(tmpFolder, { recursive: true })
     sqlSouth.compression = true
 
-    await sqlSouth.onScanImplementation(sqlConfig.scanMode)
+    await sqlSouth.historyQuery(sqlConfig.scanMode, new Date('2019-10-03T13:36:38.590Z'), new Date('2019-10-03T15:36:38.590Z'))
 
     expect(sqlSouth.logger.error).toHaveBeenCalledWith(new Error('unlink Error'))
     expect(sqlSouth.logger.error).toHaveBeenCalledWith(new Error('add file error'))
