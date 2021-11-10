fa9c1073589b2a7776723b72365dd190fb53d771
Nicolas Burger <burgerni10@outlook.fr>
2021-11-10 16:21:28 +0100

    fix(log): logs can be disabled with 'none' level

--- src/engine/Logger.class.js
+++ src/engine/Logger.class.js
@@ -11,16 +11,10 @@ const ENGINE_LOG_LEVEL_ENTRY = 'engine'
 class Logger {
   constructor() {
     const defaultFormat = combine(timestamp(), printf((info) => `${info.source} - ${info.timestamp} ${info.level}: ${info.message}`))
-    const consoleFormat = combine(timestamp(), printf((msg) => {
-      const logPrefix = `${msg.source} - ${msg.timestamp}-${msg.level}:`
-      return `${colorize().colorize(msg.level, logPrefix)} ${msg.message}`
-    }))
+
     this.logger = createLogger({
-      level: 'info',
       format: defaultFormat,
-      transports: [
-        new transports.Console({ format: consoleFormat, handleExceptions: true }),
-      ],
+      transports: [],
     })
   }
 
@@ -38,29 +32,49 @@ class Logger {
     }
 
     const { consoleLevel, fileLevel, filename, maxFiles, maxsize, tailable, sqliteLevel, sqliteFilename, sqliteMaxFileSize } = logParameters
-    this.logger.level = consoleLevel
-    this.logger.add(new transports.File({ filename, level: fileLevel, maxsize, maxFiles, tailable, handleExceptions: true }))
-    this.logger.add(new SqliteTransport({ filename: sqliteFilename, level: sqliteLevel, maxFileSize: sqliteMaxFileSize, handleExceptions: true }))
+    if (consoleLevel && consoleLevel !== 'none') {
+      const consoleFormat = combine(timestamp(), printf((msg) => {
+        const logPrefix = `${msg.source} - ${msg.timestamp}-${msg.level}:`
+        return `${colorize().colorize(msg.level, logPrefix)} ${msg.message}`
+      }))
+      this.logger.add(new transports.Console({ format: consoleFormat, level: consoleLevel, handleExceptions: true }))
+    }
+    if (fileLevel && fileLevel !== 'none') {
+      this.logger.add(new transports.File({ filename, level: fileLevel, maxsize, maxFiles, tailable, handleExceptions: true }))
+    }
+    if (sqliteLevel && sqliteLevel !== 'none') {
+      this.logger.add(new SqliteTransport({ filename: sqliteFilename, level: sqliteLevel, maxFileSize: sqliteMaxFileSize, handleExceptions: true }))
+    }
   }
 
   info(message) {
-    this.logger.info(message, { source: this.getSource() })
+    if (this.logger.transports.length > 0) {
+      this.logger.info(message, { source: this.getSource() })
+    }
   }
 
   warn(message) {
-    this.logger.warn(message, { source: this.getSource() })
+    if (this.logger.transports.length > 0) {
+      this.logger.warn(message, { source: this.getSource() })
+    }
   }
 
   error(message) {
-    this.logger.error(message.stack || message, { source: this.getSource() })
+    if (this.logger.transports.length > 0) {
+      this.logger.error(message.stack || message, { source: this.getSource() })
+    }
   }
 
   debug(message) {
-    this.logger.debug(message, { source: this.getSource() })
+    if (this.logger.transports.length > 0) {
+      this.logger.debug(message, { source: this.getSource() })
+    }
   }
 
   silly(message) {
-    this.logger.silly(message, { source: this.getSource() })
+    if (this.logger.transports.length > 0) {
+      this.logger.silly(message, { source: this.getSource() })
+    }
   }
 
   /* eslint-disable-next-line class-methods-use-this */
