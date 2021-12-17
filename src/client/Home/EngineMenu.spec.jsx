2ed7a6bce58fb57c8ae3a18f9463ca0b8ccb0749
franckfotso1 <73475604+franckfotso1@users.noreply.github.com>
2021-12-17 10:45:19 +0100

    feat(home): ui-improvement

--- src/client/Home/EngineMenu.spec.jsx
+++ src/client/Home/EngineMenu.spec.jsx
@@ -111,17 +111,4 @@ describe('EngineMenu', () => {
     expect(mockNavigate).toBeCalledWith('/engine/')
     expect(container).toMatchSnapshot()
   })
-
-  test('go to about section', () => {
-    act(() => {
-      ReactDOM.render(
-        <EngineMenu onRestart={restartFunction} onShutdown={shutdownFunction} />,
-        container,
-      )
-    })
-    Simulate.click(document.getElementById('dropdown-toggle'))
-    Simulate.click(document.getElementById('oi-about'))
-    expect(mockNavigate).toBeCalledWith('/about/')
-    expect(container).toMatchSnapshot()
-  })
 })
