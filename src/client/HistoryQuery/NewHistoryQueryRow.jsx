730fdd5afa0c9be40744200cebbd26dcf2a5a384
Döngölő Zsolt <dongolo.zsolt@kadeno.ro>
2021-12-19 16:09:39 +0200

    feat(bulk-ui): Lint fix

--- src/client/HistoryQuery/NewHistoryQueryRow.jsx
+++ src/client/HistoryQuery/NewHistoryQueryRow.jsx
@@ -1,23 +1,17 @@
-import React from "react";
-import { Button, Form, Col, Row } from "reactstrap";
-import PropTypes from "prop-types";
-import { nanoid } from "nanoid";
-import { OIbSelect } from "../components/OIbForm/index";
-import ProtocolSchemas from "../South/Protocols.jsx";
+import React from 'react'
+import { Button, Form, Col, Row } from 'reactstrap'
+import PropTypes from 'prop-types'
+import { nanoid } from 'nanoid'
+import { OIbSelect } from '../components/OIbForm/index'
+import ProtocolSchemas from '../South/Protocols.jsx'
 
-const NewHistoryQueryRow = ({
-  northHandlers,
-  southHandlers,
-  addQuery,
-  queriesNumber,
-}) => {
-  const [southHandler, setSouthHandler] = React.useState(southHandlers[0]);
-  const [northHandler, setNorthHandler] = React.useState(northHandlers[0]);
-  const { protocol } = southHandler;
-  const schema =
-    protocol === "SQLDbToFile"
-      ? ProtocolSchemas.SQLDbToFile.withDriver(southHandler.SQLDbToFile.driver)
-      : ProtocolSchemas[protocol];
+const NewHistoryQueryRow = ({ northHandlers, southHandlers, addQuery, queriesNumber }) => {
+  const [southHandler, setSouthHandler] = React.useState(southHandlers[0])
+  const [northHandler, setNorthHandler] = React.useState(northHandlers[0])
+  const { protocol } = southHandler
+  const schema = protocol === 'SQLDbToFile'
+    ? ProtocolSchemas.SQLDbToFile.withDriver(southHandler.SQLDbToFile.driver)
+    : ProtocolSchemas[protocol]
 
   /**
    * Creates a new history query with the chosen north and south handler
@@ -28,13 +22,13 @@ const NewHistoryQueryRow = ({
       id: nanoid(),
       name: `${northHandler.name} -> ${southHandler.name}`,
       enabled: false,
-      status: "pending",
+      status: 'pending',
       southId: southHandler.id,
       northId: northHandler.id,
-      ...(schema.points ? { points: [] } : { query: "" }),
+      ...(schema.points ? { points: [] } : { query: '' }),
       order: queriesNumber + 1,
-    });
-  };
+    })
+  }
 
   /**
    * Sets the new value for the south or north handler
@@ -44,15 +38,15 @@ const NewHistoryQueryRow = ({
    */
   const handleChange = (attributeName, value) => {
     switch (attributeName) {
-      case "southHandler":
-        setSouthHandler(value);
-        break;
-      case "northHandler":
+      case 'southHandler':
+        setSouthHandler(value)
+        break
+      case 'northHandler':
       default:
-        setNorthHandler(value);
-        break;
+        setNorthHandler(value)
+        break
     }
-  };
+  }
 
   return (
     <Form>
@@ -80,25 +74,20 @@ const NewHistoryQueryRow = ({
           />
         </Col>
         <Col md="2">
-          <Button
-            size="sm"
-            className="oi-add-history-query"
-            color="primary"
-            onClick={() => handleAddHistoryQuery()}
-          >
+          <Button size="sm" className="oi-add-history-query" color="primary" onClick={() => handleAddHistoryQuery()}>
             Add
           </Button>
         </Col>
       </Row>
     </Form>
-  );
-};
+  )
+}
 
 NewHistoryQueryRow.propTypes = {
   northHandlers: PropTypes.arrayOf(PropTypes.object).isRequired,
   southHandlers: PropTypes.arrayOf(PropTypes.object).isRequired,
   addQuery: PropTypes.func.isRequired,
   queriesNumber: PropTypes.number.isRequired,
-};
+}
 
-export default NewHistoryQueryRow;
+export default NewHistoryQueryRow
