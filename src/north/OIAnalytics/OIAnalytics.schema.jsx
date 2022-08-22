880d96eeb30722567b1ce0d180ae8551290dffdb
burgerni10 <nicolas.burger@optimistik.fr>
2022-08-22 15:33:09 +0200

    fix(oia): adapt OIAnalytics North connector help with the latest API key gen feature #1820

--- src/north/OIAnalytics/OIAnalytics.schema.jsx
+++ src/north/OIAnalytics/OIAnalytics.schema.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { notEmpty, startsWith, combinedValidations } from '../../services/validation.service'
-import screenshot from './screenshot.png'
 import userpass from './userpass.png'
+import oiaApiKeyGen from './oia-api-key-gen.png'
 
 const schema = { name: 'OIAnalytics' }
 schema.form = {
@@ -14,15 +14,25 @@ schema.form = {
         <p>
           OIAnalytics sends the received values/file to OIAnalytics.
         </p>
-        <p>To fill the user and password :</p>
+        <p>To fill the username and password:</p>
         <ul>
           <li>Connect to OIAnalytics using your account</li>
-          <li>Go to &apos;User Management&apos;</li>
-          <li>Select the user &apos;Api&apos;</li>
-          <li>Click on the key icon in the top left corner</li>
-          <img src={screenshot} alt="OIanalytics screenshot" style={{ width: '600px' }} />
-          <li>This will download a text file containing an encoded string</li>
-          <li>The string contain the user name and the password like this : username:password</li>
+          <li>Go to Configuration -&gt; Users</li>
+          <li>
+            Click on the key icon for the user you want to create an API key to.
+            The user must have API access. We suggest a dedicated user with API access only.
+          </li>
+          <li>
+            Create an API key. It will generate an API key. It is the only time you can download and copy the password.
+            Be sure to store it securely somewhere.
+          </li>
+          <img src={oiaApiKeyGen} alt="OIanalytics KeyGen" style={{ width: '600px' }} />
+          <li>
+            If you downloaded the key and its password, the string contain the username (key) and the password like
+            this:
+            {' '}
+            <i>username:password</i>
+          </li>
           <img src={userpass} alt="user password" style={{ width: '300px' }} />
           <li>Fill the encoded user and password in OIBus</li>
         </ul>
