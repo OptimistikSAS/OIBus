feb41aab3e785765b165e6d3df8a92550c2a9115
burgerni10 <nicolas.burger@optimistik.fr>
2022-12-05 11:21:26 +0100

    fix(esm): update some dependencies with tsc compiler

--- src/web-server/middlewares/web-client.js
+++ src/web-server/middlewares/web-client.js
@@ -1,7 +1,7 @@
 import koaSend from 'koa-send'
 
 const serveClient = async (ctx) => {
-  const root = `${__dirname}/../../../web-client`
+  const root = `${__dirname}/../../../../web-client`
   if (ctx.path?.match(/\.(js|js\.map|ico|ttf)$/)) {
     await koaSend(ctx, ctx.path, { root, index: '/index.html' })
   } else {
