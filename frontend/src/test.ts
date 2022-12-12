dc149b71d87dc2a7f3800c4576efcc17a6e5ba94
burgerni10 <nicolas.burger@optimistik.fr>
2022-12-12 12:04:02 +0100

    feat(frontend): create angular frontend

---
+++ frontend/src/test.ts
@@ -0,0 +1,16 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { speculoosMatchers } from 'ngx-speculoos';
+
+beforeEach(() => {
+  jasmine.addMatchers(speculoosMatchers);
+});
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true
+});
