191ceff5dd17fd8e814f6a0930194304613b95d2
ced <cedric@ninja-squad.com>
2022-12-13 16:14:42 +0100

    chore: add ngx-translate

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'oib-home',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
