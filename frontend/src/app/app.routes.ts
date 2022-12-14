1071907cf11aa5b9955b15c5479f845ebc4bedd1
burgerni10 <nicolas.burger@optimistik.fr>
2022-12-14 16:07:58 +0100

    feat(frontend): add all navigable pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,26 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogsComponent } from './logs/logs.component';
 import { AboutComponent } from './about/about.component';
+import { EngineComponent } from './engine/engine.component';
+import { SouthListComponent } from './south-list/south-list.component';
+import { NorthListComponent } from './north-list/north-list.component';
 
 export const ROUTES: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'engine',
+    component: EngineComponent
+  },
+  {
+    path: 'north',
+    component: NorthListComponent
+  },
+  {
+    path: 'south',
+    component: SouthListComponent
   },
   {
     path: 'logs',
