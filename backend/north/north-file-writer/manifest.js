4940b87a15218a9cbb8f8d1458df69b36208c8ec
burgerni10 <nicolas.burger@optimistik.fr>
2023-01-13 16:41:42 +0100

    feat(north): backend repository and endpoints

--- backend/north/north-file-writer/manifest.js
+++ backend/north/north-file-writer/manifest.js
@@ -1,8 +1,35 @@
 export default {
   name: 'FileWriter',
-  category: 'FileIn',
+  category: 'file',
   modes: {
     files: true,
     points: true,
   },
+  settings: [
+    {
+      key: 'outputFolder',
+      type: 'OibText',
+      label: 'Output folder',
+      defaultValue: './output',
+      newRow: true,
+      validators: [{ key: 'required' }],
+
+    },
+    {
+      key: 'prefixFileName',
+      type: 'OibText',
+      label: 'Prefix',
+      defaultValue: '',
+      newRow: true,
+      validators: [{ key: 'minLength', params: { minLength: 1 } }, { key: 'maxLength', params: { maxLength: 255 } }],
+    },
+    {
+      key: 'suffixFileName',
+      type: 'OibText',
+      label: 'Suffix',
+      defaultValue: '',
+      newRow: false,
+      validators: [{ key: 'minLength', params: { minLength: 1 } }, { key: 'maxLength', params: { maxLength: 255 } }],
+    },
+  ],
 }
