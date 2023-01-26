9b37ff3bbbbf35877fe07f1a46870f3368d4e885
kukukk <szabolcs.balogh@lynxsolutions.eu>
2023-01-26 14:42:42 +0200

    feat(web-server): Unit test for IpFilterValidator

--- backend/validators/ip-filter.validator.ts
+++ backend/validators/ip-filter.validator.ts
@@ -1,8 +1,10 @@
-import { ipFilterSchema } from "../engine/oibus-validation-schema";
 import { IpFilterCommandDTO } from "../model/ip-filter.model";
+import Joi from "joi";
 
 export default class IpFilterValidator {
+  constructor(private readonly ipFilterSchema: Joi.ObjectSchema) {}
+
   async validate(ipFilterCommandDTO: IpFilterCommandDTO): Promise<void> {
-    await ipFilterSchema.validateAsync(ipFilterCommandDTO);
+    await this.ipFilterSchema.validateAsync(ipFilterCommandDTO);
   }
 }
