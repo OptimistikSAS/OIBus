89026923738c5b366b9c39a3bba9873c5dadb9ad
kukukk <szabolcs.balogh@lynxsolutions.eu>
2023-01-26 16:38:41 +0200

    feat(web-server): Test IpFilterValidator without mocking Joi

--- backend/validators/ip-filter.validator.spec.ts
+++ backend/validators/ip-filter.validator.spec.ts
@@ -1,34 +1,72 @@
-import Joi from "joi";
-import JoiObjectSchemaMock from "../tests/__mocks__/joi-objectschema.mock";
 import IpFilterValidator from "./ip-filter.validator";
-import { IpFilterCommandDTO } from "../model/ip-filter.model";
+import { ipFilterSchema } from "../engine/oibus-validation-schema";
 
-describe("IP Filter validator", () => {
-  const ipFilterCommandDTO: IpFilterCommandDTO = {
-    address: "ip1",
-    description: "my first ip filter",
-  };
-  const schemaMock = new JoiObjectSchemaMock();
-  const validator: IpFilterValidator = new IpFilterValidator(
-    schemaMock as Joi.ObjectSchema
+interface IpFilterDataProvider {
+  ipFilterCommandDTO: any;
+  isValid: boolean;
+  errorMessage: string | null;
+}
+
+const ipFilterDataProviders: IpFilterDataProvider[] = [
+  {
+    ipFilterCommandDTO: {
+      address1: "missing",
+      description1: "missing",
+    },
+    isValid: false,
+    errorMessage:
+      '"address" is required. "description" is required. "address1" is not allowed. "description1" is not allowed',
+  },
+  {
+    ipFilterCommandDTO: {
+      address: false,
+      description: null,
+    },
+    isValid: false,
+    errorMessage: '"address" must be a string',
+  },
+  {
+    ipFilterCommandDTO: {
+      address: "",
+      description: "",
+    },
+    isValid: false,
+    errorMessage: '"address" is not allowed to be empty',
+  },
+  {
+    ipFilterCommandDTO: {
+      address: "valid",
+      description: "valid",
+      description1: "invalid",
+    },
+    isValid: false,
+    errorMessage: '"description1" is not allowed',
+  },
+  {
+    ipFilterCommandDTO: {
+      address: "valid",
+      description: "valid",
+    },
+    isValid: true,
+    errorMessage: null,
+  },
+];
+
+describe("Ip filter validator", () => {
+  const validator: IpFilterValidator = new IpFilterValidator(ipFilterSchema);
+
+  it.each(ipFilterDataProviders)(
+    `$# Should be valid: $isValid`,
+    async (ipFilterDataProvider) => {
+      if (ipFilterDataProvider.isValid) {
+        await expect(
+          validator.validate(ipFilterDataProvider.ipFilterCommandDTO)
+        ).resolves.not.toThrow();
+      } else {
+        await expect(
+          validator.validate(ipFilterDataProvider.ipFilterCommandDTO)
+        ).rejects.toThrowError(new Error(ipFilterDataProvider.errorMessage));
+      }
+    }
   );
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-    jest.resetAllMocks();
-  });
-
-  it("should throw exception when DTO is invalid", async () => {
-    const expectedError = new Error("Error message");
-    schemaMock.validateAsync.mockRejectedValue(expectedError);
-    await expect(validator.validate(ipFilterCommandDTO)).rejects.toThrowError(
-      expectedError
-    );
-    expect(schemaMock.validateAsync).toBeCalledWith(ipFilterCommandDTO);
-  });
-
-  it("should not throw exception when DTO valid", async () => {
-    await expect(validator.validate(ipFilterCommandDTO)).resolves.not.toThrow();
-    expect(schemaMock.validateAsync).toBeCalledWith(ipFilterCommandDTO);
-  });
 });
