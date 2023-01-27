a6e142cb89c9b4a7955ee27f63259074eb8ca044
burgerni10 <nicolas.burger@optimistik.com>
2023-01-27 11:25:07 +0100

    feat(history-query): frontend list and service for history queries

---
+++ frontend/src/app/services/history-query.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HistoryQueryCommandDTO, HistoryQueryDTO } from '../model/history-query.model';
+
+/**
+ * Service used to interact with the backend for CRUD operations on History queries
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class HistoryQueryService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Get History queries
+   */
+  getHistoryQueries(): Observable<Array<HistoryQueryDTO>> {
+    return this.http.get<Array<HistoryQueryDTO>>(`/api/history-queries`);
+  }
+
+  /**
+   * Get one History query
+   * @param historyQueryId - the ID of the History query
+   */
+  getHistoryQuery(historyQueryId: string): Observable<HistoryQueryDTO> {
+    return this.http.get<HistoryQueryDTO>(`/api/history-queries/${historyQueryId}`);
+  }
+
+  /**
+   * Create a new History query
+   * @param command - the new History query
+   */
+  createHistoryQuery(command: HistoryQueryCommandDTO): Observable<HistoryQueryDTO> {
+    return this.http.post<HistoryQueryDTO>(`/api/history-queries`, command);
+  }
+
+  /**
+   * Update the selected History query
+   * @param historyQueryId - the ID of the History query
+   * @param command - the new values of the selected History query
+   */
+  updateHistoryQuery(historyQueryId: string, command: HistoryQueryCommandDTO) {
+    return this.http.put<void>(`/api/history-queries/${historyQueryId}`, command);
+  }
+
+  /**
+   * Delete the selected History query
+   * @param historyQueryId - the ID of the History query to delete
+   */
+  deleteHistoryQuery(historyQueryId: string) {
+    return this.http.delete<void>(`/api/history-queries/${historyQueryId}`);
+  }
+}
