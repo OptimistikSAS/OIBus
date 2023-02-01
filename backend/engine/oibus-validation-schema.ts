c8b4c1237c176f8a1b3a048cf944321e0fd5e973
kukukk <szabolcs.balogh@lynxsolutions.eu>
2023-02-01 08:34:11 +0200

    feat(web-server): Validate engine parameters

--- backend/engine/oibus-validation-schema.ts
+++ backend/engine/oibus-validation-schema.ts
@@ -1,5 +1,15 @@
 import Joi from "joi";
 
+const logLevels: string[] = [
+  "silent",
+  "error",
+  "warning",
+  "info",
+  "debug",
+  "trace",
+];
+const authenticationTypes = ["none", "basic", "bearer", "api-key"];
+
 const scanModeSchema: Joi.ObjectSchema = Joi.object({
   name: Joi.string().required().min(1),
   description: Joi.string().required().min(0).allow(null),
@@ -16,7 +26,51 @@ const externalSourceSchema: Joi.ObjectSchema = Joi.object({
   reference: Joi.string().required().min(1),
   description: Joi.string().required().min(0).allow(null),
 });
-const engineSchema: Joi.ObjectSchema = Joi.object({});
+const engineSchema: Joi.ObjectSchema = Joi.object({
+  name: Joi.string().required().min(1),
+  port: Joi.number().required().port(),
+  logParameters: Joi.object({
+    console: Joi.options({
+      level: Joi.string().required().allow(logLevels),
+    }),
+    file: Joi.object({
+      level: Joi.string().required().allow(logLevels),
+      maxFileSize: Joi.number().required().min(1),
+      numberOfFiles: Joi.number().required().min(1),
+    }),
+    database: Joi.object({
+      level: Joi.string().required().allow(logLevels),
+      maxNumberOfLogs: Joi.number().required().min(100000),
+    }),
+    loki: Joi.object({
+      level: Joi.string().required().allow(logLevels),
+      interval: Joi.number().required().min(10),
+      address: Joi.string().required().alphanum().min(1),
+      tokenAddress: Joi.string().required().alphanum().min(1),
+      username: Joi.string().required().min(0).allow(null),
+      password: Joi.string().required().min(0).allow(null),
+      proxyId: Joi.string().required().min(0).allow(null),
+    }),
+  }),
+  healthSignal: Joi.object({
+    logging: Joi.options({
+      enabled: Joi.boolean().required(),
+      interval: Joi.number().required().min(10),
+    }),
+    http: Joi.options({
+      enabled: Joi.boolean().required(),
+      interval: Joi.number().required().min(10),
+      verbose: Joi.boolean().required(),
+      address: Joi.string().required().alphanum().min(1),
+      proxyId: Joi.string().required(),
+      authentication: Joi.object({
+        type: Joi.string().required().allow(authenticationTypes),
+        key: Joi.string().required().min(0).allow(null),
+        secret: Joi.string().required().min(0).allow(null),
+      }),
+    }),
+  }),
+});
 const ipFilterSchema: Joi.ObjectSchema = Joi.object({
   address: Joi.string().required().alphanum().min(1),
   description: Joi.string().required().min(0).allow(null),
