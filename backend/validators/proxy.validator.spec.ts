9f9a64efe8bc1f8f428cc97fc9739624f75b5118
burgerni10 <nicolas.burger@optimistik.com>
2023-02-02 15:31:21 +0100

    fix(validators): Adapt validators after typescript and eslint rebase

--- backend/validators/proxy.validator.spec.ts
+++ backend/validators/proxy.validator.spec.ts
@@ -1,6 +1,6 @@
-import JoiValidator from "./joi.validator";
-import { proxySchema } from "../engine/oibus-validation-schema";
-import ValidatorInterface from "./validator.interface";
+import JoiValidator from './joi.validator';
+import { proxySchema } from '../engine/oibus-validation-schema';
+import ValidatorInterface from './validator.interface';
 
 interface DataProvider {
   dto: any;
@@ -11,15 +11,15 @@ interface DataProvider {
 const dataProviders: DataProvider[] = [
   {
     dto: {
-      name1: "missing",
-      description1: "missing",
-      address1: "missing",
-      username1: "missing",
-      password1: "missing",
+      name1: 'missing',
+      description1: 'missing',
+      address1: 'missing',
+      username1: 'missing',
+      password1: 'missing'
     },
     isValid: false,
     errorMessage:
-      '"name" is required. "description" is required. "address" is required. "username" is required. "password" is required. "name1" is not allowed. "description1" is not allowed. "address1" is not allowed. "username1" is not allowed. "password1" is not allowed',
+      '"name" is required. "description" is required. "address" is required. "username" is required. "password" is required. "name1" is not allowed. "description1" is not allowed. "address1" is not allowed. "username1" is not allowed. "password1" is not allowed'
   },
   {
     dto: {
@@ -27,63 +27,55 @@ const dataProviders: DataProvider[] = [
       description: null,
       address: null,
       username: null,
-      password: null,
+      password: null
     },
     isValid: false,
-    errorMessage: '"name" must be a string. "address" must be a string',
+    errorMessage: '"name" must be a string. "address" must be a string'
   },
   {
     dto: {
-      name: "",
-      description: "",
-      address: "",
-      username: "",
-      password: "",
+      name: '',
+      description: '',
+      address: '',
+      username: '',
+      password: ''
     },
     isValid: false,
-    errorMessage:
-      '"name" is not allowed to be empty. "address" is not allowed to be empty',
+    errorMessage: '"name" is not allowed to be empty. "address" is not allowed to be empty'
   },
   {
     dto: {
-      name: "valid",
-      description: "valid",
-      description1: "valid",
-      address: "valid",
-      username: "valid",
-      password: "valid",
+      name: 'valid',
+      description: 'valid',
+      description1: 'valid',
+      address: 'valid',
+      username: 'valid',
+      password: 'valid'
     },
     isValid: false,
-    errorMessage: '"description1" is not allowed',
+    errorMessage: '"description1" is not allowed'
   },
   {
     dto: {
-      name: "valid",
-      description: "valid",
-      address: "valid",
-      username: "valid",
-      password: "valid",
+      name: 'valid',
+      description: 'valid',
+      address: 'valid',
+      username: 'valid',
+      password: 'valid'
     },
     isValid: true,
-    errorMessage: null,
-  },
+    errorMessage: null
+  }
 ];
 
-describe("Proxy validator", () => {
+describe('Proxy validator', () => {
   const validator: ValidatorInterface = new JoiValidator(proxySchema);
 
-  it.each(dataProviders)(
-    `$# Should be valid: $isValid`,
-    async (dataProvider) => {
-      if (dataProvider.isValid) {
-        await expect(
-          validator.validate(dataProvider.dto)
-        ).resolves.not.toThrow();
-      } else {
-        await expect(validator.validate(dataProvider.dto)).rejects.toThrowError(
-          new Error(dataProvider.errorMessage)
-        );
-      }
+  it.each(dataProviders)(`$# Should be valid: $isValid`, async dataProvider => {
+    if (dataProvider.isValid) {
+      await expect(validator.validate(dataProvider.dto)).resolves.not.toThrow();
+    } else {
+      await expect(validator.validate(dataProvider.dto)).rejects.toThrowError(new Error(dataProvider.errorMessage as string));
     }
-  );
+  });
 });
--- backend/validators/scan-mode.validator.spec.ts
+++ backend/validators/scan-mode.validator.spec.ts
@@ -1,6 +1,6 @@
-import JoiValidator from "./joi.validator";
-import { scanModeSchema } from "../engine/oibus-validation-schema";
-import ValidatorInterface from "./validator.interface";
+import JoiValidator from './joi.validator';
+import { scanModeSchema } from '../engine/oibus-validation-schema';
+import ValidatorInterface from './validator.interface';
 
 interface DataProvider {
   dto: any;
@@ -11,69 +11,61 @@ interface DataProvider {
 const dataProviders: DataProvider[] = [
   {
     dto: {
-      name1: "missing",
-      description1: "missing",
-      cron1: "missing",
+      name1: 'missing',
+      description1: 'missing',
+      cron1: 'missing'
     },
     isValid: false,
     errorMessage:
-      '"name" is required. "description" is required. "cron" is required. "name1" is not allowed. "description1" is not allowed. "cron1" is not allowed',
+      '"name" is required. "description" is required. "cron" is required. "name1" is not allowed. "description1" is not allowed. "cron1" is not allowed'
   },
   {
     dto: {
       name: null,
       description: null,
-      cron: null,
+      cron: null
     },
     isValid: false,
-    errorMessage: '"name" must be a string. "cron" must be a string',
+    errorMessage: '"name" must be a string. "cron" must be a string'
   },
   {
     dto: {
-      name: "",
-      description: "",
-      cron: "",
+      name: '',
+      description: '',
+      cron: ''
     },
     isValid: false,
-    errorMessage:
-      '"name" is not allowed to be empty. "cron" is not allowed to be empty',
+    errorMessage: '"name" is not allowed to be empty. "cron" is not allowed to be empty'
   },
   {
     dto: {
-      name: "valid",
-      description: "valid",
-      description1: "valid",
-      cron: "valid",
+      name: 'valid',
+      description: 'valid',
+      description1: 'valid',
+      cron: 'valid'
     },
     isValid: false,
-    errorMessage: '"description1" is not allowed',
+    errorMessage: '"description1" is not allowed'
   },
   {
     dto: {
-      name: "valid",
-      description: "valid",
-      cron: "valid",
+      name: 'valid',
+      description: 'valid',
+      cron: 'valid'
     },
     isValid: true,
-    errorMessage: null,
-  },
+    errorMessage: null
+  }
 ];
 
-describe("Scan mode validator", () => {
+describe('Scan mode validator', () => {
   const validator: ValidatorInterface = new JoiValidator(scanModeSchema);
 
-  it.each(dataProviders)(
-    `$# Should be valid: $isValid`,
-    async (dataProvider) => {
-      if (dataProvider.isValid) {
-        await expect(
-          validator.validate(dataProvider.dto)
-        ).resolves.not.toThrow();
-      } else {
-        await expect(validator.validate(dataProvider.dto)).rejects.toThrowError(
-          new Error(dataProvider.errorMessage)
-        );
-      }
+  it.each(dataProviders)(`$# Should be valid: $isValid`, async dataProvider => {
+    if (dataProvider.isValid) {
+      await expect(validator.validate(dataProvider.dto)).resolves.not.toThrow();
+    } else {
+      await expect(validator.validate(dataProvider.dto)).rejects.toThrowError(new Error(dataProvider.errorMessage as string));
     }
-  );
+  });
 });
