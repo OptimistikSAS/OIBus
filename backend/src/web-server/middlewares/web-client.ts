2b4952d7d70247c3c1b9a61144c615f461a3374a
burgerni10 <nicolas.burger@optimistik.com>
2023-04-04 16:00:45 +0200

    chore: frontend build and release

--- backend/src/web-server/middlewares/web-client.ts
+++ backend/src/web-server/middlewares/web-client.ts
@@ -2,11 +2,11 @@ import koaSend from 'koa-send';
 import { KoaContext } from '../koa';
 
 const serveClient = async (ctx: KoaContext<any, any>) => {
-  const root = `${__dirname}/../../../`;
+  const root = `${__dirname}/../../../../frontend`;
   if (ctx.path?.match(/\.(js|js\.map|ico|ttf|css|png)$/)) {
     await koaSend(ctx, ctx.path, { root });
   } else {
-    await koaSend(ctx, 'frontend/index.html', { root });
+    await koaSend(ctx, 'index.html', { root });
   }
 };
 
