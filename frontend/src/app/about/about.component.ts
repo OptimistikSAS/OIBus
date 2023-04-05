675d72645b58fa47907e79cb8799d53d813986b5
burgerni10 <nicolas.burger@optimistik.com>
2023-04-05 10:49:07 +0200

    feat(about): Create about page

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControlValidationDirective } from '../shared/form-control-validation.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { ValErrorDelayDirective } from '../shared/val-error-delay.directive';
+import { ValidationErrorsComponent } from 'ngx-valdemort';
 
 @Component({
   selector: 'oib-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormControlValidationDirective,
+    ReactiveFormsModule,
+    TranslateModule,
+    ValErrorDelayDirective,
+    ValidationErrorsComponent
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
