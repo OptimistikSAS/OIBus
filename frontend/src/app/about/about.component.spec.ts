ed09737673fecfde7c25f2559ff9d69b23deecfd
burgerni10 <nicolas.burger@optimistik.com>
2023-04-05 10:49:07 +0200

    feat(about): Create about page

--- frontend/src/app/about/about.component.spec.ts
+++ frontend/src/app/about/about.component.spec.ts
@@ -2,10 +2,23 @@ import { TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 import { ComponentTester } from 'ngx-speculoos';
+import { provideTestingI18n } from '../../i18n/mock-i18n';
 
 class AboutComponentTester extends ComponentTester<AboutComponent> {
   constructor() {
     super(AboutComponent);
+  }
+
+  get officialSite() {
+    return this.element('#official-site');
+  }
+
+  get version() {
+    return this.element('#version');
+  }
+
+  get license() {
+    return this.element('#license');
   }
 }
 describe('AboutComponent', () => {
@@ -14,7 +27,7 @@ describe('AboutComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AboutComponent],
-      providers: []
+      providers: [provideTestingI18n()]
     });
 
     tester = new AboutComponentTester();
@@ -22,6 +35,8 @@ describe('AboutComponent', () => {
   });
 
   it('should create', () => {
-    expect(tester).toBeTruthy();
+    expect(tester.officialSite).toContainText('Official site');
+    expect(tester.license).toContainText('License');
+    expect(tester.version).toContainText('Version');
   });
 });
