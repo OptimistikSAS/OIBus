697a8513174d69364952f1362388e0467eeb8872
burgerni10 <nicolas.burger@optimistik.com>
2023-04-07 10:33:11 +0200

    feat(home): Create OIBus Home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'oib-home',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly copyrightYear = new Date().getFullYear();
+}
