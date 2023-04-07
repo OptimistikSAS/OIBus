e96a1b50d2227646be9c6406e5d229f3614fa552
burgerni10 <nicolas.burger@optimistik.com>
2023-04-07 10:33:11 +0200

    feat(home): Create OIBus Home page

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -3,14 +3,15 @@ import { TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
 import { provideTestingI18n } from '../../i18n/mock-i18n';
 import { ComponentTester } from 'ngx-speculoos';
+import { provideRouter } from '@angular/router';
 
 class HomeComponentTester extends ComponentTester<HomeComponent> {
   constructor() {
     super(HomeComponent);
   }
 
-  get title() {
-    return this.element('h1');
+  get homeButtons() {
+    return this.elements('.home-button');
   }
 }
 
@@ -20,14 +21,30 @@ describe('HomeComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideTestingI18n()]
+      providers: [provideTestingI18n(), provideRouter([])]
     });
 
     tester = new HomeComponentTester();
     tester.detectChanges();
   });
 
-  it('should create', () => {
-    expect(tester.title).toHaveText('Welcome to OiBus');
+  it('should display engine home', () => {
+    expect(tester.homeButtons.length).toBe(4);
+    expect(tester.homeButtons[0]).toContainText('Engine');
+  });
+
+  it('should display history queries home', () => {
+    expect(tester.homeButtons.length).toBe(4);
+    expect(tester.homeButtons[2]).toContainText('History Queries');
+  });
+
+  it('should display North home', () => {
+    expect(tester.homeButtons.length).toBe(4);
+    expect(tester.homeButtons[1]).toContainText('North');
+  });
+
+  it('should display South home', () => {
+    expect(tester.homeButtons.length).toBe(4);
+    expect(tester.homeButtons[3]).toContainText('South');
   });
 });
