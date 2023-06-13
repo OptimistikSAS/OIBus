3e83e0900f72864466736e989a97cb55492a069c
cexbrayat <cedric@ninja-squad.com>
2023-06-13 16:05:45 +0200

    refactor: use functional interceptors

--- frontend/src/app/auth/authentication.interceptor.ts
+++ frontend/src/app/auth/authentication.interceptor.ts
@@ -1,21 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { WindowService } from '../shared/window.service';
 
 /**
  * The authentication interceptor uses the token stored locally
  */
-@Injectable()
-export class AuthenticationInterceptor implements HttpInterceptor {
-  constructor(private windowService: WindowService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = this.windowService.getStorageItem('oibus-token');
-    return next.handle(this.cloneRequest(request, token));
-  }
-
-  private cloneRequest(request: HttpRequest<unknown>, token: string | null): HttpRequest<unknown> {
-    return token ? request.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : request;
-  }
-}
+export const authenticationInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn) => {
+  const windowService = inject(WindowService);
+  const token = windowService.getStorageItem('oibus-token');
+  return next(token ? request.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : request);
+};
