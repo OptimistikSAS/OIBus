bdafee349ff890d18ec15e62390a055f9f477024
burgerni10 <nicolas.burger@optimistik.com>
2023-06-26 16:35:43 +0200

    fix(rest): Fix HTTP(S) protocol

--- src/south/south-rest/south-rest.js
+++ src/south/south-rest/south-rest.js
@@ -245,7 +245,7 @@ export default class SouthRest extends SouthConnector {
         timeout: this.connectionTimeout,
         host: this.host,
         port: this.port,
-        protocol: `${this.protocol}:`,
+        protocol: this.protocol,
         path: this.endpoint,
         headers,
       }
