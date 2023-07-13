9cd65aedb940aeb3a88ce72bef57f0955ed4d20e
Nagy Szabolcs <szabolcs.nagy@lynxsolutions.eu>
2023-07-13 10:22:41 +0300

    feat(opcua-da): Add OPCUA-DA connection test

--- backend/src/south/south-opcua-da/south-opcua-da.ts
+++ backend/src/south/south-opcua-da/south-opcua-da.ts
@@ -16,6 +16,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { QueriesLastPoint, TestsConnection } from '../south-interface';
 import { SouthOPCUADAItemSettings, SouthOPCUADASettings } from '../../../../shared/model/south-settings.model';
+import { randomUUID } from 'crypto';
 
 /**
  * Class SouthOPCUADA - Connect to an OPCUA server in DA (Data Access) mode
@@ -63,10 +64,79 @@ export default class SouthOPCUADA
     await this.connectToOpcuaServer();
   }
 
-  // TODO: method needs to be implemented
-  static async testConnection(settings: SouthOPCUADASettings, logger: pino.Logger, _encryptionService: EncryptionService): Promise<void> {
-    logger.trace(`Testing connection`);
-    throw new Error('TODO: method needs to be implemented');
+  static async testConnection(settings: SouthOPCUADASettings, logger: pino.Logger, encryptionService: EncryptionService): Promise<void> {
+    logger.trace(`Testing if OPCUA DA connection settings are correct`);
+
+    const tempCertFolder = `opcua-test-${randomUUID()}`;
+    await initOpcuaCertificateFolders(tempCertFolder);
+    const clientCertificateManager = new OPCUACertificateManager({
+      rootFolder: `${tempCertFolder}/opcua`,
+      automaticallyAcceptUnknownCertificate: true
+    });
+    // Set the state to the CertificateManager to 2 (Initialized) to avoid a call to openssl
+    // It is useful for offline instances of OIBus where downloading openssl is not possible
+    clientCertificateManager.state = 2;
+
+    logger.trace(`Created OPCUA DA temporary folder for certificates: ${tempCertFolder}`);
+
+    let session;
+    try {
+      const { options, userIdentity } = await SouthOPCUADA.createSessionConfigs(
+        settings,
+        clientCertificateManager,
+        encryptionService,
+        'OIBus Connector test'
+      );
+      logger.trace(`Connecting to OPCUA DA on ${settings.url}`);
+      session = await OPCUAClient.createSession(settings.url, userIdentity, options);
+      logger.info(`OPCUA DA connected`);
+    } catch (error: any) {
+      logger.error(`Error while connecting to the OPCUA DA server: ${error}`);
+      const message = error.message;
+
+      if (/BadTcpEndpointUrlInvalid/i.test(message)) {
+        throw new Error('Please check the URL');
+      }
+
+      // Security policy
+      if (/Cannot find an Endpoint matching {1,2}security mode/i.test(message) && settings.securityPolicy) {
+        throw new Error(`Security Policy '${settings.securityPolicy}' is not supported on the server`);
+      }
+      if (/The connection may have been rejected by server/i.test(message) && settings.securityPolicy !== 'None') {
+        throw new Error('Please check if the OIBus certificate has been trusted by the server');
+      }
+
+      // Authentication
+      if (/BadIdentityTokenRejected/i.test(message)) {
+        if (settings.authentication.type === 'basic') {
+          throw new Error('Please check username and password');
+        }
+
+        if (settings.authentication.type === 'cert') {
+          throw new Error('Please check the certificate and key');
+        }
+      }
+      if (error.code === 'ENOENT' && settings.authentication.type === 'cert') {
+        throw new Error(`File '${error.path}' does not exist`);
+      }
+      if (/Failed to read private key/i.test(message)) {
+        const keyPath = path.resolve(settings.authentication.keyFilePath!);
+        throw new Error(`Could not read private key '${keyPath}'`);
+      }
+
+      // Unhandled erros
+      throw new Error('Please check logs');
+    } finally {
+      await fs.rm(tempCertFolder, { recursive: true, force: true });
+      logger.trace('OPCUA DA temporary folder deleted');
+
+      if (session) {
+        await session.close();
+        session = null;
+      }
+
+      logger.trace(`Closed connection to OPCUA DA on ${settings.url}`);
+    }
   }
 
   /**
@@ -74,42 +144,12 @@ export default class SouthOPCUADA
    */
   async connectToOpcuaServer(): Promise<void> {
     try {
-      const options: OPCUAClientOptions = {
-        applicationName: 'OIBus',
-        connectionStrategy: {
-          initialDelay: 1000,
-          maxRetry: 1
-        },
-        securityMode: MessageSecurityMode[this.connector.settings.securityMode],
-        securityPolicy: this.connector.settings.securityPolicy || undefined,
-        endpointMustExist: false,
-        keepSessionAlive: this.connector.settings.keepSessionAlive,
-        keepPendingSessionsOnDisconnect: false,
-        clientName: this.connector.id, // the id of the connector
-        clientCertificateManager: this.clientCertificateManager!
-      };
-
-      let userIdentity: UserIdentityInfo;
-      switch (this.connector.settings.authentication.type) {
-        case 'basic':
-          userIdentity = {
-            type: UserTokenType.UserName,
-            userName: this.connector.settings.authentication.username!,
-            password: await this.encryptionService.decryptText(this.connector.settings.authentication.password!)
-          };
-          break;
-        case 'cert':
-          const certContent = await fs.readFile(path.resolve(this.connector.settings.authentication.certFilePath!));
-          const privateKeyContent = await fs.readFile(path.resolve(this.connector.settings.authentication.keyFilePath!));
-          userIdentity = {
-            type: UserTokenType.Certificate,
-            certificateData: certContent,
-            privateKey: privateKeyContent.toString('utf8')
-          };
-          break;
-        default:
-          userIdentity = { type: UserTokenType.Anonymous };
-      }
+      const { options, userIdentity } = await SouthOPCUADA.createSessionConfigs(
+        this.connector.settings,
+        this.clientCertificateManager!,
+        this.encryptionService,
+        this.connector.id // the id of the connector
+      );
       this.logger.debug(`Connecting to OPCUA_DA on ${this.connector.settings.url}`);
       this.session = await OPCUAClient.createSession(this.connector.settings.url, userIdentity, options);
       this.logger.info(`OPCUA DA ${this.connector.name} connected`);
@@ -172,6 +212,52 @@ export default class SouthOPCUADA
   override async disconnect(): Promise<void> {
     this.disconnecting = true;
     await this.internalDisconnect();
+  }
+
+  static async createSessionConfigs(
+    settings: SouthOPCUADASettings,
+    clientCertificateManager: OPCUACertificateManager,
+    encryptionService: EncryptionService,
+    clientName: string
+  ) {
+    const options: OPCUAClientOptions = {
+      applicationName: 'OIBus',
+      connectionStrategy: {
+        initialDelay: 1000,
+        maxRetry: 1
+      },
+      securityMode: MessageSecurityMode[settings.securityMode],
+      securityPolicy: settings.securityPolicy || undefined,
+      endpointMustExist: false,
+      keepSessionAlive: settings.keepSessionAlive,
+      keepPendingSessionsOnDisconnect: false,
+      clientName,
+      clientCertificateManager
+    };
+
+    let userIdentity: UserIdentityInfo;
+    switch (settings.authentication.type) {
+      case 'basic':
+        userIdentity = {
+          type: UserTokenType.UserName,
+          userName: settings.authentication.username!,
+          password: await encryptionService.decryptText(settings.authentication.password!)
+        };
+        break;
+      case 'cert':
+        const certContent = await fs.readFile(path.resolve(settings.authentication.certFilePath!));
+        const privateKeyContent = await fs.readFile(path.resolve(settings.authentication.keyFilePath!));
+        userIdentity = {
+          type: UserTokenType.Certificate,
+          certificateData: certContent,
+          privateKey: privateKeyContent.toString('utf8')
+        };
+        break;
+      default:
+        userIdentity = { type: UserTokenType.Anonymous };
+    }
+
+    return { options, userIdentity };
   }
 
   /*
