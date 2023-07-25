abca66646dde9562dc779d1d4d9df0a5acbf2f50
burgerni10 <nicolas.burger@optimistik.com>
2023-07-25 15:14:28 +0200

    feat(north): Subscribe from an external source

--- frontend/src/app/north/create-north-subscription-modal/create-north-subscription-modal.component.spec.ts
+++ frontend/src/app/north/create-north-subscription-modal/create-north-subscription-modal.component.spec.ts
@@ -7,6 +7,8 @@ import { SouthConnectorDTO } from '../../../../../shared/model/south-connector.m
 import { NorthConnectorService } from '../../services/north-connector.service';
 import { of } from 'rxjs';
 import { provideI18nTesting } from '../../../i18n/mock-i18n';
+import { ExternalSourceDTO } from '../../../../../shared/model/external-sources.model';
+import { NotificationService } from '../../shared/notification.service';
 
 class CreateNorthSubscriptionModalComponentTester extends ComponentTester<CreateNorthSubscriptionModalComponent> {
   constructor() {
@@ -15,6 +17,14 @@ class CreateNorthSubscriptionModalComponentTester extends ComponentTester<Create
 
   get south() {
     return this.select('#south')!;
+  }
+
+  get externalSource() {
+    return this.select('#external-source')!;
+  }
+
+  get type() {
+    return this.select('#type')!;
   }
 
   get validationErrors() {
@@ -34,33 +44,43 @@ describe('CreateNorthSubscriptionModalComponent', () => {
   let tester: CreateNorthSubscriptionModalComponentTester;
   let fakeActiveModal: NgbActiveModal;
   let northConnectorService: jasmine.SpyObj<NorthConnectorService>;
+  let notificationService: jasmine.SpyObj<NotificationService>;
 
   const southConnectors: Array<SouthConnectorDTO> = [
     { id: 'id1', name: 'South1' } as SouthConnectorDTO,
     { id: 'id2', name: 'South2' } as SouthConnectorDTO
   ];
+  const externalSources: Array<ExternalSourceDTO> = [
+    { id: 'idRef1', reference: 'Reference1' } as ExternalSourceDTO,
+    { id: 'idRef2', reference: 'Reference2' } as ExternalSourceDTO
+  ];
 
   beforeEach(() => {
     fakeActiveModal = createMock(NgbActiveModal);
     northConnectorService = createMock(NorthConnectorService);
+    notificationService = createMock(NotificationService);
 
     TestBed.configureTestingModule({
       providers: [
         provideI18nTesting(),
         { provide: NgbActiveModal, useValue: fakeActiveModal },
-        { provide: NorthConnectorService, useValue: northConnectorService }
+        { provide: NorthConnectorService, useValue: northConnectorService },
+        { provide: NotificationService, useValue: notificationService }
       ]
     });
 
     TestBed.createComponent(DefaultValidationErrorsComponent).detectChanges();
 
     tester = new CreateNorthSubscriptionModalComponentTester();
-    tester.componentInstance.prepareForCreation('northId', southConnectors);
+    tester.componentInstance.prepareForCreation('northId', southConnectors, externalSources);
     northConnectorService.createNorthConnectorSubscription.and.returnValue(of(undefined));
+    northConnectorService.createNorthConnectorExternalSubscription.and.returnValue(of(undefined));
     tester.detectChanges();
   });
 
   it('should have an empty form', () => {
+    expect(tester.type).toBeDefined();
+    expect(tester.externalSource).toBeDefined();
     expect(tester.south).toBeDefined();
   });
 
@@ -72,7 +92,7 @@ describe('CreateNorthSubscriptionModalComponent', () => {
     expect(fakeActiveModal.close).not.toHaveBeenCalled();
   });
 
-  it('should save if valid', fakeAsync(() => {
+  it('should save south subscription if valid', fakeAsync(() => {
     tester.south.selectLabel('South2');
     tester.save.click();
 
@@ -81,8 +101,26 @@ describe('CreateNorthSubscriptionModalComponent', () => {
       name: 'South2'
     } as SouthConnectorDTO;
 
+    expect(tester.type).toContainText('South connector');
     expect(northConnectorService.createNorthConnectorSubscription).toHaveBeenCalledWith('northId', expectedSouth.id);
-    expect(fakeActiveModal.close).toHaveBeenCalledWith(expectedSouth);
+    expect(notificationService.success).toHaveBeenCalledWith('north.subscriptions.south.created', { name: 'South2' });
+    expect(fakeActiveModal.close).toHaveBeenCalled();
+  }));
+
+  it('should save external subscription if valid', fakeAsync(() => {
+    tester.type.selectLabel('External source');
+    tester.externalSource.selectLabel('Reference2');
+    tester.save.click();
+
+    const expectedReference: ExternalSourceDTO = {
+      id: 'idRef2',
+      reference: 'Reference2'
+    } as ExternalSourceDTO;
+
+    expect(tester.type).toContainText('External source');
+    expect(northConnectorService.createNorthConnectorExternalSubscription).toHaveBeenCalledWith('northId', expectedReference.id);
+    expect(notificationService.success).toHaveBeenCalledWith('north.subscriptions.external-source.created', { name: 'Reference2' });
+    expect(fakeActiveModal.close).toHaveBeenCalled();
   }));
 
   it('should cancel', () => {
--- frontend/src/app/north/north-subscriptions/north-subscriptions.component.ts
+++ frontend/src/app/north/north-subscriptions/north-subscriptions.component.ts
@@ -5,13 +5,21 @@ import { Modal, ModalService } from '../../shared/modal.service';
 import { ConfirmationService } from '../../shared/confirmation.service';
 import { NotificationService } from '../../shared/notification.service';
 import { TranslateModule } from '@ngx-translate/core';
-import { SubscriptionDTO } from '../../../../../shared/model/subscription.model';
+import { ExternalSubscriptionDTO, SubscriptionDTO } from '../../../../../shared/model/subscription.model';
 import { NorthConnectorService } from '../../services/north-connector.service';
 import { NorthConnectorDTO } from '../../../../../shared/model/north-connector.model';
 import { SouthConnectorDTO } from '../../../../../shared/model/south-connector.model';
 import { SouthConnectorService } from '../../services/south-connector.service';
 import { CreateNorthSubscriptionModalComponent } from '../create-north-subscription-modal/create-north-subscription-modal.component';
 import { BoxComponent, BoxTitleDirective } from '../../shared/box/box.component';
+import { ExternalSourceService } from '../../services/external-source.service';
+import { ExternalSourceDTO } from '../../../../../shared/model/external-sources.model';
+
+interface Subscription {
+  type: 'south' | 'external-source';
+  externalSubscription?: ExternalSourceDTO;
+  subscription?: SouthConnectorDTO;
+}
 
 @Component({
   selector: 'oib-north-subscriptions',
@@ -23,24 +31,29 @@ import { BoxComponent, BoxTitleDirective } from '../../shared/box/box.component'
 export class NorthSubscriptionsComponent implements OnInit {
   @Input() northConnector: NorthConnectorDTO | null = null;
 
-  northSubscriptions: Array<SubscriptionDTO> = [];
+  subscriptions: Array<Subscription> = [];
   southConnectors: Array<SouthConnectorDTO> = [];
+  externalSources: Array<ExternalSourceDTO> = [];
 
   constructor(
     private confirmationService: ConfirmationService,
     private modalService: ModalService,
     private notificationService: NotificationService,
     private northConnectorService: NorthConnectorService,
+    private externalSourceService: ExternalSourceService,
     private southConnectorService: SouthConnectorService
   ) {}
 
   ngOnInit() {
     combineLatest([
       this.northConnectorService.getNorthConnectorSubscriptions(this.northConnector!.id),
-      this.southConnectorService.list()
-    ]).subscribe(([subscriptions, southConnectors]) => {
-      this.northSubscriptions = subscriptions;
+      this.northConnectorService.getNorthConnectorExternalSubscriptions(this.northConnector!.id),
+      this.southConnectorService.list(),
+      this.externalSourceService.list()
+    ]).subscribe(([subscriptions, externalSubscriptions, southConnectors, externalSources]) => {
       this.southConnectors = southConnectors;
+      this.externalSources = externalSources;
+      this.createSubscriptionList(subscriptions, externalSubscriptions);
     });
   }
 
@@ -50,55 +63,86 @@ export class NorthSubscriptionsComponent implements OnInit {
   addSubscription() {
     const modalRef = this.modalService.open(CreateNorthSubscriptionModalComponent);
     const component: CreateNorthSubscriptionModalComponent = modalRef.componentInstance;
-    component.prepareForCreation(this.northConnector!.id, this.southConnectors);
+
+    component.prepareForCreation(
+      this.northConnector!.id,
+      this.southConnectors.filter(south => !this.subscriptions.some(subscription => subscription.subscription?.id === south.id)),
+      this.externalSources.filter(
+        externalSource => !this.subscriptions.some(subscription => subscription.externalSubscription?.id === externalSource.id)
+      )
+    );
     this.refreshAfterEditScanModeModalClosed(modalRef);
   }
 
   /**
    * Refresh the scan mode list when the scan mode is edited
    */
-  private refreshAfterEditScanModeModalClosed(modalRef: Modal<any>) {
+  private refreshAfterEditScanModeModalClosed(modalRef: Modal<CreateNorthSubscriptionModalComponent>) {
     modalRef.result
       .pipe(
-        tap(southConnector =>
-          this.notificationService.success(`north.subscriptions.created`, {
-            south: southConnector.name
-          })
-        ),
-        switchMap(() => this.northConnectorService.getNorthConnectorSubscriptions(this.northConnector!.id))
+        switchMap(() =>
+          combineLatest([
+            this.northConnectorService.getNorthConnectorSubscriptions(this.northConnector!.id),
+            this.northConnectorService.getNorthConnectorExternalSubscriptions(this.northConnector!.id)
+          ])
+        )
       )
-      .subscribe(subscriptions => (this.northSubscriptions = subscriptions));
+      .subscribe(([subscriptions, externalSubscriptions]) => {
+        this.createSubscriptionList(subscriptions, externalSubscriptions);
+      });
   }
 
   /**
    * Remove a subscription from the connector
    */
-  deleteSubscription(southConnectorId: string) {
-    const southConnector = this.southConnectors.find(south => south.id === southConnectorId);
-    if (!southConnector) return;
+  deleteSubscription(subscription: Subscription) {
     this.confirmationService
       .confirm({
-        messageKey: 'north.subscriptions.confirm-deletion',
-        interpolateParams: { name: southConnector.name }
+        messageKey: `north.subscriptions.${subscription.type}.confirm-deletion`,
+        interpolateParams: {
+          name: subscription.type === 'south' ? subscription.subscription!.name : subscription.externalSubscription!.reference
+        }
       })
       .pipe(
         switchMap(() => {
-          return this.northConnectorService.deleteNorthConnectorSubscription(this.northConnector!.id, southConnector.id);
+          if (subscription.type === 'south') {
+            return this.northConnectorService.deleteNorthConnectorSubscription(this.northConnector!.id, subscription.subscription!.id);
+          }
+          return this.northConnectorService.deleteNorthConnectorExternalSubscription(
+            this.northConnector!.id,
+            subscription.externalSubscription!.id
+          );
         }),
         tap(() =>
-          this.notificationService.success('north.subscriptions.deleted', {
-            name: southConnector.name
+          this.notificationService.success(`north.subscriptions.${subscription.type}.deleted`, {
+            name: subscription.type === 'south' ? subscription.subscription!.name : subscription.externalSubscription!.reference
           })
         ),
-        switchMap(() => this.northConnectorService.getNorthConnectorSubscriptions(this.northConnector!.id))
+        switchMap(() =>
+          combineLatest([
+            this.northConnectorService.getNorthConnectorSubscriptions(this.northConnector!.id),
+            this.northConnectorService.getNorthConnectorExternalSubscriptions(this.northConnector!.id)
+          ])
+        )
       )
-      .subscribe(subscriptions => {
-        this.northSubscriptions = subscriptions;
+      .subscribe(([subscriptions, externalSubscriptions]) => {
+        this.createSubscriptionList(subscriptions, externalSubscriptions);
       });
   }
 
-  getSouthName(northSubscription: SubscriptionDTO): string {
-    const southConnector = this.southConnectors.find(south => south.id === northSubscription);
-    return southConnector?.name || '';
+  private createSubscriptionList(subscriptions: Array<SubscriptionDTO>, externalSubscriptions: Array<ExternalSubscriptionDTO>) {
+    this.subscriptions = [];
+    subscriptions.forEach(subscription => {
+      const southConnector = this.southConnectors.find(south => south.id === subscription);
+      if (southConnector) {
+        this.subscriptions.push({ type: 'south', subscription: southConnector });
+      }
+    });
+    externalSubscriptions.forEach(subscription => {
+      const foundExternalSource = this.externalSources.find(externalSource => externalSource.id === subscription);
+      if (foundExternalSource) {
+        this.subscriptions.push({ type: 'external-source', externalSubscription: foundExternalSource });
+      }
+    });
   }
 }
