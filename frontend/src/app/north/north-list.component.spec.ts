664d1624025a22f48987e56e7c29ba6e125d85b1
burgerni10 <nicolas.burger@optimistik.com>
2023-08-07 11:50:05 +0200

    fix(styles): Fix test on connector / history query lists

--- frontend/src/app/north/north-list.component.spec.ts
+++ frontend/src/app/north/north-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NorthListComponent } from './north-list.component';
-import { ComponentTester, createMock } from 'ngx-speculoos';
+import { ComponentTester, createMock, TestButton } from 'ngx-speculoos';
 import { provideI18nTesting } from '../../i18n/mock-i18n';
 import { NorthConnectorService } from '../services/north-connector.service';
 import { provideRouter } from '@angular/router';
@@ -70,26 +70,26 @@ describe('NorthListComponent', () => {
   it('should display the north  list', () => {
     expect(tester.title).toContainText('North list');
     expect(tester.northList.length).toBe(2);
-    expect(tester.northList[0].elements('td')[0]).toContainText(northConnectors[0].name);
-    expect(tester.northList[0].elements('td')[1]).toContainText(northConnectors[0].type);
-    expect(tester.northList[0].elements('td')[2]).toContainText('active');
+    expect(tester.northList[0].elements('td')[1]).toContainText(northConnectors[0].name);
+    expect(tester.northList[0].elements('td')[2]).toContainText(northConnectors[0].type);
     expect(tester.northList[0].elements('td')[3]).toContainText(northConnectors[0].description);
     expect(tester.northList[0].elements('td')[4].elements('a').length).toBe(3);
     expect(tester.northList[0].elements('td')[4].elements('button').length).toBe(2);
-    expect(tester.northList[1].elements('td')[0]).toContainText(northConnectors[1].name);
-    expect(tester.northList[1].elements('td')[1]).toContainText(northConnectors[1].type);
-    expect(tester.northList[1].elements('td')[2]).toContainText('paused');
+    expect(tester.northList[1].elements('td')[1]).toContainText(northConnectors[1].name);
+    expect(tester.northList[1].elements('td')[2]).toContainText(northConnectors[1].type);
     expect(tester.northList[1].elements('td')[3]).toContainText(northConnectors[1].description);
     expect(tester.northList[1].elements('td')[4].elements('a').length).toBe(3);
     expect(tester.northList[1].elements('td')[4].elements('button').length).toBe(2);
   });
 
   it('should toggle north connector', () => {
-    tester.northList[0].elements('td')[4].elements('button')[0].click();
+    const toggle1 = tester.northList[0].elements('td')[0].elements('.form-check-input')[0] as TestButton;
+    toggle1.click();
     expect(northConnectorService.stopNorth).toHaveBeenCalledWith('id1');
     expect(notificationService.success).toHaveBeenCalledWith('north.stopped', { name: northConnectors[0].name });
 
-    tester.northList[1].elements('td')[4].elements('button')[0].click();
+    const toggle2 = tester.northList[1].elements('td')[0].elements('.form-check-input')[0] as TestButton;
+    toggle2.click();
     expect(northConnectorService.startNorth).toHaveBeenCalledWith('id2');
     expect(notificationService.success).toHaveBeenCalledWith('north.started', { name: northConnectors[1].name });
   });
