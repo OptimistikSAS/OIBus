7e6a0418b5eec34953bf0e35d8b94417dfe854dc
burgerni10 <nicolas.burger@optimistik.com>
2023-09-14 17:51:20 +0200

    fix(south): Remove required attribute for form arrays

--- shared/model/manifest-factory.ts
+++ shared/model/manifest-factory.ts
@@ -6,7 +6,6 @@ export function buildDateTimeFieldsFormControl(dataTypes: Array<DateTimeType>): 
     key: 'dateTimeFields',
     type: 'OibArray',
     label: 'Date time fields',
-    validators: [{ key: 'required' }],
     content: [
       {
         key: 'fieldName',
