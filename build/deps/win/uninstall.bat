7c87dfac955e5769fdce87fc6c3a6071dc914310
burgerni10 <nicolas.burger@optimistik.com>
2023-11-09 15:22:33 +0100

    feat: Adapt bat install scripts

--- build/deps/win/uninstall.bat
+++ build/deps/win/uninstall.bat
@@ -3,14 +3,33 @@
 title Remove OIBus from Windows service
 
 echo Administrator permissions required. Detecting permission...
-
 net session >nul 2>&1
-if %errorLevel% == 0 (
-    echo Stopping OIBus service...
-    START nssm.exe stop OIBus
-
-    echo Removing OIBus service...
-    START nssm.exe remove OIBus confirm
-) else (
+if NOT %errorLevel% == 0 (
     echo No Administrator permission. Please run Command Prompt as Administrator
+    pause
+    goto EOF
 )
+
+set n="OIBus"
+
+:initial
+if "%1"=="" goto done
+echo              %1
+set aux=%1
+if "%aux:~0,1%"=="-" (
+   set nome=%aux:~1,250%
+) else (
+   set "%nome%=%1"
+   set nome=
+)
+shift
+goto initial
+:done
+
+echo Stopping %n% service...
+START nssm.exe stop %n%
+
+echo Removing %n% service...
+START nssm.exe remove %n% confirm
+pause
+
