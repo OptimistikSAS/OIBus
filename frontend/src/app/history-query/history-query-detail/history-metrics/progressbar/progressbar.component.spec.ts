ce2f2132991e770db0134e0414945d300f81ba9e
Nagy Szabolcs <szabolcs.nagy@lynxsolutions.eu>
2024-02-05 15:37:01 +0200

    fix(history): Add additional tests for progress bar component

--- frontend/src/app/history-query/history-query-detail/history-metrics/progressbar/progressbar.component.spec.ts
+++ frontend/src/app/history-query/history-query-detail/history-metrics/progressbar/progressbar.component.spec.ts
@@ -19,4 +19,35 @@ describe('ProgressbarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the width of the progress bar', () => {
+    component.value = 0.5;
+    fixture.detectChanges();
+    // class .progress-bar is a child of ngb-progressbar
+    const progressBar = fixture.nativeElement.querySelector('.progress-bar');
+    expect(progressBar.style.width).toBe('50%');
+  });
+
+  it('should set the width of the progress bar to 100%', () => {
+    component.value = 1;
+    fixture.detectChanges();
+    const progressBar = fixture.nativeElement.querySelector('.progress-bar');
+    expect(progressBar.style.width).toBe('100%');
+  });
+
+  it('should animate the progress bar', () => {
+    component.value = 0.5;
+    component.animated = true;
+    fixture.detectChanges();
+    const progressBar = fixture.nativeElement.querySelector('.progress-bar');
+    expect(progressBar.classList).toContain('progress-bar-animated');
+  });
+
+  it('should take max value into account', () => {
+    component.value = 63;
+    component.max = 180;
+    fixture.detectChanges();
+    const progressBar = fixture.nativeElement.querySelector('.progress-bar');
+    expect(progressBar.style.width).toBe('35%');
+  });
 });
