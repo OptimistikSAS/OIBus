e4d63dfc202146ccb519315e1faae90664c047e8
burgerni10 <nicolas.burger@optimistik.com>
2024-02-08 16:28:58 +0100

    feat(launcher): Backup and rollback data-folder

---
+++ launcher/src/utils.ts
@@ -0,0 +1,34 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+/**
+ * Method to return a delayed promise.
+ */
+export const delay = async (timeout: number): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+
+/**
+ * Create a folder if it does not exist
+ */
+export const createFolder = async (folder: string): Promise<void> => {
+  const folderPath = path.resolve(folder);
+  try {
+    await fs.stat(folderPath);
+  } catch (error) {
+    await fs.mkdir(folderPath, { recursive: true });
+  }
+};
+
+/**
+ * Check if a file exists in async way
+ */
+export const filesExists = async (filePath: string): Promise<boolean> => {
+  try {
+    await fs.stat(filePath);
+  } catch {
+    return false;
+  }
+  return true;
+};
