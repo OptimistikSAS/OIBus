ef274ad378e577917498451220ac05ce12d96b40
Nagy Szabolcs <szabolcs.nagy@lynxsolutions.eu>
2024-04-19 17:05:19 +0300

    feat(connection-service): Create ConnectionService to manage shared connections and implement it for OPCUA

--- backend/src/south/south-interface.ts
+++ backend/src/south/south-interface.ts
@@ -1,3 +1,4 @@
+import { ManagedConnection, ManagedConnectionSettings } from '../service/connection.service';
 import { SouthConnectorItemDTO } from '../../../shared/model/south-connector.model';
 import { Instant } from '../../../shared/model/types';
 
@@ -16,4 +17,22 @@ export interface QueriesHistory {
 export interface QueriesSubscription {
   subscribe(items: Array<SouthConnectorItemDTO>): Promise<void>;
   unsubscribe(items: Array<SouthConnectorItemDTO>): Promise<void>;
+}
+
+export interface DelegatesConnection<TConnection = any> {
+  /**
+   * The connection that is being managed
+   */
+  connection: ManagedConnection<TConnection>;
+  /**
+   * The settings for the connection
+   */
+  connectionSettings: ManagedConnectionSettings<TConnection>;
+  /**
+   * The function to create a new session
+   *
+   * This function will be called by the connection service to create a new session using
+   * the context of the class that implements this interface
+   */
+  createSession(): Promise<TConnection | null>;
 }
