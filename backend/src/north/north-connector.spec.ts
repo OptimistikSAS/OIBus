9ee9453ce144089486d89786ffb05b1f5f2516a4
burgerni10 <nicolas.burger@optimistik.com>
2024-05-13 14:08:33 +0200

    feat(engine/south): addContent replaces addValues and addFile

--- backend/src/north/north-connector.spec.ts
+++ backend/src/north/north-connector.spec.ts
@@ -127,11 +127,6 @@ let configuration: NorthConnectorDTO;
 class TestNorth extends NorthConnector {}
 let north: TestNorth;
 
-class TestNorthWithItems extends NorthConnector {
-  async handleContent(): Promise<void> {}
-}
-let northWithItems: TestNorthWithItems;
-
 describe('NorthConnector enabled', () => {
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -599,117 +594,6 @@ describe('NorthConnector enabled', () => {
   it('should get archive file content', async () => {
     await north.getArchiveFileContent('file1.queue.tmp');
     expect(getArchiveFileContent).toHaveBeenCalledWith('file1.queue.tmp');
-  });
-
-  it('should properly add item', () => {
-    const item: NorthConnectorItemDTO = {
-      id: 'itemId',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-
-    northWithItems.addItem(item);
-    expect(northWithItems['items']).toEqual([item]);
-  });
-
-  it('should properly update item', () => {
-    const item: NorthConnectorItemDTO = {
-      id: 'itemId',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-    northWithItems.addItem = jest.fn();
-    northWithItems.deleteItem = jest.fn();
-
-    northWithItems.updateItem(item, {
-      id: 'itemId',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my updated item',
-      settings: {}
-    });
-
-    expect(northWithItems.deleteItem).toHaveBeenCalledTimes(1);
-    expect(northWithItems.addItem).toHaveBeenCalledTimes(1);
-  });
-
-  it('should properly delete item', () => {
-    const item1: NorthConnectorItemDTO = {
-      id: 'itemId1',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-    const item2: NorthConnectorItemDTO = {
-      id: 'itemId2',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-    const item3: NorthConnectorItemDTO = {
-      id: 'itemId3',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-    const item4: NorthConnectorItemDTO = {
-      id: 'itemId4',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-
-    northWithItems['items'] = [item1, item2, item3, item4];
-
-    northWithItems.deleteItem(item1);
-    northWithItems.deleteItem(item1); // Second deletion should do nothing
-    expect(northWithItems['items']).toEqual([item2, item3, item4]);
-
-    northWithItems.deleteAllItems();
-    expect(northWithItems['items']).toEqual([]);
-  });
-
-  xit('should do nothing with items if does not support items mode', () => {
-    const item: NorthConnectorItemDTO = {
-      id: 'itemId',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my item',
-      settings: {}
-    };
-
-    // Adding items
-    const pushSpy = jest.spyOn(north['items'], 'push');
-    north.addItem(item);
-    expect(pushSpy).not.toHaveBeenCalled();
-
-    // Updating items
-    north.addItem = jest.fn();
-    north.deleteItem = jest.fn();
-
-    north.updateItem(item, {
-      id: 'itemId',
-      enabled: true,
-      connectorId: 'id',
-      name: 'my updated item',
-      settings: {}
-    });
-
-    expect(north.deleteItem).not.toHaveBeenCalled();
-    expect(north.addItem).not.toHaveBeenCalled();
-
-    // Deleting items
-    const filterSpy = jest.spyOn(north['items'], 'filter');
-    north.deleteItem(item);
-    expect(filterSpy).not.toHaveBeenCalled();
   });
 });
 
