c48ddba59bc410d2014804a420157321b6511843
burgerni10 <nicolas.burger@optimistik.com>
2024-06-02 11:44:16 +0200

    fix(migration): move transformer migration into 3.5 set change

--- backend/src/db/entity-migrations/v3.5.0.ts
+++ backend/src/db/entity-migrations/v3.5.0.ts
@@ -9,6 +9,11 @@ const HISTORY_QUERIES_TABLE = 'history_queries';
 const HISTORY_ITEMS_TABLE = 'history_items';
 const EXTERNAL_SOURCES_TABLE = 'external_sources';
 const EXTERNAL_SUBSCRIPTION_TABLE = 'external_subscription';
+const HISTORY_TRANSFORMERS_TABLE = 'history_transformers';
+const SOUTH_TRANSFORMERS_TABLE = 'south_transformers';
+const NORTH_TRANSFORMERS_TABLE = 'north_transformers';
+const TRANSFORMERS_TABLE = 'transformers';
+const NORTH_ITEMS_TABLE = 'north_items';
 
 export async function up(knex: Knex): Promise<void> {
   await removeNorthOIBusConnectors(knex);
@@ -16,6 +21,11 @@ export async function up(knex: Knex): Promise<void> {
   await removeExternalSubscriptions(knex);
   await updateSouthConnectorsTable(knex);
   await updateHistoryQueriesTable(knex);
+  await createNorthItemsTable(knex);
+  await createTransformersTable(knex);
+  await createNorthTransformersTable(knex);
+  await createSouthTransformersTable(knex);
+  await createHistoryTransformersTable(knex);
 }
 
 async function removeNorthOIBusConnectors(knex: Knex): Promise<void> {
@@ -60,6 +70,60 @@ async function updateSouthConnectorsTable(knex: Knex): Promise<void> {
 async function updateHistoryQueriesTable(knex: Knex): Promise<void> {
   await knex.schema.alterTable(HISTORY_QUERIES_TABLE, table => {
     table.boolean('south_shared_connection').defaultTo(false);
+  });
+}
+
+async function createNorthItemsTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(NORTH_ITEMS_TABLE, table => {
+    table.uuid('id').primary();
+    table.timestamps(false, true);
+    table.uuid('connector_id').notNullable().references('id').inTable(NORTH_CONNECTORS_TABLE);
+    table.string('name').notNullable();
+    table.boolean('enabled').notNullable();
+    table.json('settings').notNullable();
+
+    table.unique(['connector_id', 'name']);
+  });
+}
+
+async function createTransformersTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(TRANSFORMERS_TABLE, table => {
+    table.uuid('id').primary();
+    table.timestamps(false, true);
+    table.string('name').notNullable().unique();
+    table.string('description');
+    table.string('input_type').notNullable();
+    table.string('output_type').notNullable();
+    table.string('code').notNullable();
+    table.string('file_regex');
+  });
+}
+
+async function createNorthTransformersTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(NORTH_TRANSFORMERS_TABLE, table => {
+    table.uuid('north_id').notNullable().references('id').inTable(NORTH_CONNECTORS_TABLE);
+    table.uuid('transformer_id').notNullable().references('id').inTable(TRANSFORMERS_TABLE);
+
+    table.unique(['north_id', 'transformer_id']);
+  });
+}
+
+async function createSouthTransformersTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(SOUTH_TRANSFORMERS_TABLE, table => {
+    table.uuid('south_id').notNullable().references('id').inTable(SOUTH_CONNECTORS_TABLE);
+    table.uuid('transformer_id').notNullable().references('id').inTable(TRANSFORMERS_TABLE);
+
+    table.unique(['south_id', 'transformer_id']);
+  });
+}
+
+async function createHistoryTransformersTable(knex: Knex): Promise<void> {
+  await knex.schema.createTable(HISTORY_TRANSFORMERS_TABLE, table => {
+    table.uuid('history_id').notNullable().references('id').inTable(HISTORY_QUERIES_TABLE);
+    table.enum('connector_type', ['south', 'north']).notNullable();
+    table.uuid('transformer_id').notNullable().references('id').inTable(TRANSFORMERS_TABLE);
+
+    table.unique(['history_id', 'connector_type', 'transformer_id']);
   });
 }
 
