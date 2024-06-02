77e9e3866edacbe9b963cb01164b3407bd0ced37
burgerni10 <nicolas.burger@optimistik.com>
2024-06-02 11:44:16 +0200

    fix(migration): move transformer migration into 3.4 set change

--- backend/src/db/entity-migrations/v3.4.0-rework-north.ts
+++ backend/src/db/entity-migrations/v3.4.0-rework-north.ts
@@ -1,19 +1,62 @@
 import { Knex } from 'knex';
+import { HISTORY_QUERIES_TABLE } from '../../repository/history-query.repository';
+import { NORTH_CONNECTORS_TABLE } from '../../repository/north-connector.repository';
+import path from 'node:path';
+import { filesExists } from '../../service/utils';
+import fs from 'node:fs/promises';
+import { HISTORY_ITEMS_TABLE } from '../../repository/history-query-item.repository';
 import { NORTH_ITEMS_TABLE } from '../../repository/north-item.repository';
 import { TRANSFORMERS_TABLE } from '../../repository/transformer.repository';
 import { NORTH_TRANSFORMERS_TABLE } from '../../repository/north-transformer.repository';
 import { SOUTH_TRANSFORMERS_TABLE } from '../../repository/south-transformer.repository';
-import { HISTORY_TRANSFORMERS_TABLE } from '../../repository/history-transformer.repository';
-import { NORTH_CONNECTORS_TABLE } from '../../repository/north-connector.repository';
 import { SOUTH_CONNECTORS_TABLE } from '../../repository/south-connector.repository';
-import { HISTORY_QUERIES_TABLE } from '../../repository/history-query.repository';
+import { HISTORY_TRANSFORMERS_TABLE } from '../../repository/history-transformer.repository';
+
+const EXTERNAL_SOURCES_TABLE = 'external_sources';
+const EXTERNAL_SUBSCRIPTION_TABLE = 'external_subscription';
 
 export async function up(knex: Knex): Promise<void> {
+  await removeNorthOIBusConnectors(knex);
+  await removeNorthOIBusHistoryQueries(knex);
+  await removeExternalSubscriptions(knex);
   await createNorthItemsTable(knex);
   await createTransformersTable(knex);
   await createNorthTransformersTable(knex);
   await createSouthTransformersTable(knex);
   await createHistoryTransformersTable(knex);
+}
+
+async function removeNorthOIBusConnectors(knex: Knex): Promise<void> {
+  const northConnectors: Array<{ id: string; name: string }> = await knex(NORTH_CONNECTORS_TABLE)
+    .select('id', 'name')
+    .where('type', 'oibus');
+  await knex(NORTH_CONNECTORS_TABLE).delete().where('type', 'oibus');
+  for (const north of northConnectors) {
+    const baseFolder = path.resolve('./cache/data-stream', `north-${north.id}`);
+    if (await filesExists(baseFolder)) {
+      await fs.rm(baseFolder, { recursive: true });
+    }
+  }
+}
+
+async function removeNorthOIBusHistoryQueries(knex: Knex): Promise<void> {
+  const historyQueries: Array<{ id: string; name: string }> = await knex(HISTORY_QUERIES_TABLE)
+    .select('id', 'name')
+    .where('north_type', 'oibus');
+
+  for (const history of historyQueries) {
+    await knex(HISTORY_ITEMS_TABLE).delete().where('history_id', history.id);
+    const baseFolder = path.resolve('./cache/history-query', `history-${history.id}`);
+    if (await filesExists(baseFolder)) {
+      await fs.rm(baseFolder, { recursive: true });
+    }
+  }
+  await knex(HISTORY_QUERIES_TABLE).delete().where('north_type', 'oibus');
+}
+
+async function removeExternalSubscriptions(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists(EXTERNAL_SUBSCRIPTION_TABLE);
+  await knex.schema.dropTableIfExists(EXTERNAL_SOURCES_TABLE);
 }
 
 async function createNorthItemsTable(knex: Knex): Promise<void> {
