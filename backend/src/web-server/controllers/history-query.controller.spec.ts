17e6f626d839b5d79583107239a4a21631c911ea
burgerni10 <nicolas.burger@optimistik.com>
2024-07-16 12:27:07 +0200

    fix(web-server): fix controller after rebase

--- backend/src/web-server/controllers/history-query.controller.spec.ts
+++ backend/src/web-server/controllers/history-query.controller.spec.ts
@@ -688,9 +688,9 @@ describe('History query controller', () => {
       historyQuery.northSettings,
       northManifest.settings
     );
-    expect(ctx.app.reloadService.onDeleteHistoryItem).toHaveBeenCalledWith('id', 'id1', false);
+    expect(ctx.app.reloadService.onDeleteHistoryItem).toHaveBeenCalledWith('id', 'id1');
 
-    expect(ctx.app.reloadService.onUpdateHistoryQuerySettings).toHaveBeenCalledWith('id', historyQueryCommand, true);
+    expect(ctx.app.reloadService.onUpdateHistoryQuerySettings).toHaveBeenCalledWith('id', historyQueryCommand);
     expect(ctx.noContent).toHaveBeenCalled();
   });
 
@@ -743,7 +743,7 @@ describe('History query controller', () => {
     );
     expect(ctx.app.reloadService.onDeleteHistoryItem).toHaveBeenCalledWith('id', 'id1');
 
-    expect(ctx.app.reloadService.onUpdateHistoryQuerySettings).toHaveBeenCalledWith('id', historyQueryCommand);
+    expect(ctx.app.reloadService.onUpdateHistoryQuerySettings).toHaveBeenCalledWith('id', ctx.request.body.historyQuery);
     expect(ctx.app.reloadService.historyEngine.resetCache).toHaveBeenCalledTimes(1);
     expect(ctx.noContent).toHaveBeenCalled();
 
