0372c3b130aa0294726ac1357baec0ad1bb55a92
burgerni10 <nicolas.burger@optimistik.com>
2024-09-10 17:30:22 +0200

    chore: refactor subscription model, controller, services and tests

--- backend/src/web-server/controllers/subscription.controller.ts
+++ backend/src/web-server/controllers/subscription.controller.ts
@@ -1,48 +1,32 @@
 import { KoaContext } from '../koa';
 import { SubscriptionDTO } from '../../../../shared/model/subscription.model';
+import { toSubscriptionDTO } from '../../model/subscription.model';
 
 export default class SubscriptionController {
-  async getNorthSubscriptions(ctx: KoaContext<void, Array<SubscriptionDTO>>): Promise<void> {
-    const northConnector = ctx.app.repositoryService.northConnectorRepository.findById(ctx.params.northId);
-    if (!northConnector) {
-      return ctx.notFound();
+  async findByNorth(ctx: KoaContext<void, Array<SubscriptionDTO>>): Promise<void> {
+    try {
+      const subscriptions = await ctx.app.subscriptionService.findByNorth(ctx.params.northId);
+      return ctx.ok(subscriptions.map(subscription => toSubscriptionDTO(subscription)));
+    } catch (error: any) {
+      return ctx.badRequest(error.message);
     }
-
-    const subscriptions = ctx.app.repositoryService.subscriptionRepository.list(ctx.params.northId);
-    ctx.ok(subscriptions);
   }
 
-  async createNorthSubscription(ctx: KoaContext<void, SubscriptionDTO>): Promise<void> {
-    const northConnector = ctx.app.repositoryService.northConnectorRepository.findById(ctx.params.northId);
-    if (!northConnector) {
-      return ctx.notFound();
+  async create(ctx: KoaContext<void, void>): Promise<void> {
+    try {
+      await ctx.app.subscriptionService.create(ctx.params.northId, ctx.params.southId);
+      return ctx.noContent();
+    } catch (error: any) {
+      return ctx.badRequest(error.message);
     }
-
-    const southConnector = ctx.app.repositoryService.southConnectorRepository.findById(ctx.params.southId);
-    if (!southConnector) {
-      return ctx.notFound();
-    }
-
-    if (ctx.app.repositoryService.subscriptionRepository.checkSubscription(ctx.params.northId, ctx.params.southId)) {
-      return ctx.throw(409, 'Subscription already exists');
-    }
-
-    await ctx.app.reloadService.onCreateNorthSubscription(ctx.params.northId, ctx.params.southId);
-    return ctx.noContent();
   }
 
-  async deleteNorthSubscription(ctx: KoaContext<void, void>): Promise<void> {
-    const northConnector = ctx.app.repositoryService.northConnectorRepository.findById(ctx.params.northId);
-    if (!northConnector) {
-      return ctx.notFound();
+  async delete(ctx: KoaContext<void, void>): Promise<void> {
+    try {
+      await ctx.app.subscriptionService.delete(ctx.params.northId, ctx.params.southId);
+      return ctx.noContent();
+    } catch (error: any) {
+      return ctx.badRequest(error.message);
     }
-
-    const southConnector = ctx.app.repositoryService.southConnectorRepository.findById(ctx.params.southId);
-    if (!southConnector) {
-      return ctx.notFound();
-    }
-
-    await ctx.app.reloadService.onDeleteNorthSubscription(ctx.params.northId, ctx.params.southId);
-    return ctx.noContent();
   }
 }
