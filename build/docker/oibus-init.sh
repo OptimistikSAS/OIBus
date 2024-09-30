893606c86f6d9cf46d8d213fa9c73088ebd64547
Nicolas Noël <nicolas.noel@optimistik.fr>
2024-09-30 12:22:35 +0200

    build: publish docker images to ghcr.io

--- build/docker/oibus-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl --location --request POST 'http://localhost:2223/api/ip-filters' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "address": "*",
-    "description": "All"
-}' \
--u "admin:pass"
