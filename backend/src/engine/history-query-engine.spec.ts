c50a4da92d07d075eb88ea202903ab8862df9a21
burgerni10 <nicolas.burger@optimistik.com>
2024-10-17 15:07:32 +0200

    fix(engine): refactor engine to reload on config change

--- backend/src/engine/history-query-engine.spec.ts
+++ backend/src/engine/history-query-engine.spec.ts
@@ -1,23 +1,23 @@
 import PinoLogger from '../tests/__mocks__/service/logger/logger.mock';
 import SouthServiceMock from '../tests/__mocks__/service/south-service.mock';
 import NorthServiceMock from '../tests/__mocks__/service/north-service.mock';
-import HistoryQueryServiceMock from '../tests/__mocks__/service/history-query-service.mock';
 
 import SouthService from '../service/south.service';
 import NorthService from '../service/north.service';
 
 import pino from 'pino';
-import EncryptionService from '../service/encryption.service';
-import EncryptionServiceMock from '../tests/__mocks__/service/encryption-service.mock';
 import HistoryQueryEngine from './history-query-engine';
-import HistoryQueryService from '../service/history-query.service';
-import { PassThrough } from 'node:stream';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { filesExists } from '../service/utils';
-import RepositoryService from '../service/repository.service';
-import RepositoryServiceMock from '../tests/__mocks__/service/repository-service.mock';
 import testData from '../tests/utils/test-data';
+import SouthConnectorMetricsRepository from '../repository/logs/south-connector-metrics.repository';
+import SouthMetricsRepositoryMock from '../tests/__mocks__/repository/log/south-metrics-repository.mock';
+import NorthMetricsRepositoryMock from '../tests/__mocks__/repository/log/north-metrics-repository.mock';
+import NorthConnectorMetricsRepository from '../repository/logs/north-connector-metrics.repository';
+import HistoryQueryRepository from '../repository/config/history-query.repository';
+import HistoryQueryRepositoryMock from '../tests/__mocks__/repository/config/history-query-repository.mock';
+import HistoryQueryMock from '../tests/__mocks__/history-query.mock';
 
 jest.mock('../service/south.service');
 jest.mock('../service/north.service');
@@ -33,52 +33,84 @@ const anotherLogger: pino.Logger = new PinoLogger();
 
 const southService: SouthService = new SouthServiceMock();
 const northService: NorthService = new NorthServiceMock();
-const encryptionService: EncryptionService = new EncryptionServiceMock('', '');
-const repositoryService: RepositoryService = new RepositoryServiceMock();
-const historyQueryService: HistoryQueryService = new HistoryQueryServiceMock();
-
-let engine: HistoryQueryEngine;
+const southMetricsRepository: SouthConnectorMetricsRepository = new SouthMetricsRepositoryMock();
+const northMetricsRepository: NorthConnectorMetricsRepository = new NorthMetricsRepositoryMock();
+const historyQueryRepository: HistoryQueryRepository = new HistoryQueryRepositoryMock();
 
 describe('HistoryQueryEngine', () => {
+  let engine: HistoryQueryEngine;
+
+  const mockedHistoryQuery1 = new HistoryQueryMock(
+    testData.historyQueries.list[0],
+    southService,
+    northService,
+    southMetricsRepository,
+    northMetricsRepository,
+    historyQueryRepository,
+    'baseFolder',
+    logger
+  );
+  const mockedHistoryQuery2 = new HistoryQueryMock(
+    testData.historyQueries.list[1],
+    southService,
+    northService,
+    southMetricsRepository,
+    northMetricsRepository,
+    historyQueryRepository,
+    'baseFolder',
+    logger
+  );
+
   beforeEach(async () => {
-    jest.resetAllMocks();
+    jest.clearAllMocks();
     jest.useFakeTimers().setSystemTime(new Date(testData.constants.dates.FAKE_NOW));
-
     (logger.child as jest.Mock).mockReturnValue(logger);
-    (historyQueryService.findAll as jest.Mock).mockReturnValue(testData.historyQueries.list);
-    (historyQueryService.findById as jest.Mock).mockImplementation((id: string) =>
-      testData.historyQueries.list.find(element => element.id === id)
-    );
 
-    engine = new HistoryQueryEngine(encryptionService, northService, southService, repositoryService, historyQueryService, logger);
+    engine = new HistoryQueryEngine(logger);
   });
 
   it('it should start connectors and stop all', async () => {
-    await engine.start();
-    expect(historyQueryService.findAll as jest.Mock).toHaveBeenCalledTimes(1);
-    expect(logger.child).toHaveBeenCalledWith({
-      scopeType: 'history-query',
-      scopeId: testData.historyQueries.list[0].id,
-      scopeName: testData.historyQueries.list[0].name
-    });
+    await engine.start([mockedHistoryQuery1, mockedHistoryQuery2]);
 
+    expect(engine.logger).toBeDefined();
+    expect(engine.baseFolder).toBeDefined();
     expect(engine.getHistoryDataStream('bad id')).toEqual(null);
-    expect(engine.getHistoryDataStream(testData.historyQueries.list[0].id)).toEqual(expect.any(PassThrough));
+    expect(engine.getHistoryDataStream(testData.historyQueries.list[0].id)).toEqual(mockedHistoryQuery1.getMetricsDataStream());
 
+    (mockedHistoryQuery1.start as jest.Mock).mockImplementationOnce(() => Promise.reject(new Error('start fail')));
     await engine.startHistoryQuery(testData.historyQueries.list[0].id);
-    await engine.createHistoryQuery(testData.historyQueries.list[0]);
+    expect(logger.error).toHaveBeenCalledWith(
+      `Error while starting History Query "${mockedHistoryQuery1.settings.name}" (${mockedHistoryQuery1.settings.id}): start fail`
+    );
     await engine.stop();
+    expect(mockedHistoryQuery1.stop).toHaveBeenCalledTimes(1);
     await engine.stopHistoryQuery('anotherId');
+    expect(mockedHistoryQuery1.stop).toHaveBeenCalledTimes(1);
     await engine.resetCache('anotherId');
+    expect(mockedHistoryQuery1.resetCache).not.toHaveBeenCalled();
+  });
+
+  it('should reload history query', async () => {
+    await engine.start([mockedHistoryQuery1]);
+
+    await engine.reloadHistoryQuery({ ...testData.historyQueries.list[0], id: 'bad id' }, true);
+    await engine.reloadHistoryQuery(testData.historyQueries.list[0], false);
+
+    expect(mockedHistoryQuery1.stop).toHaveBeenCalled();
+    expect(mockedHistoryQuery1.resetCache).not.toHaveBeenCalled();
+    expect(mockedHistoryQuery1.setLogger).toHaveBeenCalledTimes(1);
+    expect(logger.child).toHaveBeenCalledTimes(1);
+    expect(mockedHistoryQuery1.start).toHaveBeenCalled();
+  });
+
+  it('should not start history query if not set', async () => {
+    await engine.startHistoryQuery('bad id');
+    expect(logger.trace).toHaveBeenCalledWith(`History Query "bad id" not set`);
   });
 
   it('should properly set logger', async () => {
-    await engine.start();
-
-    (historyQueryService.findById as jest.Mock).mockReturnValueOnce(undefined);
-
+    await engine.start([mockedHistoryQuery1, mockedHistoryQuery2]);
     engine.setLogger(anotherLogger);
-    expect(historyQueryService.findById).toHaveBeenCalledWith(testData.historyQueries.list[1].id);
     expect(anotherLogger.child).toHaveBeenCalledWith({
       scopeType: 'history-query',
       scopeId: testData.historyQueries.list[1].id,
@@ -90,21 +122,22 @@ describe('HistoryQueryEngine', () => {
     (filesExists as jest.Mock).mockImplementationOnce(() => Promise.resolve(true)).mockImplementationOnce(() => Promise.resolve(false));
     const stopHistoryQuerySpy = jest.spyOn(engine, 'stopHistoryQuery');
 
-    await engine.start();
-
-    const historyId = testData.historyQueries.list[0].id;
-    const name = testData.historyQueries.list[0].name;
-    const baseFolder = path.resolve('./cache/history-query', `history-${historyId}`);
-    await engine.deleteHistoryQuery(historyId, name);
+    await engine.start([mockedHistoryQuery1, mockedHistoryQuery2]);
+    const baseFolder = path.resolve('./cache/history-query', `history-${testData.historyQueries.list[0].id}`);
+    await engine.deleteHistoryQuery(testData.historyQueries.list[0]);
 
     expect(stopHistoryQuerySpy).toHaveBeenCalled();
     expect(filesExists).toHaveBeenCalledWith(baseFolder);
     expect(fs.rm).toHaveBeenCalledWith(baseFolder, { recursive: true });
-    expect(logger.trace).toHaveBeenCalledWith(`Deleting base folder "${baseFolder}" of History query "${name}" (${historyId})`);
-    expect(logger.info).toHaveBeenCalledWith(`Deleted History query "${name}" (${historyId})`);
+    expect(logger.trace).toHaveBeenCalledWith(
+      `Deleting base folder "${baseFolder}" of History query "${testData.historyQueries.list[0].name}" (${testData.historyQueries.list[0].id})`
+    );
+    expect(logger.info).toHaveBeenCalledWith(
+      `Deleted History query "${testData.historyQueries.list[0].name}" (${testData.historyQueries.list[0].id})`
+    );
 
     // Removing again should not call rm, meaning that it's actually removed
-    await engine.deleteHistoryQuery(historyId, name);
+    await engine.deleteHistoryQuery(testData.historyQueries.list[0]);
     expect(fs.rm).toHaveBeenCalledTimes(1);
   });
 
@@ -112,21 +145,23 @@ describe('HistoryQueryEngine', () => {
     (filesExists as jest.Mock).mockImplementation(() => Promise.resolve(true));
     const stopHistoryQuerySpy = jest.spyOn(engine, 'stopHistoryQuery');
 
-    await engine.start();
+    await engine.start([mockedHistoryQuery1, mockedHistoryQuery2]);
 
     const error = new Error(`Can't remove folder`);
     (fs.rm as jest.Mock).mockImplementation(() => {
       throw error;
     });
 
-    const historyId = testData.historyQueries.list[0].id;
-    const name = testData.historyQueries.list[0].name;
-    const baseFolder = path.resolve('./cache/history-query', `history-${historyId}`);
-    await engine.deleteHistoryQuery(historyId, name);
+    const baseFolder = path.resolve('./cache/history-query', `history-${testData.historyQueries.list[0].id}`);
+    await engine.deleteHistoryQuery(testData.historyQueries.list[0]);
 
     expect(stopHistoryQuerySpy).toHaveBeenCalled();
     expect(filesExists).toHaveBeenCalled();
-    expect(logger.trace).toHaveBeenCalledWith(`Deleting base folder "${baseFolder}" of History query "${name}" (${historyId})`);
-    expect(logger.error).toHaveBeenCalledWith(`Unable to delete History query "${name}" (${historyId}) base folder: ${error}`);
+    expect(logger.trace).toHaveBeenCalledWith(
+      `Deleting base folder "${baseFolder}" of History query "${testData.historyQueries.list[0].name}" (${testData.historyQueries.list[0].id})`
+    );
+    expect(logger.error).toHaveBeenCalledWith(
+      `Unable to delete History query "${testData.historyQueries.list[0].name}" (${testData.historyQueries.list[0].id}) base folder: ${error}`
+    );
   });
 });
