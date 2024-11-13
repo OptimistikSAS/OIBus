5cdc906db33acff183cd02f51062713813cf8e71
burgerni10 <nicolas.burger@optimistik.com>
2024-11-13 19:20:46 +0100

    chore: rename db into migration folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import DataStreamEngine from './engine/data-stream-engine';
 import HistoryQueryEngine from './engine/history-query-engine';
 import HistoryQueryService from './service/history-query.service';
 import OIBusService from './service/oibus.service';
-import { migrateCrypto, migrateEntities, migrateDataFolder, migrateLogsAndMetrics, migrateSouthCache } from './db/migration-service';
+import { migrateCrypto, migrateEntities, migrateDataFolder, migrateLogsAndMetrics, migrateSouthCache } from './migration/migration-service';
 import OIAnalyticsCommandService from './service/oia/oianalytics-command.service';
 import OianalyticsRegistrationService from './service/oia/oianalytics-registration.service';
 import ConnectionService from './service/connection.service';
