33e0629d518b0b087b1639d84c6e58b318add366
burgerni10 <nicolas.burger@optimistik.com>
2024-11-14 10:50:50 +0100

    fix(oianalytics): fix translation of OIAnalytics commands and remove legacy UPGRADE command

--- frontend/src/app/shared/oibus-command-type-enum.pipe.spec.ts
+++ frontend/src/app/shared/oibus-command-type-enum.pipe.spec.ts
@@ -4,7 +4,20 @@ import { OibusCommandTypeEnumPipe } from './oibus-command-type-enum.pipe';
 describe('OibusCommandTypeEnumPipe', () => {
   it('should translate OIBus command type', () => {
     testEnumPipe(OibusCommandTypeEnumPipe, {
-      UPGRADE: 'Upgrade'
+      'update-version': 'Upgrade version',
+      'restart-engine': 'Restart',
+      'regenerate-cipher-keys': 'Regenerate cipher keys',
+      'update-engine-settings': 'Update engine settings',
+      'create-scan-mode': 'Create scan mode',
+      'update-scan-mode': 'Update scan mode',
+      'delete-scan-mode': 'Delete scan mode',
+      'create-south': 'Create south connector',
+      'update-south': 'Update south connector',
+      'delete-south': 'Delete south connector',
+      'create-or-update-south-items-from-csv': 'Load south items from CSV',
+      'create-north': 'Create north connector',
+      'update-north': 'Update north connector',
+      'delete-north': 'Delete north connector'
     });
   });
 });
