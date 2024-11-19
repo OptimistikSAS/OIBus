713bcda6985797fbe647e1e61cf992c88558d4ab
burgerni10 <nicolas.burger@optimistik.com>
2024-11-19 15:48:08 +0100

    feat(oianalytics-commands): display oianalytics commands in a modal

---
+++ frontend/src/app/engine/oia-module/oibus-command-details-modal/oia-command-details-modal.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { TranslateModule } from '@ngx-translate/core';
+import { JsonPipe } from '@angular/common';
+import { DatetimePipe } from '../../../shared/datetime.pipe';
+import { OibusCommandTypeEnumPipe } from '../../../shared/oibus-command-type-enum.pipe';
+import { BooleanEnumPipe } from '../../../shared/boolean-enum.pipe';
+import { OIBusCommandDTO } from '../../../../../../backend/shared/model/command.model';
+
+@Component({
+  selector: 'oib-oia-command-details-modal',
+  templateUrl: './oia-command-details-modal.component.html',
+  styleUrl: './oia-command-details-modal.component.scss',
+  imports: [TranslateModule, OibusCommandTypeEnumPipe, DatetimePipe, JsonPipe, BooleanEnumPipe],
+  standalone: true
+})
+export class OiaCommandDetailsModalComponent {
+  private activeModal = inject(NgbActiveModal);
+
+  command: OIBusCommandDTO | null = null;
+
+  constructor() {}
+
+  prepare(command: OIBusCommandDTO) {
+    this.command = command;
+  }
+
+  close() {
+    this.activeModal.dismiss();
+  }
+}
