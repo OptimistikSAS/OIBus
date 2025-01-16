7e6b9825c9bf7ddf8e010965eccedcdc8122b799
Nagy Szabolcs <szabolcs.nagy@lynxsolutions.eu>
2025-01-16 20:08:56 +0200

    feat(frontend): add multiple ways to display item test results

--- frontend/src/app/south/south-item-test/south-item-test.component.spec.ts
+++ frontend/src/app/south/south-item-test/south-item-test.component.spec.ts
@@ -10,19 +10,18 @@ import { delay, of, throwError } from 'rxjs';
 import { HttpErrorResponse, provideHttpClient } from '@angular/common/http';
 import { provideI18nTesting } from '../../../i18n/mock-i18n';
 import { SouthItemTestComponent } from './south-item-test.component';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SouthItemSettings, SouthSettings } from '../../../../../backend/shared/model/south-settings.model';
 import { HistoryQueryItemCommandDTO } from '../../../../../backend/shared/model/history-query.model';
 import { HistoryQueryService } from '../../services/history-query.service';
 import { DateTime, Settings } from 'luxon';
 import { provideDatepicker } from '../../shared/datepicker.providers';
-import { OibCodeBlockComponent } from '../../shared/form/oib-code-block/oib-code-block.component';
-import { OIBusRawContent, OIBusTimeValueContent } from '../../../../../backend/shared/model/engine.model';
-import { getMessageFromHttpErrorResponse } from '../../shared/error-interceptor.service';
+import { OIBusContent, OIBusRawContent, OIBusTimeValueContent } from '../../../../../backend/shared/model/engine.model';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   template: ` <oib-south-item-test
+    #testedComponent
     [type]="type"
     [entityId]="entityId"
     [item]="item"
@@ -33,6 +32,10 @@ import { TranslateService } from '@ngx-translate/core';
   imports: [SouthItemTestComponent]
 })
 class TestComponent {
+  @ViewChild('testedComponent') testedComponent!: SouthItemTestComponent<'south'>;
+
+  constructor(private translate: TranslateService) {}
+
   type!: string;
   entityId!: string;
 
@@ -121,23 +124,24 @@ class SouthItemTestComponentTester extends ComponentTester<TestComponent> {
     return this.button('#cancel-test-item')!;
   }
 
-  get southItemTestComponent() {
-    return this.component(SouthItemTestComponent);
+  get testingSettingsForm() {
+    return this.componentInstance.testedComponent.testingSettingsForm!;
   }
 
-  get testingSettingsForm() {
-    return this.component(SouthItemTestComponent).testingSettingsForm!;
+  get translate() {
+    return this.componentInstance['translate'];
+  }
+
+  get testResultViewComponent() {
+    return this.componentInstance.testedComponent.testResultView();
   }
 }
 
 describe('SouthItemTestComponent', () => {
   let tester: SouthItemTestComponentTester;
+  const testResultOIBusContent: OIBusContent = { type: 'raw', filePath: '/file/path' };
   const southConnectorService: jasmine.SpyObj<SouthConnectorService> = createMock(SouthConnectorService);
   const historyQueryService: jasmine.SpyObj<HistoryQueryService> = createMock(HistoryQueryService);
-  let codeBlockSpy: {
-    changeLanguage: jasmine.SpyObj<OibCodeBlockComponent['changeLanguage']>;
-    writeValue: jasmine.SpyObj<OibCodeBlockComponent['writeValue']>;
-  };
 
   // Force DateTime to use the date mocked by Jasmine
   // This works becaus we mock the date before creating the component,
@@ -163,21 +167,23 @@ describe('SouthItemTestComponent', () => {
     jasmine.clock().install();
     jasmine.clock().mockDate(new Date('2024-01-01T00:10:00.000Z'));
 
-    southConnectorService.testItem.and.returnValue(of({ type: 'raw', filePath: '/file/path' }));
-    historyQueryService.testSouthItem.and.returnValue(of({ type: 'raw', filePath: '/file/path' }));
+    southConnectorService.testItem.calls.reset();
+    southConnectorService.testItem.and.returnValue(of(testResultOIBusContent));
+
+    historyQueryService.testSouthItem.calls.reset();
+    historyQueryService.testSouthItem.and.returnValue(of(testResultOIBusContent));
 
     tester = new SouthItemTestComponentTester();
     tester.changeType('south');
     tester.changeEntityId('southId');
     tester.detectChanges();
 
-    // Even though the codeblock does not work in the testing env
-    // we can spy on it's calls
-    const codeBlockComponent = tester.component(OibCodeBlockComponent);
-    codeBlockSpy = {
-      changeLanguage: spyOn(codeBlockComponent, 'changeLanguage'),
-      writeValue: spyOn(codeBlockComponent, 'writeValue')
-    };
+    spyOn(tester.testResultViewComponent, 'displayInfo');
+    spyOn(tester.testResultViewComponent, 'displayResult');
+    spyOn(tester.testResultViewComponent, 'displayError');
+    spyOn(tester.testResultViewComponent, 'changeDisplayMode');
+    spyOn(tester.testResultViewComponent, 'displayLoading');
+    tester.detectChanges();
   });
 
   afterEach(() => {
@@ -229,9 +235,8 @@ describe('SouthItemTestComponent', () => {
         expectedSettings
       );
 
-      expect(codeBlockSpy.changeLanguage).toHaveBeenCalledWith('plaintext');
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith('/file/path');
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(testResultOIBusContent);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     });
 
     it(`[${testCase.type}] should test the item with custom history settings`, () => {
@@ -269,9 +274,8 @@ describe('SouthItemTestComponent', () => {
         expectedSettings
       );
 
-      expect(codeBlockSpy.changeLanguage).toHaveBeenCalledWith('plaintext');
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith('/file/path');
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(testResultOIBusContent);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     });
 
     it(`[${testCase.type}] should test the item without history`, () => {
@@ -294,9 +298,8 @@ describe('SouthItemTestComponent', () => {
         expectedSettings
       );
 
-      expect(codeBlockSpy.changeLanguage).toHaveBeenCalledWith('plaintext');
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith('/file/path');
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(testResultOIBusContent);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     });
 
     it(`[${testCase.type}] should cancel the item test`, fakeAsync(() => {
@@ -336,9 +339,9 @@ describe('SouthItemTestComponent', () => {
       tester.detectChanges();
 
       // values are not displayed
-      expect(codeBlockSpy.changeLanguage).not.toHaveBeenCalled();
-      expect(codeBlockSpy.writeValue).toHaveBeenCalled();
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayInfo).toHaveBeenCalled();
+      expect(tester.testResultViewComponent.displayResult).not.toHaveBeenCalled();
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     }));
 
     it(`[${testCase.type}] should not cancel the item test when it's already finished`, fakeAsync(() => {
@@ -379,9 +382,8 @@ describe('SouthItemTestComponent', () => {
       tester.cancelTestButton.click();
 
       // values are properly displayed
-      expect(codeBlockSpy.changeLanguage).toHaveBeenCalledWith('plaintext');
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith('/file/path');
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(testResultOIBusContent);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     }));
 
     it(`[${testCase.type}] should handle test request error`, () => {
@@ -412,13 +414,9 @@ describe('SouthItemTestComponent', () => {
         expectedSettings
       );
 
-      const translate = TestBed.inject(TranslateService);
-
-      expect(codeBlockSpy.changeLanguage).not.toHaveBeenCalled();
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith(
-        `${translate.instant('south.test-item.editor-message.error')}:\n${getMessageFromHttpErrorResponse(error)}`
-      );
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayError).toHaveBeenCalled();
+      expect(tester.testResultViewComponent.displayResult).not.toHaveBeenCalled();
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     });
 
     it(`[${testCase.type}] should handle time values as well`, () => {
@@ -444,9 +442,59 @@ describe('SouthItemTestComponent', () => {
         expectedSettings
       );
 
-      expect(codeBlockSpy.changeLanguage).toHaveBeenCalledWith('json');
-      expect(codeBlockSpy.writeValue).toHaveBeenCalledWith(JSON.stringify(result.content));
-      expect(tester.southItemTestComponent.isTestRunning).toBeFalse();
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(result);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
     });
+
+    it(`[${testCase.type}] should be able to change display mode`, () => {
+      tester.changeSupportsHistory(false);
+      tester.changeType(testCase.type);
+      tester.changeEntityId(testCase.entityId);
+      tester.detectChanges();
+
+      const result = { type: 'time-values', content: [{ pointId: 'pointId' }] } as OIBusTimeValueContent;
+      testCase.service.testItem.and.returnValue(of(result));
+
+      tester.testButton.click();
+
+      // Mocking display mode changes after test data has been received
+      tester.componentInstance.testedComponent.availableDisplayModes = ['table', 'raw'];
+      tester.componentInstance.testedComponent.currentDisplayMode = 'table';
+      tester.detectChanges();
+      tester.button('#view-dropdown')!.click();
+      tester.button('.dropdown-item:not(.active)')!.click();
+      tester.detectChanges();
+
+      expect(tester.testResultViewComponent.changeDisplayMode).toHaveBeenCalledWith('raw');
+      expect(tester.testResultViewComponent.displayResult).toHaveBeenCalledWith(result);
+      expect(tester.componentInstance.testedComponent.isTestRunning).toBeFalse();
+    });
+
+    it(`[${testCase.type}] should not retrieve settings if form is invalid`, () => {
+      tester.changeSupportsHistory(true);
+      tester.changeType(testCase.type);
+      tester.changeEntityId(testCase.entityId);
+      tester.detectChanges();
+
+      tester.startTimeDate.fillWith('2025-01-01');
+      tester.endTimeDate.fillWith('2020-01-01');
+
+      tester.testButton.click();
+
+      expect(tester.testingSettingsForm?.valid).toBeFalse();
+      expect(tester.componentInstance.testedComponent.testingSettings).toEqual({});
+      expect(testCase.service.testItem).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should not make request if the type provided is unsupported', () => {
+    tester.changeType('unsupported');
+    tester.changeEntityId('entityId');
+    tester.detectChanges();
+
+    tester.testButton.click();
+    expect(tester.testResultViewComponent.displayResult).not.toHaveBeenCalled();
+    expect(southConnectorService.testItem).not.toHaveBeenCalled();
+    expect(historyQueryService.testSouthItem).not.toHaveBeenCalled();
   });
 });
