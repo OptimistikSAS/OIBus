10f85e543bcbc407ead1186e3997ab9c924e730f
burgerni10 <nicolas.burger@optimistik.com>
2025-01-16 10:53:26 +0100

    feat(oianalytics): retrieve commands for ip filters and certificates

--- backend/src/web-server/middlewares/ip-filter.ts
+++ backend/src/web-server/middlewares/ip-filter.ts
@@ -4,10 +4,10 @@ import { KoaContext } from '../koa';
  * Filter connection based on IP addresses. IPv4 and IPv6 are both accepted
  * Return ipFilter middleware
  */
-const ipFilter = (ignoreIpFilters: boolean) => async (ctx: KoaContext<unknown, unknown>, next: () => void) => {
+const ipFilter = (ignoreIpFilters: boolean) => async (ctx: KoaContext<unknown, unknown>, next: () => Promise<void>) => {
   const allowed =
     ignoreIpFilters ||
-    ctx.app.ipFilters.whiteList.some(ipToTest => {
+    ctx.app.whiteList.some(ipToTest => {
       const formattedRegext = `^${ipToTest.replace(/\\/g, '\\\\').replace(/\./g, '\\.').replace(/\*$/g, '.*')}$`;
       return new RegExp(formattedRegext).test(ctx.request.ip);
     });
