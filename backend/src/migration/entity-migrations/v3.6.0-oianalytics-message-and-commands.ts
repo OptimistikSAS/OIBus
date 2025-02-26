d215be53cf1a6c0c347024cc386589ba0c0c69b5
burgerni10 <nicolas.burger@optimistik.com>
2025-02-26 11:52:28 +0100

    feat(engine): rework cache structure with content

--- backend/src/migration/entity-migrations/v3.6.0-oianalytics-message-and-commands.ts
+++ backend/src/migration/entity-migrations/v3.6.0-oianalytics-message-and-commands.ts
@@ -3,11 +3,15 @@ import { Knex } from 'knex';
 const OIANALYTICS_MESSAGE_TABLE = 'oianalytics_messages';
 const COMMANDS_TABLE = 'commands';
 const REGISTRATIONS_TABLE = 'registrations';
+const NORTH_CONNECTORS_TABLE = 'north_connectors';
+const HISTORY_QUERIES_TABLE = 'history_queries';
 
 export async function up(knex: Knex): Promise<void> {
   await updateOIAMessageTable(knex);
   await updateOIACommandTable(knex);
   await updateRegistrationSettings(knex);
+  await updateNorthConnectors(knex);
+  await updateHistoryQueries(knex);
 }
 
 async function updateOIAMessageTable(knex: Knex): Promise<void> {
@@ -41,6 +45,26 @@ async function updateRegistrationSettings(knex: Knex): Promise<void> {
     command_test_history_south_connection: true,
     command_test_history_south_item: true,
     command_test_history_north_connection: true
+  });
+}
+
+async function updateNorthConnectors(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(NORTH_CONNECTORS_TABLE, table => {
+    table.integer('caching_run_min_delay');
+  });
+
+  await knex(NORTH_CONNECTORS_TABLE).update({
+    caching_run_min_delay: 200
+  });
+}
+
+async function updateHistoryQueries(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(HISTORY_QUERIES_TABLE, table => {
+    table.integer('caching_run_min_delay');
+  });
+
+  await knex(HISTORY_QUERIES_TABLE).update({
+    caching_run_min_delay: 200
   });
 }
 
