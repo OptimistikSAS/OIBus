ef5fa62b3cfad412366784fcf3058e4627ac0af4
Kovács Bálint Hunor <hunor.kovacs@lynxsolutions.eu>
2025-05-23 11:51:44 +0300

    feat(backend): WIP added unique and singleTrue reference fields

--- backend/src/south/south-sqlite/manifest.ts
+++ backend/src/south/south-sqlite/manifest.ts
@@ -83,7 +83,7 @@ const manifest: SouthConnectorManifest = {
             translationKey: 'south.items.sqlite.date-time-fields.field-name',
             type: 'OibText',
             defaultValue: '',
-            validators: [{ key: 'required' }],
+            validators: [{ key: 'required' }, { key: 'unique' }],
             displayInViewMode: true
           },
           {
@@ -92,7 +92,7 @@ const manifest: SouthConnectorManifest = {
             type: 'OibCheckbox',
             defaultValue: false,
             displayInViewMode: true,
-            validators: [{ key: 'required' }]
+            validators: [{ key: 'required' }, { key: 'singleTrue' }]
           },
           {
             key: 'type',
