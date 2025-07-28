f143ef38425f400004c03091b47b28d8d14b5a83
Optimistik SAS <optimistiksas@users.noreply.github.com>
2025-07-28 20:27:42 +0000

    deploy: cec1d0665ccc63dadd5748fd99a9fe404a3b7429

---
+++ assets/js/5e95c892.bd74ad9d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdoc_oibus=self.webpackChunkdoc_oibus||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var u=r(4164),c=r(7559),a=r(5500),o=r(2831),d=r(3850),n=r(4848);function t(e){return(0,n.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,o.v)(e.route.routes)})})}}}]);
