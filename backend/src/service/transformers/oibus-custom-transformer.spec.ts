30305df99eecfd9b67f8089bfc408fe67ae19055
burgerni10 <nicolas.burger@optimistik.com>
2025-08-20 19:06:55 +0200

    feat(transformer): create and update custom transformer from engine

---
+++ backend/src/service/transformers/oibus-custom-transformer.spec.ts
@@ -0,0 +1,89 @@
+import SandboxServiceMock from '../../tests/__mocks__/service/sandbox-service.mock';
+import { Readable } from 'stream';
+import pino from 'pino';
+import PinoLogger from '../../tests/__mocks__/service/logger/logger.mock';
+import testData from '../../tests/utils/test-data';
+import OIBusCustomTransformer from './oibus-custom-transformer';
+import { CustomTransformer } from '../../model/transformer.model';
+import SandboxService from '../sandbox.service';
+import { OIBusTimeValue } from '../../../shared/model/engine.model';
+import { flushPromises } from '../../tests/utils/test-utils';
+
+const sandboxService: SandboxService = new SandboxServiceMock();
+
+jest.mock('../utils', () => ({
+  generateRandomId: jest.fn().mockReturnValue('randomId')
+}));
+
+jest.mock(
+  '../sandbox.service',
+  () =>
+    function () {
+      return sandboxService;
+    }
+);
+
+const logger: pino.Logger = new PinoLogger();
+
+describe('OIBusCustomTransformer', () => {
+  const sandboxOutput = {
+    output: '',
+    metadata: {
+      contentFile: '',
+      contentSize: 0,
+      createdAt: '',
+      numberOfElement: 0,
+      contentType: '',
+      source: '',
+      options: {}
+    }
+  };
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.useFakeTimers().setSystemTime(new Date(testData.constants.dates.FAKE_NOW));
+    (sandboxService.execute as jest.Mock).mockReturnValueOnce(sandboxOutput);
+  });
+
+  it('should transform data from a stream and return sandbox result', async () => {
+    // Arrange
+    const transformer = new OIBusCustomTransformer(logger, testData.transformers.list[0] as CustomTransformer, testData.north.list[0], {});
+    const source = 'test-source';
+    const dataChunks: Array<OIBusTimeValue> = [
+      {
+        pointId: 'reference1',
+        timestamp: testData.constants.dates.DATE_1,
+        data: {
+          value: 'value1'
+        }
+      },
+      {
+        pointId: 'reference1',
+        timestamp: testData.constants.dates.DATE_2,
+        data: {
+          value: 'value2',
+          quality: 'good'
+        }
+      },
+      {
+        pointId: 'reference2',
+        timestamp: testData.constants.dates.DATE_3,
+        data: {
+          value: 'value1'
+        }
+      }
+    ];
+
+    // Mock Readable stream
+    const mockStream = new Readable();
+
+    // Act
+    const promise = transformer.transform(mockStream, source, null);
+    mockStream.push(JSON.stringify(dataChunks));
+    mockStream.push(null); // End the stream
+
+    await flushPromises();
+    const result = await promise;
+    expect(result).toEqual(sandboxOutput);
+    expect(sandboxService.execute).toHaveBeenCalledWith(JSON.stringify(dataChunks), source, null, testData.transformers.list[0], {});
+  });
+});
