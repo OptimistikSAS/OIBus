9264c0f8aeae92d3e56c97ec2a21cca430526879
burgerni10 <nicolas.burger@optimistik.com>
2025-11-18 18:25:45 +0100

    fix(south-oledb): add password migration

--- backend/src/migration/entity-migrations/v3.7.0.ts
+++ backend/src/migration/entity-migrations/v3.7.0.ts
@@ -1,5 +1,5 @@
 import { Knex } from 'knex';
-import { SouthPostgreSQLSettingsThrottling } from '../../../shared/model/south-settings.model';
+import { SouthOLEDBSettingsThrottling, SouthPostgreSQLSettingsThrottling } from '../../../shared/model/south-settings.model';
 
 const TRANSFORMERS_TABLE = 'transformers';
 const NORTH_TRANSFORMERS_TABLE = 'north_transformers';
@@ -32,12 +32,32 @@ interface NewSouthPostgreSQLSettings {
   password: string | null;
 }
 
+interface OldSouthOLEDBSettings {
+  throttling: SouthOLEDBSettingsThrottling;
+  agentUrl: string;
+  connectionTimeout: number;
+  retryInterval: number;
+  requestTimeout: number;
+  connectionString: string;
+}
+
+interface NewSouthOLEDBSettings {
+  throttling: SouthOLEDBSettingsThrottling;
+  agentUrl: string;
+  connectionTimeout: number;
+  retryInterval: number;
+  requestTimeout: number;
+  connectionString: string;
+  password: string | null;
+}
+
 export async function up(knex: Knex): Promise<void> {
   await createTransformersTable(knex);
   await createNorthTransformersTable(knex);
   await createHistoryTransformersTable(knex);
   await updateRegistrationSettings(knex);
   await updatePostgresqlSettings(knex);
+  await updateOleDBSettings(knex);
 
   // TODO on 3.7.1
   // TODO: remove enum on command table
@@ -135,6 +155,36 @@ async function updatePostgresqlSettings(knex: Knex) {
   for (const history of oldHistory) {
     const oldSettings = JSON.parse(history.south_settings) as OldSouthPostgreSQLSettings;
     const newSettings: NewSouthPostgreSQLSettings = { ...oldSettings, sslMode: false };
+    await knex(HISTORY_QUERIES_TABLE)
+      .update({ south_settings: JSON.stringify(newSettings) })
+      .where('id', history.id);
+  }
+}
+
+async function updateOleDBSettings(knex: Knex) {
+  const oldSouth: Array<{
+    id: string;
+    type: string;
+    settings: string;
+  }> = await knex(SOUTH_CONNECTORS_TABLE).select('id', 'type', 'settings').where('type', 'oledb');
+
+  for (const connector of oldSouth) {
+    const oldSettings = JSON.parse(connector.settings) as OldSouthOLEDBSettings;
+    const newSettings: NewSouthOLEDBSettings = { ...oldSettings, password: null };
+    await knex(SOUTH_CONNECTORS_TABLE)
+      .update({ settings: JSON.stringify(newSettings) })
+      .where('id', connector.id);
+  }
+
+  const oldHistory: Array<{
+    id: string;
+    south_type: string;
+    south_settings: string;
+  }> = await knex(HISTORY_QUERIES_TABLE).select('id', 'south_type', 'south_settings').where('south_type', 'oledb');
+
+  for (const history of oldHistory) {
+    const oldSettings = JSON.parse(history.south_settings) as OldSouthOLEDBSettings;
+    const newSettings: NewSouthOLEDBSettings = { ...oldSettings, password: null };
     await knex(HISTORY_QUERIES_TABLE)
       .update({ south_settings: JSON.stringify(newSettings) })
       .where('id', history.id);
