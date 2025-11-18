734403076523b3274b6fc82d0b900295a9f41178
burgerni10 <nicolas.burger@optimistik.com>
2025-11-18 17:46:16 +0100

    fix(install): improve arg management in install script and adapt mass install script in doc

--- build/deps/win-x64/install.bat
+++ build/deps/win-x64/install.bat
@@ -2,7 +2,6 @@
 
 title Install OIBus as Windows service
 
-
 echo Administrator permissions required. Detecting permission...
 net session >nul 2>&1
 if NOT %errorLevel% == 0 (
@@ -14,14 +13,21 @@ set c=
 set n="OIBus"
 
 :PARSE_PARAMETERS
-set parameter=%~1
-if "%parameter%"=="" goto PARSE_PARAMETERS_DONE
-if "%parameter:~0,1%"=="-" (
-    set %parameter:~1%=%~2
+if "%~1"=="" goto PARSE_PARAMETERS_DONE
+if "%~1"=="-n" (
+    set "n=%~2"
     shift
     shift
     goto PARSE_PARAMETERS
 )
+if "%~1"=="-c" (
+    set "c=%~2"
+    shift
+    shift
+    goto PARSE_PARAMETERS
+)
+shift
+goto PARSE_PARAMETERS
 :PARSE_PARAMETERS_DONE
 
 goto CHECK
