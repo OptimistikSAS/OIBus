a065fe677deb7ccddc791f6d739378084a06e75c
Kovács Bálint Hunor <hunor.kovacs@lynxsolutions.eu>
2025-12-09 15:56:35 +0200

    feat(oianalytics): added cache content management commands and proper permissions

--- backend/src/service/oia/oianalytics-registration.service.spec.ts
+++ backend/src/service/oia/oianalytics-registration.service.spec.ts
@@ -169,7 +169,15 @@ describe('OIAnalytics Registration Service', () => {
         updateNorth: true,
         deleteNorth: true,
         testNorthConnection: true,
-        setpoint: true
+        setpoint: true,
+        searchNorthCacheContent: true,
+        getNorthCacheFileContent: true,
+        removeNorthCacheContent: true,
+        moveNorthCacheContent: true,
+        searchHistoryCacheContent: true,
+        getHistoryCacheFileContent: true,
+        removeHistoryCacheContent: true,
+        moveHistoryCacheContent: true
       }
     };
     const result = {
@@ -256,7 +264,15 @@ describe('OIAnalytics Registration Service', () => {
         updateNorth: true,
         deleteNorth: true,
         testNorthConnection: true,
-        setpoint: true
+        setpoint: true,
+        searchNorthCacheContent: true,
+        getNorthCacheFileContent: true,
+        removeNorthCacheContent: true,
+        moveNorthCacheContent: true,
+        searchHistoryCacheContent: true,
+        getHistoryCacheFileContent: true,
+        removeHistoryCacheContent: true,
+        moveHistoryCacheContent: true
       }
     };
     (oIAnalyticsRegistrationRepository.get as jest.Mock).mockReturnValueOnce(testData.oIAnalytics.registration.completed);
