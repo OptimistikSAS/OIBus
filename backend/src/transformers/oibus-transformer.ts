20b629a90061362f4ea55c6283d126a9fadf2d12
burgerni10 <nicolas.burger@optimistik.com>
2026-01-12 10:42:31 +0100

    chore(transformers): reorganize transformers folders

---
+++ backend/src/transformers/oibus-transformer.ts
@@ -0,0 +1,21 @@
+import pino from 'pino';
+import { Transformer } from '../model/transformer.model';
+import { NorthConnectorEntity } from '../model/north-connector.model';
+import { NorthSettings } from '../../shared/model/north-settings.model';
+import { CacheMetadata, CacheMetadataSource } from '../../shared/model/engine.model';
+import { ReadStream } from 'node:fs';
+import { Readable } from 'node:stream';
+
+export default abstract class OIBusTransformer {
+  constructor(
+    protected logger: pino.Logger,
+    protected transformer: Transformer,
+    protected northConnector: NorthConnectorEntity<NorthSettings>,
+    protected _options: object
+  ) {}
+  abstract transform(
+    data: ReadStream | Readable,
+    source: CacheMetadataSource,
+    filename: string | null
+  ): Promise<{ metadata: CacheMetadata; output: string }>;
+}
