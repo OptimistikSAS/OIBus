f7b3cbe4e7f57cf7ea45011871e77e7085213686
burgerni10 <nicolas.burger@optimistik.com>
2026-01-12 10:42:31 +0100

    chore(transformers): reorganize transformers folders

---
+++ backend/src/transformers/ignore-transformer.ts
@@ -0,0 +1,41 @@
+import OIBusTransformer from './oibus-transformer';
+import { ReadStream } from 'node:fs';
+import { Readable } from 'node:stream';
+import { CacheMetadata, CacheMetadataSource } from '../../shared/model/engine.model';
+import { OIBusObjectAttribute } from '../../shared/model/form.model';
+
+export default class IgnoreTransformer extends OIBusTransformer {
+  public static transformerName = 'ignore';
+
+  async transform(
+    _data: ReadStream | Readable,
+    _source: CacheMetadataSource,
+    _filename: string | null
+  ): Promise<{ metadata: CacheMetadata; output: string }> {
+    return {
+      output: '',
+      metadata: {
+        contentFile: '',
+        contentSize: 0,
+        createdAt: '',
+        numberOfElement: 0,
+        contentType: ''
+      }
+    };
+  }
+
+  public static get manifestSettings(): OIBusObjectAttribute {
+    return {
+      type: 'object',
+      key: 'options',
+      translationKey: 'configuration.oibus.manifest.transformers.options',
+      attributes: [],
+      enablingConditions: [],
+      validators: [],
+      displayProperties: {
+        visible: true,
+        wrapInBox: false
+      }
+    };
+  }
+}
