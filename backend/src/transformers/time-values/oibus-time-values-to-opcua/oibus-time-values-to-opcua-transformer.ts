381a9303904e8337f25207498bd47e877211ab29
burgerni10 <nicolas.burger@optimistik.com>
2026-01-12 10:42:31 +0100

    chore(transformers): reorganize transformers folders

---
+++ backend/src/transformers/time-values/oibus-time-values-to-opcua/oibus-time-values-to-opcua-transformer.ts
@@ -0,0 +1,135 @@
+import OIBusTransformer from '../../oibus-transformer';
+import { ReadStream } from 'node:fs';
+import { pipeline, Readable, Transform } from 'node:stream';
+import { CacheMetadata, CacheMetadataSource, OIBusTimeValue } from '../../../../shared/model/engine.model';
+import { promisify } from 'node:util';
+import { OIBusObjectAttribute } from '../../../../shared/model/form.model';
+import { generateRandomId } from '../../../service/utils';
+import { OIBusOPCUAValue } from '../../connector-types.model';
+
+const pipelineAsync = promisify(pipeline);
+
+interface TransformerOptions {
+  mapping: Array<{ pointId: string; nodeId: string }>;
+}
+
+export default class OIBusTimeValuesToOPCUATransformer extends OIBusTransformer {
+  public static transformerName = 'time-values-to-opcua';
+
+  async transform(
+    data: ReadStream | Readable,
+    _source: CacheMetadataSource,
+    _filename: string | null
+  ): Promise<{ metadata: CacheMetadata; output: string }> {
+    // Collect the data from the stream
+    const chunks: Array<Buffer> = [];
+    await pipelineAsync(
+      data,
+      new Transform({
+        transform(chunk, encoding, callback) {
+          chunks.push(chunk);
+          callback();
+        }
+      })
+    );
+    const stringContent = Buffer.concat(chunks).toString('utf-8');
+    // Combine the chunks into a single buffer
+    const content: Array<OIBusOPCUAValue> = (JSON.parse(stringContent) as Array<OIBusTimeValue>)
+      .map(element => {
+        const mappedElement = this.options.mapping.find(matchingElement => matchingElement.pointId === element.pointId);
+        if (!mappedElement) return null;
+        return {
+          nodeId: mappedElement.nodeId,
+          value: element.data.value as number | string | boolean
+        };
+      })
+      .filter((mappedElement): mappedElement is OIBusOPCUAValue => mappedElement !== null);
+
+    const metadata: CacheMetadata = {
+      contentFile: `${generateRandomId(10)}.json`,
+      contentSize: 0, // It will be set outside the transformer, once the file is written
+      createdAt: '', // It will be set outside the transformer, once the file is written
+      numberOfElement: content.length,
+      contentType: 'opcua'
+    };
+    return {
+      output: JSON.stringify(content),
+      metadata
+    };
+  }
+
+  get options(): TransformerOptions {
+    return this._options as TransformerOptions;
+  }
+
+  public static get manifestSettings(): OIBusObjectAttribute {
+    return {
+      type: 'object',
+      key: 'options',
+      translationKey: 'configuration.oibus.manifest.transformers.options',
+      attributes: [
+        {
+          type: 'array',
+          key: 'mapping',
+          translationKey: 'configuration.oibus.manifest.transformers.time-values-to-opcua.mapping.title',
+          paginate: true,
+          numberOfElementPerPage: 20,
+          validators: [],
+          rootAttribute: {
+            type: 'object',
+            key: 'item',
+            translationKey: 'configuration.oibus.manifest.transformers.time-values-to-opcua.mapping.title',
+            displayProperties: {
+              visible: true,
+              wrapInBox: false
+            },
+            enablingConditions: [],
+            validators: [],
+            attributes: [
+              {
+                type: 'string',
+                key: 'pointId',
+                translationKey: 'configuration.oibus.manifest.transformers.time-values-to-opcua.mapping.point-id',
+                defaultValue: null,
+                validators: [
+                  {
+                    type: 'REQUIRED',
+                    arguments: []
+                  }
+                ],
+                displayProperties: {
+                  row: 0,
+                  columns: 4,
+                  displayInViewMode: true
+                }
+              },
+              {
+                type: 'string',
+                key: 'nodeId',
+                translationKey: 'configuration.oibus.manifest.transformers.time-values-to-opcua.mapping.node-id',
+                defaultValue: null,
+                validators: [
+                  {
+                    type: 'REQUIRED',
+                    arguments: []
+                  }
+                ],
+                displayProperties: {
+                  row: 0,
+                  columns: 4,
+                  displayInViewMode: true
+                }
+              }
+            ]
+          }
+        }
+      ],
+      enablingConditions: [],
+      validators: [],
+      displayProperties: {
+        visible: true,
+        wrapInBox: false
+      }
+    };
+  }
+}
