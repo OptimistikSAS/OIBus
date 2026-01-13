d2b21c7794078604d2a506e9113784d4fb5c0c3f
Kovács Bálint Hunor <hunor.kovacs@lynxsolutions.eu>
2026-01-13 13:24:51 +0200

    feat(versioning): implemented version monitoring and update notification in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotificationComponent } from './shared/notification/notification.component';
@@ -8,6 +8,10 @@ import { WindowService } from './shared/window.service';
 import { CurrentUserService } from './shared/current-user.service';
 import { UserDTO } from '../../../backend/shared/model/user.model';
 import { NavigationService } from './shared/navigation.service';
+import { VersionCheckService } from './shared/version-check.service';
+import { ModalService } from './shared/modal.service';
+import { VersionUpdateModalComponent } from './shared/version-update-modal/version-update-modal.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'oib-root',
@@ -15,16 +19,48 @@ import { NavigationService } from './shared/navigation.service';
   styleUrl: './app.component.scss',
   imports: [RouterOutlet, NavbarComponent, NotificationComponent, DefaultValidationErrorsComponent, BreadcrumbComponent]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   private currentUserService = inject(CurrentUserService);
   private windowService = inject(WindowService);
   private navigationService = inject(NavigationService);
+  private versionCheckService = inject(VersionCheckService);
+  private modalService = inject(ModalService);
+
+  private versionChangeSubscription: Subscription | null = null;
 
   title = 'OIBus';
 
   ngOnInit(): void {
     this.navigationService.init();
-    this.currentUserService.get().subscribe(user => this.reloadIfLanguageOrTimezoneNeedsChange(user));
+    this.currentUserService.get().subscribe(user => {
+      this.reloadIfLanguageOrTimezoneNeedsChange(user);
+
+      // Start version monitoring when user is authenticated
+      if (user) {
+        this.startVersionMonitoring();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.versionCheckService.stopMonitoring();
+    if (this.versionChangeSubscription) {
+      this.versionChangeSubscription.unsubscribe();
+    }
+  }
+
+  private startVersionMonitoring(): void {
+    this.versionCheckService.startMonitoring();
+
+    this.versionChangeSubscription = this.versionCheckService.versionChange$.subscribe(info => {
+      // Open non-dismissible modal when version changes
+      const modalRef = this.modalService.open(VersionUpdateModalComponent, {
+        backdrop: 'static',
+        keyboard: false
+      });
+      modalRef.componentInstance.oldVersion = info.oldVersion;
+      modalRef.componentInstance.newVersion = info.newVersion;
+    });
   }
 
   private reloadIfLanguageOrTimezoneNeedsChange(user: UserDTO | null) {
