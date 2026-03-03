93bf6cdcc4af632c1b25fc2ba3ee8271df7b117f
Kovács Bálint Hunor <hunor.kovacs@lynxsolutions.eu>
2026-03-03 13:59:47 +0200

    feat(transformers): added timeout property to custom transformers and updated related components

--- backend/src/repository/config/north-connector.repository.ts
+++ backend/src/repository/config/north-connector.repository.ts
@@ -243,7 +243,7 @@ export default class NorthConnectorRepository {
   }
 
   private findTransformersForNorth(northId: string): Array<NorthTransformerWithOptions> {
-    const query = `SELECT t.id, t.type, nt.input_type, t.output_type, t.function_name, t.name, t.description, t.custom_manifest, t.custom_code, t.language, nt.options, nt.south_id, nt.id as ntId FROM ${NORTH_TRANSFORMERS_TABLE} nt JOIN ${TRANSFORMERS_TABLE} t ON nt.transformer_id = t.id WHERE nt.north_id = ?;`;
+    const query = `SELECT t.id, t.type, nt.input_type, t.output_type, t.function_name, t.name, t.description, t.custom_manifest, t.custom_code, t.language, t.timeout, nt.options, nt.south_id, nt.id as ntId FROM ${NORTH_TRANSFORMERS_TABLE} nt JOIN ${TRANSFORMERS_TABLE} t ON nt.transformer_id = t.id WHERE nt.north_id = ?;`;
     const result = this.database.prepare(query).all(northId) as Array<Record<string, string>>;
     return result.map(element => ({
       id: element.ntId,
