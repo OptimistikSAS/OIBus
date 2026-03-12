03fc2c3d6d36f41756897eb1701fb1f2c1449972
burgerni10 <nicolas.burger@optimistik.com>
2026-03-12 09:59:34 +0100

    fix(https-proxy-agent): fix dependencies loading

--- backend/src/tests/jest-ts.config.mjs
+++ backend/src/tests/jest-ts.config.mjs
@@ -2,12 +2,14 @@ export default {
   rootDir: '../',
   coverageDirectory: './tests/coverage',
   transform: { '^.+\\.(ts|js)?$': 'ts-jest' },
-  transformIgnorePatterns: ['/node_modules/(?!hexy)/'],
+  transformIgnorePatterns: ['/node_modules/(?!hexy|https-proxy-agent|agent-base)/'],
   testEnvironment: 'node',
   testRegex: '.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
   moduleNameMapper: {
-    oracledb: '<rootDir>/tests/__mocks__/oracledb.mock.ts'
+    oracledb: '<rootDir>/tests/__mocks__/oracledb.mock.ts',
+    '^https-proxy-agent/dist$': '<rootDir>/../node_modules/https-proxy-agent/dist/index.js',
+    '^agent-base$': '<rootDir>/../node_modules/agent-base/dist/index.js'
   },
   coveragePathIgnorePatterns: ['src/migration/', 'src/tests/utils/', 'src/south/south-odbc/odbc-loader.ts']
 };
