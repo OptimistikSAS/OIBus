6128d4383ce517312edc5e006d827a86c5033e26
kbalinthunor <hunor.kovacs@lynxsolutions.eu>
2026-06-22 10:28:12 +0300

    nit(south): fix prettier formatting after route regeneration

--- backend/src/web-server/routes.ts
+++ backend/src/web-server/routes.ts
@@ -1,4 +1,3 @@
-/* tslint:disable */
 /* eslint-disable */
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import type { TsoaRoute } from '@tsoa/runtime';
