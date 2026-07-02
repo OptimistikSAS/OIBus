24645b42ba23c35f4265ac7273eedde0a8c7391e
burgerni10 <nicolas.burger@optimistik.com>
2026-07-02 11:47:38 +0200

    fix(migration): merge 3.9.0 migration

--- backend/src/migration/entity-migrations/3/3.9/v3.9.0.ts
+++ backend/src/migration/entity-migrations/3/3.9/v3.9.0.ts
@@ -1,6 +1,8 @@
 import { Knex } from 'knex';
 
 const ENGINES_TABLE = 'engines';
+const SOUTH_ITEM_GROUPS_TABLE = 'south_item_groups';
+const SOUTH_ITEMS_TABLE = 'south_items';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable(ENGINES_TABLE, t => {
@@ -20,8 +22,44 @@ export async function up(knex: Knex): Promise<void> {
     log_syslog_port: 514,
     log_syslog_protocol: 'udp4'
   });
+
+  await knex.schema.alterTable(SOUTH_ITEM_GROUPS_TABLE, t => {
+    t.string('recovery_strategy').nullable();
+    t.integer('start_time_offset').nullable().defaultTo(0);
+    t.integer('end_time_offset').nullable().defaultTo(0);
+  });
+  await knex.schema.alterTable(SOUTH_ITEMS_TABLE, t => {
+    t.string('recovery_strategy').nullable();
+    t.integer('start_time_offset').nullable().defaultTo(0);
+    t.integer('end_time_offset').nullable().defaultTo(0);
+  });
+
+  await knex.raw(`UPDATE ${SOUTH_ITEM_GROUPS_TABLE} SET start_time_offset = -overlap WHERE overlap IS NOT NULL`);
+  await knex.raw(`UPDATE ${SOUTH_ITEMS_TABLE} SET start_time_offset = -overlap WHERE overlap IS NOT NULL`);
+
+  await knex.schema.alterTable(SOUTH_ITEM_GROUPS_TABLE, t => {
+    t.dropColumn('overlap');
+  });
+  await knex.schema.alterTable(SOUTH_ITEMS_TABLE, t => {
+    t.dropColumn('overlap');
+  });
 }
 
-export async function down(_knex: Knex): Promise<void> {
-  return;
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable(SOUTH_ITEM_GROUPS_TABLE, t => {
+    t.integer('overlap').nullable().defaultTo(null);
+  });
+  await knex.schema.alterTable(SOUTH_ITEMS_TABLE, t => {
+    t.integer('overlap').nullable().defaultTo(null);
+  });
+  await knex.raw(`UPDATE ${SOUTH_ITEM_GROUPS_TABLE} SET overlap = -start_time_offset WHERE start_time_offset IS NOT NULL`);
+  await knex.raw(`UPDATE ${SOUTH_ITEMS_TABLE} SET overlap = -start_time_offset WHERE start_time_offset IS NOT NULL`);
+  await knex.schema.alterTable(SOUTH_ITEM_GROUPS_TABLE, t => {
+    t.dropColumn('start_time_offset');
+    t.dropColumn('end_time_offset');
+  });
+  await knex.schema.alterTable(SOUTH_ITEMS_TABLE, t => {
+    t.dropColumn('start_time_offset');
+    t.dropColumn('end_time_offset');
+  });
 }
