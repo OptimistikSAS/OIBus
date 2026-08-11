716d3bf8fd0e1b7f4c4515fdf8b682767ff18d25
burgerni10 <nicolas.burger@optimistik.com>
2026-08-11 09:28:16 +0200

    feat(transformers): south record-list content type for sql like connectors, and new record-list-to-csv transformer

--- backend/src/south/south-sqlite/manifest.ts
+++ backend/src/south/south-sqlite/manifest.ts
@@ -139,226 +139,31 @@ const manifest: SouthConnectorManifest = {
               }
             },
             {
-              type: 'array',
-              key: 'dateTimeFields',
-              translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields',
-              paginate: false,
-              numberOfElementPerPage: 0,
-              validators: [],
-              rootAttribute: {
-                type: 'object',
-                key: 'dateTimeField',
-                translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.date-time-field',
-                displayProperties: {
-                  visible: true,
-                  wrapInBox: false
-                },
-                enablingConditions: [
-                  {
-                    referralPathFromRoot: 'type',
-                    targetPathFromRoot: 'timezone',
-                    values: ['string']
-                  },
-                  {
-                    referralPathFromRoot: 'type',
-                    targetPathFromRoot: 'locale',
-                    values: ['string']
-                  },
-                  {
-                    referralPathFromRoot: 'type',
-                    targetPathFromRoot: 'format',
-                    values: ['string']
-                  }
-                ],
-                validators: [],
-                attributes: [
-                  {
-                    type: 'string',
-                    key: 'fieldName',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.field-name',
-                    defaultValue: null,
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      },
-                      {
-                        type: 'UNIQUE',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 0,
-                      columns: 4,
-                      displayInViewMode: true
-                    }
-                  },
-                  {
-                    type: 'boolean',
-                    key: 'useAsReference',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.use-as-reference',
-                    defaultValue: false,
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      },
-                      {
-                        type: 'SINGLE_TRUE',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 0,
-                      columns: 4,
-                      displayInViewMode: true
-                    }
-                  },
-                  {
-                    type: 'string-select',
-                    key: 'type',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.type',
-                    defaultValue: 'string',
-                    selectableValues: ['iso-string', 'unix-epoch', 'unix-epoch-ms', 'string'],
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 0,
-                      columns: 4,
-                      displayInViewMode: true
-                    }
-                  },
-                  {
-                    type: 'timezone',
-                    key: 'timezone',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.timezone',
-                    defaultValue: 'UTC',
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 1,
-                      columns: 4,
-                      displayInViewMode: true
-                    }
-                  },
-                  {
-                    type: 'string',
-                    key: 'format',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.format',
-                    defaultValue: 'yyyy-MM-dd HH:mm:ss',
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 1,
-                      columns: 4,
-                      displayInViewMode: false
-                    }
-                  },
-                  {
-                    type: 'string',
-                    key: 'locale',
-                    translationKey: 'configuration.oibus.manifest.south.items.sqlite.date-time-fields.locale',
-                    defaultValue: 'en-En',
-                    validators: [
-                      {
-                        type: 'REQUIRED',
-                        arguments: []
-                      }
-                    ],
-                    displayProperties: {
-                      row: 1,
-                      columns: 4,
-                      displayInViewMode: false
-                    }
-                  }
-                ]
-              }
-            },
-            {
               type: 'object',
-              key: 'serialization',
-              translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.title',
+              key: 'trackingInstant',
+              translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.title',
               displayProperties: {
                 visible: true,
                 wrapInBox: true
               },
-              enablingConditions: [],
-              validators: [
+              enablingConditions: [
                 {
-                  type: 'REQUIRED',
-                  arguments: []
+                  referralPathFromRoot: 'trackInstant',
+                  targetPathFromRoot: 'fieldName',
+                  values: [true]
+                },
+                {
+                  referralPathFromRoot: 'trackInstant',
+                  targetPathFromRoot: 'dateTimeInput',
+                  values: [true]
                 }
               ],
+              validators: [],
               attributes: [
                 {
-                  type: 'string-select',
-                  key: 'type',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.type',
-                  defaultValue: 'csv',
-                  selectableValues: ['csv'],
-                  validators: [
-                    {
-                      type: 'REQUIRED',
-                      arguments: []
-                    }
-                  ],
-                  displayProperties: {
-                    row: 0,
-                    columns: 2,
-                    displayInViewMode: false
-                  }
-                },
-                {
-                  type: 'string',
-                  key: 'filename',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.filename',
-                  defaultValue: '@ConnectorName-@ItemName-@CurrentDate.csv',
-                  validators: [
-                    {
-                      type: 'REQUIRED',
-                      arguments: []
-                    }
-                  ],
-                  displayProperties: {
-                    row: 0,
-                    columns: 4,
-                    displayInViewMode: false
-                  }
-                },
-                {
-                  type: 'string-select',
-                  key: 'delimiter',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.delimiter',
-                  defaultValue: 'COMMA',
-                  selectableValues: ['DOT', 'SEMI_COLON', 'COLON', 'COMMA', 'NON_BREAKING_SPACE', 'SLASH', 'TAB', 'PIPE'],
-                  validators: [
-                    {
-                      type: 'REQUIRED',
-                      arguments: []
-                    }
-                  ],
-                  displayProperties: {
-                    row: 0,
-                    columns: 3,
-                    displayInViewMode: false
-                  }
-                },
-                {
                   type: 'boolean',
-                  key: 'compression',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.compression',
+                  key: 'trackInstant',
+                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.track-instant',
                   defaultValue: false,
                   validators: [
                     {
@@ -368,15 +173,15 @@ const manifest: SouthConnectorManifest = {
                   ],
                   displayProperties: {
                     row: 0,
-                    columns: 3,
+                    columns: 4,
                     displayInViewMode: true
                   }
                 },
                 {
                   type: 'string',
-                  key: 'outputTimestampFormat',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.output-timestamp-format',
-                  defaultValue: 'yyyy-MM-dd HH:mm:ss.SSS',
+                  key: 'fieldName',
+                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.field-name',
+                  defaultValue: null,
                   validators: [
                     {
                       type: 'REQUIRED',
@@ -384,27 +189,108 @@ const manifest: SouthConnectorManifest = {
                     }
                   ],
                   displayProperties: {
-                    row: 1,
-                    columns: 3,
-                    displayInViewMode: false
+                    row: 0,
+                    columns: 8,
+                    displayInViewMode: true
                   }
                 },
                 {
-                  type: 'timezone',
-                  key: 'outputTimezone',
-                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.serialization.output-timezone',
-                  defaultValue: 'Europe/Paris',
-                  validators: [
+                  type: 'object',
+                  key: 'dateTimeInput',
+                  translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.date-time-input.title',
+                  displayProperties: {
+                    visible: true,
+                    wrapInBox: false
+                  },
+                  enablingConditions: [
                     {
-                      type: 'REQUIRED',
-                      arguments: []
+                      referralPathFromRoot: 'type',
+                      targetPathFromRoot: 'timezone',
+                      values: ['string']
+                    },
+                    {
+                      referralPathFromRoot: 'type',
+                      targetPathFromRoot: 'format',
+                      values: ['string']
+                    },
+                    {
+                      referralPathFromRoot: 'type',
+                      targetPathFromRoot: 'locale',
+                      values: ['string']
                     }
                   ],
-                  displayProperties: {
-                    row: 1,
-                    columns: 3,
-                    displayInViewMode: false
-                  }
+                  validators: [],
+                  attributes: [
+                    {
+                      type: 'string-select',
+                      key: 'type',
+                      translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.date-time-input.type',
+                      defaultValue: 'string',
+                      selectableValues: ['iso-string', 'unix-epoch', 'unix-epoch-ms', 'string'],
+                      validators: [
+                        {
+                          type: 'REQUIRED',
+                          arguments: []
+                        }
+                      ],
+                      displayProperties: {
+                        row: 0,
+                        columns: 3,
+                        displayInViewMode: true
+                      }
+                    },
+                    {
+                      type: 'timezone',
+                      key: 'timezone',
+                      translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.date-time-input.timezone',
+                      defaultValue: 'UTC',
+                      validators: [
+                        {
+                          type: 'REQUIRED',
+                          arguments: []
+                        }
+                      ],
+                      displayProperties: {
+                        row: 0,
+                        columns: 3,
+                        displayInViewMode: false
+                      }
+                    },
+                    {
+                      type: 'string',
+                      key: 'format',
+                      translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.date-time-input.format',
+                      defaultValue: 'yyyy-MM-dd HH:mm:ss',
+                      validators: [
+                        {
+                          type: 'REQUIRED',
+                          arguments: []
+                        }
+                      ],
+                      displayProperties: {
+                        row: 0,
+                        columns: 3,
+                        displayInViewMode: false
+                      }
+                    },
+                    {
+                      type: 'string',
+                      key: 'locale',
+                      translationKey: 'configuration.oibus.manifest.south.items.sqlite.tracking-instant.date-time-input.locale',
+                      defaultValue: 'en-En',
+                      validators: [
+                        {
+                          type: 'REQUIRED',
+                          arguments: []
+                        }
+                      ],
+                      displayProperties: {
+                        row: 0,
+                        columns: 3,
+                        displayInViewMode: false
+                      }
+                    }
+                  ]
                 }
               ]
             }
