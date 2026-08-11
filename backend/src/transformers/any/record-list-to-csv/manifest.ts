6687c932cdad916c51cc2d72cb208d51d6948dc8
burgerni10 <nicolas.burger@optimistik.com>
2026-08-11 12:07:32 +0200

    fix(transformer): fix record-to-csv behaviour to parse different fields

--- backend/src/transformers/any/record-list-to-csv/manifest.ts
+++ backend/src/transformers/any/record-list-to-csv/manifest.ts
@@ -79,27 +79,41 @@ const manifest: TransformerManifest = {
         displayProperties: { row: 1, columns: 3, displayInViewMode: false }
       },
       {
+        type: 'string',
+        key: 'nullValue',
+        translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.null-value',
+        defaultValue: '',
+        validators: [],
+        displayProperties: { row: 2, columns: 3, displayInViewMode: true }
+      },
+      {
         type: 'array',
-        key: 'datetimeFields',
-        translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.title',
+        key: 'fields',
+        translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.title',
         paginate: false,
-        numberOfElementPerPage: 0,
+        numberOfElementPerPage: 20,
         validators: [],
         rootAttribute: {
           type: 'object',
-          key: 'datetimeField',
-          translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.item.title',
+          key: 'fieldItem',
+          translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.item.title',
           displayProperties: {
             visible: true,
             wrapInBox: true
           },
-          enablingConditions: [],
+          enablingConditions: [
+            {
+              referralPathFromRoot: 'dataType',
+              targetPathFromRoot: 'datetimeSettings',
+              values: ['datetime']
+            }
+          ],
           validators: [],
           attributes: [
             {
               type: 'string',
               key: 'fieldName',
-              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.field-name',
+              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.field-name',
               defaultValue: null,
               validators: [
                 { type: 'REQUIRED', arguments: [] },
@@ -108,70 +122,115 @@ const manifest: TransformerManifest = {
               displayProperties: { row: 0, columns: 4, displayInViewMode: true }
             },
             {
+              type: 'string',
+              key: 'columnName',
+              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.column-name',
+              defaultValue: null,
+              validators: [],
+              displayProperties: { row: 0, columns: 4, displayInViewMode: true }
+            },
+            {
+              type: 'string-select',
+              key: 'dataType',
+              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.data-type',
+              defaultValue: 'string',
+              selectableValues: ['string', 'number', 'datetime', 'array', 'boolean', 'object'],
+              validators: [{ type: 'REQUIRED', arguments: [] }],
+              displayProperties: { row: 0, columns: 4, displayInViewMode: true }
+            },
+            {
+              type: 'string',
+              key: 'fieldProcess',
+              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.field-process',
+              defaultValue: null,
+              validators: [],
+              displayProperties: { row: 1, columns: 12, displayInViewMode: false }
+            },
+            {
               type: 'object',
-              key: 'input',
-              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.input.title',
+              key: 'datetimeSettings',
+              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.title',
               displayProperties: {
                 visible: true,
                 wrapInBox: false
               },
               enablingConditions: [
-                { referralPathFromRoot: 'type', targetPathFromRoot: 'timezone', values: ['string', 'date-time'] },
-                { referralPathFromRoot: 'type', targetPathFromRoot: 'format', values: ['string'] },
-                { referralPathFromRoot: 'type', targetPathFromRoot: 'locale', values: ['string'] }
+                { referralPathFromRoot: 'inputType', targetPathFromRoot: 'inputTimezone', values: ['string'] },
+                { referralPathFromRoot: 'inputType', targetPathFromRoot: 'inputFormat', values: ['string'] },
+                { referralPathFromRoot: 'inputType', targetPathFromRoot: 'inputLocale', values: ['string'] },
+                { referralPathFromRoot: 'outputType', targetPathFromRoot: 'outputTimezone', values: ['string'] },
+                { referralPathFromRoot: 'outputType', targetPathFromRoot: 'outputFormat', values: ['string'] },
+                { referralPathFromRoot: 'outputType', targetPathFromRoot: 'outputLocale', values: ['string'] }
               ],
               validators: [],
               attributes: [
                 {
                   type: 'string-select',
-                  key: 'type',
-                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.input.type',
-                  defaultValue: 'string',
-                  selectableValues: ['string', 'iso-string', 'unix-epoch', 'unix-epoch-ms', 'date-time'],
+                  key: 'inputType',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.input-type',
+                  defaultValue: 'iso-string',
+                  selectableValues: ['iso-string', 'unix-epoch', 'unix-epoch-ms', 'string'],
                   validators: [{ type: 'REQUIRED', arguments: [] }],
-                  displayProperties: { row: 0, columns: 4, displayInViewMode: true }
+                  displayProperties: { row: 0, columns: 3, displayInViewMode: false }
                 },
                 {
                   type: 'timezone',
-                  key: 'timezone',
-                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.input.timezone',
+                  key: 'inputTimezone',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.timezone',
                   defaultValue: 'UTC',
                   validators: [{ type: 'REQUIRED', arguments: [] }],
-                  displayProperties: { row: 0, columns: 4, displayInViewMode: false }
+                  displayProperties: { row: 0, columns: 3, displayInViewMode: false }
                 },
                 {
                   type: 'string',
-                  key: 'format',
-                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.input.format',
+                  key: 'inputFormat',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.format',
                   defaultValue: 'yyyy-MM-dd HH:mm:ss',
                   validators: [{ type: 'REQUIRED', arguments: [] }],
-                  displayProperties: { row: 0, columns: 4, displayInViewMode: false }
+                  displayProperties: { row: 0, columns: 3, displayInViewMode: false }
                 },
                 {
                   type: 'string',
-                  key: 'locale',
-                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.input.locale',
+                  key: 'inputLocale',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.locale',
                   defaultValue: 'en-En',
                   validators: [{ type: 'REQUIRED', arguments: [] }],
-                  displayProperties: { row: 0, columns: 4, displayInViewMode: false }
+                  displayProperties: { row: 0, columns: 3, displayInViewMode: false }
+                },
+                {
+                  type: 'string-select',
+                  key: 'outputType',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.output-type',
+                  defaultValue: 'iso-string',
+                  selectableValues: ['iso-string', 'unix-epoch', 'unix-epoch-ms', 'string'],
+                  validators: [{ type: 'REQUIRED', arguments: [] }],
+                  displayProperties: { row: 1, columns: 3, displayInViewMode: false }
+                },
+                {
+                  type: 'timezone',
+                  key: 'outputTimezone',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.timezone',
+                  defaultValue: 'UTC',
+                  validators: [{ type: 'REQUIRED', arguments: [] }],
+                  displayProperties: { row: 1, columns: 3, displayInViewMode: false }
+                },
+                {
+                  type: 'string',
+                  key: 'outputFormat',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.format',
+                  defaultValue: 'yyyy-MM-dd HH:mm:ss',
+                  validators: [{ type: 'REQUIRED', arguments: [] }],
+                  displayProperties: { row: 1, columns: 3, displayInViewMode: false }
+                },
+                {
+                  type: 'string',
+                  key: 'outputLocale',
+                  translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.fields.datetime-settings.locale',
+                  defaultValue: 'en-En',
+                  validators: [{ type: 'REQUIRED', arguments: [] }],
+                  displayProperties: { row: 1, columns: 3, displayInViewMode: false }
                 }
               ]
-            },
-            {
-              type: 'string',
-              key: 'outputTimestampFormat',
-              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.output-timestamp-format',
-              defaultValue: 'yyyy-MM-dd HH:mm:ss.SSS',
-              validators: [{ type: 'REQUIRED', arguments: [] }],
-              displayProperties: { row: 1, columns: 6, displayInViewMode: false }
-            },
-            {
-              type: 'timezone',
-              key: 'outputTimezone',
-              translationKey: 'configuration.oibus.manifest.transformers.record-list-to-csv.datetime-fields.output-timezone',
-              defaultValue: 'UTC',
-              validators: [{ type: 'REQUIRED', arguments: [] }],
-              displayProperties: { row: 1, columns: 6, displayInViewMode: false }
             }
           ]
         }
